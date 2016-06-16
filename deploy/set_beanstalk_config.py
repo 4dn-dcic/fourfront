dd0937773fb37a15979a5489353e7545a6fdb489
Jeremy Johnson <jeremy@softwork.com.my>
2016-06-16 05:56:49 -0400

    updating sed command to work in aws

--- deploy/set_beanstalk_config.py
+++ deploy/set_beanstalk_config.py
@@ -13,17 +13,20 @@ def dbconn_from_env():
         host = os.environ['RDS_HOSTNAME']
         port = os.environ['RDS_PORT']
         return "postgresql://%s:%s@%s:%s/%s" % (user, pwd, host, port, db)
+    return "postgresql://abh:def@local-host:123/ebd"
 
 def update_connection_string(conn):
 
     if not conn: return
-
-    filename = 'base.ini'
+    file_dir, _ = os.path.split(os.path.abspath(__file__))
+    filename = os.path.join(file_dir, '..','base.ini')
+    print (filename)
     regex = 's#sqlalchemy.*#sqlalchemy.url = %s#' % (conn)
 
+    # not for amazon linux use sed -i for osx use sed -i ''
     print("updated base.ini with connection string", conn)
     subprocess.check_output(
-        ['sed', '-i', '', regex, filename])
+        ['sed', '-i', regex, filename])
 
 if __name__ == "__main__":
     update_connection_string(dbconn_from_env())
