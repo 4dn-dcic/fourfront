b5b128ca62706aeba52231d41c4442389f11fc75
Koray Kırlı <koraykirli@gmail.com>
2016-07-18 16:37:20 -0400

    Experiment object embedding 2

--- src/encoded/types/experimend.py
+++ src/encoded/types/experimend.py
@@ -38,5 +38,6 @@ class ExperimentHiC(Experimend):
     item_type = 'experiment_hic'
     schema = load_schema('encoded:schemas/experiment_hic.json')
     embedded = Experimend.embedded + [
+     "digestion_enzyme",
      "digestion_enzyme.enzyme_source"
      ]
