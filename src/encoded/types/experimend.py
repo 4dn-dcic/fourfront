84a4ec3ee7fd01c24f338dd65c1d9f7d378095d1
Koray Kırlı <koraykirli@gmail.com>
2016-07-26 15:09:09 -0400

    Experiment relations II

--- src/encoded/types/experimend.py
+++ src/encoded/types/experimend.py
@@ -1,13 +1,15 @@
+"""Abstract collection for experiment and integration of all experiment types."""
+
 from snovault import (
     abstract_collection,
-    calculated_property,
+    # calculated_property,
     collection,
     load_schema,
 )
-from pyramid.security import Authenticated
+# from pyramid.security import Authenticated
 from .base import (
-    Item,
-    paths_filtered_by_status,
+    Item
+    # paths_filtered_by_status,
 )
 
 
@@ -19,13 +21,37 @@ from .base import (
         'description': 'Listing of all types of experiments.',
     })
 class Experimend(Item):
+    """The main expeperiment class."""
+
     base_types = ['Experimend'] + Item.base_types
     embedded = [
      "protocol",
      "protocol_variation",
-     "lab"
+     "lab",
+     "experiment_relation"
     ]
     name_key = 'accession'
+
+    # def _update(self, properties, sheets=None):
+    #     DicRefRelation = {
+    #         "controlled by": "control for",
+    #         "derived from": "source for",
+    #         "control for": "control by",
+    #         "source for": "derived from"
+    #         }
+    #     acc = properties["accession"]
+    #     if 'experiment_relation' in properties.keys():
+    #         for relation in properties["experiment_relation"]:
+    #             switch = relation["relationship_type"]
+    #             rev_switch = DicRefRelation[switch]
+    #             related_exp = relation["experiment"]
+    #             rel_dic = {'experiment_relation': [{"relationship_type": rev_switch, "experiment": acc}]}
+    #
+    #             target_exp = Experimend.get(related_exp)
+    #             if target_exp:
+    #                 if target_exp.update(rel_dic)
+    #
+    #     super(Experimend, self)._update(properties, sheets)
 
 
 @collection(
@@ -36,6 +62,8 @@ class Experimend(Item):
         'description': 'Listing Hi-C Experiments',
     })
 class ExperimentHiC(Experimend):
+    """The experiment class for Hi-C experiments."""
+
     item_type = 'experiment_hic'
     schema = load_schema('encoded:schemas/experiment_hic.json')
     embedded = Experimend.embedded + [
