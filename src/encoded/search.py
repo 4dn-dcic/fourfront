629eb4d15f9b5b4245b4ca91686684c378d68cd6
j1z0 <jeremy@softworks.com.my>
2016-08-04 17:17:46 -0400

    fixed #76 for loading experiments @aschroed give me tacos :)

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -440,7 +440,7 @@ def search_result_actions(request, doc_types, es_results, position=None):
     # generate batch download URL for experiments
     # TODO we could enable them for Datasets as well here, but not sure how well it will work
     # batch download disabled for region-search results
-    if '/region-search/' not in request.url:
+    if '/region-search/' not in request.url and aggregations.get('files-file_type'):
         if doc_types == ['Experiment'] and any(
                 bucket['doc_count'] > 0
                 for bucket in aggregations['files-file_type']['files-file_type']['buckets']):
