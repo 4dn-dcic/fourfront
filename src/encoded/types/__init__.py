5af1549a364244dd635dbe478c5c26b25b56c4cf
Andy Schroeder <andrew_schroeder@hms.harvard.edu>
2016-08-09 11:32:16 -0400

    Software schema set up with types file

--- src/encoded/types/__init__.py
+++ src/encoded/types/__init__.py
@@ -180,53 +180,6 @@ class QualityMetricFlag(Item):
 
 
 @collection(
-    name='softwares',
-    properties={
-        'title': 'Softwares',
-        'description': 'Listing of software for 4DN analyses',
-    })
-class Software(Item):
-    """The Software class that contains the software... used."""
-
-    item_type = 'software'
-    schema = load_schema('encoded:schemas/software.json')
-
-    def _update(self, properties, sheets=None):
-        # update self first to ensure 'software_relation' are stored in self.properties
-        properties['name'] = properties['title'].replace(' ', '-').lower() + properties['version']
-        super(Software, self)._update(properties, sheets)
-        # DicRefRelation = {
-        #      "derived from": "parent of",
-        #      "parent of": "derived from",
-        #      "container for": "contained in",
-        #      "contained in": "container for"
-        #      }
-        # acc = str(self.uuid)
-        # if 'software_relation' in properties.keys():
-        #     for relation in properties["software_relation"]:
-        #         switch = relation["relationship_type"]
-        #         rev_switch = DicRefRelation[switch]
-        #         related_sw = relation["software"]
-        #         relationship_entry = {"relationship_type": rev_switch, "software": acc}
-        #         rel_dic = {'software_relation': [relationship_entry, ]}
-        #
-        #         target_sw = self.collection.get(related_sw)
-        #         # case one we don't have relations
-        #         if 'software_relation' not in target_sw.properties.keys():
-        #             target_sw.properties.update(rel_dic)
-        #             target_sw.update(target_sw.properties)
-        #         else:
-        #             # case two we have relations but not the one we need
-        #             for target_relation in target_sw.properties['software_relation']:
-        #                 if target_relation['software'] == acc:
-        #                     break
-        #             else:
-        #                 # make data for new software_relation
-        #                 target_sw.properties['software_relation'].append(relationship_entry)
-        #                 target_sw.update(target_sw.properties)
-
-
-@collection(
     name='analysis_steps',
     properties={
         'title': 'AnalysisSteps',
