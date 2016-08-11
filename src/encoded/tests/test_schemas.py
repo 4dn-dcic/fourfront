05b69bed18930695cd2774b728fd91467ea581d9
j1z0 <jeremy@softworks.com.my>
2016-08-11 11:39:53 -0400

    added additional test to verify mixins not accidently being overwritten

--- src/encoded/tests/test_schemas.py
+++ src/encoded/tests/test_schemas.py
@@ -1,16 +1,42 @@
 import pytest
 from pkg_resources import resource_listdir
+from snovault.schema_utils import load_schema
 
 SCHEMA_FILES = [
     f for f in resource_listdir('encoded', 'schemas')
     if f.endswith('.json')
 ]
 
+@pytest.fixture(scope='module')
+def master_mixins():
+    return load_schema('encoded:schemas/mixins.json')
 
 @pytest.mark.parametrize('schema', SCHEMA_FILES)
-def test_load_schema(schema):
-    from snovault.schema_utils import load_schema
-    assert load_schema('encoded:schemas/%s' % schema)
+def test_load_schema(schema, master_mixins):
+    loaded_schema = load_schema('encoded:schemas/%s' % schema)
+    assert(loaded_schema)
+
+    #check the mixin properties for each schema
+    if not schema == ('mixins.json'):
+        verify_mixins(loaded_schema, master_mixins)
+
+def verify_mixins(loaded_schema, master_mixins):
+    '''
+    test to ensure that we didn't accidently overwrite mixins somehow
+    '''
+    for mixin in loaded_schema.get('mixinProperties',[]):
+        # get the mixin name from {'$ref':'mixins.json#/schema_version'}
+        mixin_file_name, mixin_name = mixin['$ref'].split('/')
+        if mixin_file_name != "mixins.json":
+            #skip any mixins not in main mixins.json
+            continue
+        mixin_schema = master_mixins[mixin_name]
+
+        #each field in the mixin should be present in the parent schema with same properties
+        for mixin_field_name, mixin_field in mixin_schema.items():
+            schema_field = loaded_schema['properties'][mixin_field_name]
+            for key in mixin_field.keys():
+                assert mixin_field[key] == schema_field[key]
 
 
 def test_linkTo_saves_uuid(root, submitter, lab):
