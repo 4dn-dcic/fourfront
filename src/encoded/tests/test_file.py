40a9ecc0689c87761cc91f943cf1419295c20e21
j1z0 <jeremy_johnson@hms.harvard.edu>
2016-09-16 15:42:45 -0400

    ff-221 #comment change s3 key to not use date

--- src/encoded/tests/test_file.py
+++ src/encoded/tests/test_file.py
@@ -1,4 +1,7 @@
 import pytest
+from encoded.types.file import File, post_upload
+from pyramid.httpexceptions import HTTPForbidden
+pytestmark = pytest.mark.working
 
 
 def test_reference_file_by_md5(testapp, file):
@@ -15,9 +18,10 @@ def test_replaced_file_not_uniqued(testapp, file):
 def fastq(award, experiment, lab):
     return {
         'accession': '4DNFI067APU2',
-        'award': award['@id'],
-        'lab': lab['@id'],
+        'award': award['uuid'],
+        'lab': lab['uuid'],
         'file_format': 'fastq',
+        'filename' : 'test.fastq',
         'md5sum': '0123456789abcdef0123456789abcdef',
         'status': 'in progress',
     }
@@ -54,6 +58,7 @@ def fastq_related_file(fastq):
 
 
 
+
 def test_file_post_fastq_related(testapp, fastq, fastq_related_file):
     testapp.post_json('/file', fastq, status=201)
     fastq_related_res = testapp.post_json('/file', fastq_related_file, status=201)
@@ -64,3 +69,76 @@ def test_file_post_fastq_related(testapp, fastq, fastq_related_file):
     assert fastq_related_files == [{'file': fastq_related_res.json['@graph'][0]['@id'],
                                    'relationship_type': 'parent of'}]
 
+
+def test_external_creds(mocker):
+    mock_boto = mocker.patch('encoded.types.file.boto', autospec=True)
+
+    from encoded.types.file import external_creds
+    ret = external_creds('test-bucket', 'test-key', 'name')
+    assert ret['key'] == 'test-key'
+    assert ret['bucket'] == 'test-bucket'
+    assert ret['service'] == 's3'
+    assert 'upload_credentials' in ret.keys()
+
+
+
+def test_create_file_request_proper_s3_resource(registry, fastq, mocker):
+    # note mocker is pytest-mock functionality
+
+    # ensure status uploading so create tries to upload
+    fastq['status'] = "uploading"
+
+    # don't actually call aws
+    external_creds = mocker.patch('encoded.types.file.external_creds')
+    # don't actually create this bad boy
+    mocker.patch('encoded.types.base.Item.create')
+
+    my_file = File.create(registry, '1234567', fastq)
+
+    # check that we would have called aws
+    expected_s3_key = "1234567/%s.fastq.gz" % (fastq['accession'])
+    external_creds.assert_called_once_with('test-bucket', expected_s3_key,
+                                          fastq['filename'], 'test-profile')
+
+def test_name_for_replaced_file_is_uuid(registry, fastq):
+    fastq['status'] = 'replaced'
+
+    uuid ="0afb6080-1c08-11e4-8c21-0800200c9a44"
+    my_file = File.create(registry,uuid, fastq)
+    assert my_file.__name__ == uuid
+
+
+def test_upload_credentails_not_set_for_replaced_file(registry, fastq):
+    fastq['status'] = 'replaced'
+
+    uuid ="0afb6080-1c08-11e4-8c21-0800200c9a44"
+    my_file = File.create(registry,uuid, fastq)
+
+    # upload credentials only get set when status is 'uploading'
+    assert my_file.upload_credentials() is None
+
+def test_name_for_file_is_accession(registry, fastq):
+
+    uuid ="0afb6080-1c08-11e4-8c21-0800200c9a44"
+    my_file = File.create(registry,uuid, fastq)
+    assert my_file.__name__ == fastq['accession'] 
+
+def test_file_type(registry, fastq):
+
+    uuid ="0afb6080-1c08-11e4-8c21-0800200c9a44"
+    my_file = File.create(registry,uuid, fastq)
+    assert 'gz' == my_file.file_type('gz')
+    assert "fastq gz" == my_file.file_type('fastq', 'gz')
+
+def test_post_upload_only_for_uploading_or_upload_failed_status(registry, fastq, request):
+    fastq['status'] = 'in progress'
+
+    uuid ="0afb6080-1c08-11e4-8c21-0800200c9a44"
+    my_file = File.create(registry,uuid, fastq)
+
+    try:
+        post_upload(my_file,request)
+    except HTTPForbidden as e:
+        assert True 
+    else:
+        assert False
