fc59d3bda4811e1ab65a97ff66c7b4f486372268
j1z0 <jeremy_johnson@hms.harvard.edu>
2016-09-20 13:47:08 -0400

    working on travis deploy

--- deploy_beanstalk.py
+++ deploy_beanstalk.py
@@ -2,6 +2,8 @@ import os
 import sys
 import subprocess
 import hashlib
+import argparse
+
 
 def get_git_version():
     version = os.environ.get("TRAVIS_COMMIT", None)
@@ -13,6 +15,7 @@ def get_git_version():
         if diff:
             version += '-patch' + hashlib.sha1(diff).hexdigest()[:7]
     return version
+
 
 def update_version(version):
     filename = 'buildout.cfg'
@@ -28,6 +31,7 @@ def update_version(version):
     print("git commit")
     subprocess.check_output(
         ['git', 'commit', '-m', 'version bump'])
+
 
 def deploy():
     '''
@@ -45,7 +49,15 @@ def deploy():
             sys.stdout.write(out)
             sys.stdout.flush()
 
+
 if __name__ == "__main__":
-    ver = get_git_version()
-    update_version(ver)
+    parser = argparse.ArgumentParser(
+        description="update version if relevant and deploy"
+    )
+    parser.add_argument('--prod', action="store_true", help="deploy to prod")
+    args = parser.parse_args()
+
+    if not args.prod:
+        ver = get_git_version()
+        update_version(ver)
     deploy()
