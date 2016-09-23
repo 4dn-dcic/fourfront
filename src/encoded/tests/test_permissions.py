7088930b6a11e3fba017c137fc7169ff8e0615a0
j1z0 <jeremy_johnson@hms.harvard.edu>
2016-09-23 11:59:48 -0400

    got test being a bit more descriptive of what its testing

--- src/encoded/tests/test_permissions.py
+++ src/encoded/tests/test_permissions.py
@@ -159,13 +159,19 @@ def test_users_post_disallowed(submitter, access_key, submitter_testapp):
     submitter_testapp.post_json('/user', item, status=403)
 
 
-def test_users_view_basic_authenticated(submitter, authenticated_testapp):
+def test_users_cannot_view_other_users_info_with_basic_authenticated(submitter, authenticated_testapp):
     authenticated_testapp.get(submitter['@id'], status=403)
     # res = authenticated_testapp.get(submitter['@id'])
     # assert 'title' in res.json
     # assert 'email' not in res.json
     # assert 'access_keys' not in res.json
 
+
+def test_users_can_see_their_own_user_info(submitter, submitter_testapp):
+    res = submitter_testapp.get(submitter['@id'])
+    assert 'title' in res.json
+    assert 'email' in res.json
+    assert 'access_keys' in res.json
 
 def test_users_view_basic_anon(submitter, anontestapp):
     anontestapp.get(submitter['@id'], status=403)
