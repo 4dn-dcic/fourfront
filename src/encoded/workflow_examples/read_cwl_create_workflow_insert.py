fce808261f41c20c9ac1fef66aaceb3b5579b4ac
SooLee <duplexa@gmail.com>
2016-10-03 17:33:19 -0400

    FF-277 inserts for gitar workflow.

--- src/encoded/workflow_examples/read_cwl_create_workflow_insert.py
+++ src/encoded/workflow_examples/read_cwl_create_workflow_insert.py
@@ -1,65 +1,171 @@
 #!/usr/bin/python
 
 import json
+import sys
 
+
+# function that parses 'source' field of CWL, which contain information of step and argument names.
+# it returns a dictionary with 'step' and 'arg' as key.
+# if the source is global, 'step' will be ''.
 def parse_source(source_str): 
   source={}
+  source_str = source_str.strip('#')
+
   if '.' in source_str:  ## the format is  #step.ID, so if '.' doesn't exist, that implies this is a global argument.
+    ## step argument ##
     source_arr = source_str.split(".")
-    source['step'] = source_arr[0].strip('#')
+    source['step'] = source_arr[0]
     source['arg'] = source_arr[1]
   else:
+    ## global argument ##
     source['step'] = ''
-    source['arg'] = source_str.strip('#')
+    source['arg'] = source_str
   return(source)
 
 
+
+# given the type field of an element of cwl_dict['inputs'] (e.g. cwl_dict['inputs'][1]['type'], return either 'Input file' or 'parameter'. 
+def Is_Input_file_or_parameter ( cwl_param_type ):
+
+  argument_type='parameter'  ## default is parameter unless the following conditions meet
+
+  if isinstance(cwl_param_type,list):
+    if 'File' in cwl_param_type:  #  e.g. type=[ 'File','null' ]  (SBG)
+      argument_type='Input file'
+    elif isinstance(cwl_param_type[0],dict) and cwl_param_type[0]['type']=='array' and cwl_param_type[0]['items']=='File':  # e.g. type=[{'type':'array','items':'File'}] (SBG)
+      argument_type='Input file'
+  elif cwl_param_type=='File':     # e.g. type='File'
+    argument_type='Input file'
+  elif isinstance(cwl_param_type,dict) and cwl_param_type['type']=='array' and cwl_param_type['items']=='File':  # e.g. type={'type':'array','items':'File'}
+    argument_type='Input file'
+
+  return argument_type
+
+
+
+# Add a workflow output argument and map to the step output argument.
+# for now we assume that 1. a global output argument has a single source and 2. that it is an output of some step. (I think this is a reasonable assumption)
+def map_workflow_output_argument_to_step_argument ( workflow, source, workflow_argument, step, step_argument ):
+  workflow['arguments'].append( 
+    {'workflow_argument_name': workflow_argument, 
+     'argument_type':'Output file', 
+     'argument_mapping':[{ 'workflow_step': step,
+                           'step_argument_name': step_argument,
+                           'step_argument_type':'Output file' 
+                        }]
+    })  
+
+
+
+# Add a step argument and map to the global input source.
+# Assumes the global source exists in workflow['arguments']
+def map_step_argument_to_workflow_input_argument ( workflow, source, step_id, step_argument ):
+
+  if workflow.has_key('arguments'):
+    argument_index= -1
+    for i in range(0,len(workflow['arguments'])):
+      e = workflow['arguments'][i]
+      if e['workflow_argument_name'] == source['arg']:
+        argument_index = i
+        global_argument_type = e['argument_type']
+    if argument_index == -1:
+      sys.exit("Error: corresponding workflow argument doesn't exist: {}".format(source['arg']))
+  else:
+    sys.exit("Error: workflow argument doesn't exist.")
+  
+  # fill in the workflow dictionary. The step argument type is assumed to be the same as global argument type (in this case global argument type exists and since it is the source, it is either Input file or parameter.)
+  workflow['arguments'][argument_index]['argument_mapping']= \
+    [{ 'workflow_step': step_id, # id of the step.
+       'step_argument_name': step_argument, # id of an input entry of the step, remove '#step' from #step.ID
+       'step_argument_type': global_argument_type
+    }]
+
+
+
+# add a step argument and map it to another step argument
+# if source step argument doesn't exist in the workflow dictionary yet, create a new entry.
+# the function assumes that the source is not a global argument.
+def map_step_argument_to_another_step_argument ( workflow, source, step_id, step_argument ):
+
+  if workflow.has_key('arguments'):
+    for i in range(0,len(workflow['arguments'])):
+      e= workflow['arguments'][i]
+      argument_index=-1
+      if e.has_key('workflow_argument_mapping'):
+        for e2 in e['workflow_argument_mapping']:
+          if e['workflow_step'] == source.step and e['step_argument_name'] == source.arg: # sourced from a previously entered entry.
+            argument_index=i
+            workflow['arguments'][argument_index]['argument_mapping']= \
+              [{ 'workflow_step': step_id, # id of the step.
+                 'step_argument_name': step_argument, # id of an input entry of the step, remove '#step' from #step.ID
+                 'step_argument_type': 'Input file or parameter'
+              },
+              { 'workflow_step': source['step'], # id of the source step.
+                 'step_argument_name': source['arg'],
+                 'step_argument_type': 'Output file or parameter' # do we pass parameters between steps as well?
+              }]
+            break # in theory there should be only a single match, so break shoudn't be necessary except for saving time.
+      if argument_index == -1: ## corresponding source step argument doesn't exist. It may appear later in cwl.
+        # sys.exit("Error: corresponding source step argument doesn't exist.") # don't produce error message. create a new entry.
+        workflow['arguments'].append( 
+          {'workflow_argument_name': '',
+           'argument_type':'', 
+           'argument_mapping':[{ 'workflow_step': step_id,
+                                 'step_argument_name': step_argument,
+                                 'step_argument_type':'Input file or parameter'  # either Input file or parameter. # Is there a way to know this from workflow cwl? I will not decide it for now : any argument that is not globally associated doesn't matter too much in terms of schema.
+                              },
+                              { 'workflow_step': source['step'],
+                                'step_argument_name': source['arg'],
+                                'step_argument_type':'Output file or parameter' # do we pass parameters between steps as well?
+                              }]
+          })  
+  else:
+    sys.exit("Error: workflow argument doesn't exist.")
+
+
+
+
+# function that takes a cwl file and write a workflow insert json file
 def parse_cwl(cwlfile, workflow_metadata_json):
 
   # get cwl as a dict
   with open(cwlfile,'r') as f:
     cwl_dict=json.load(f)
+
+  # handle SBG cwl.
   cwl_dict=cwl_dict['raw']  # SBG's cwl is one level down under the 'raw' field.
   
-
+  # initialize dictionary to write to output json file
   workflow={ 'arguments':[] }  # this is what we will create.
 
-
-  ## parsing global input files and parameters (can't map to step arguments yet)
+  # parsing global input files and parameters and storing to the workflow dictionary (can't map to step arguments yet)
+  # argument type is either 'Input file' or 'parameter'.
   for x in cwl_dict['inputs']:
-    argument_type='parameter'  ## default.
-    if isinstance(x['type'],list):
-       if 'File' in x['type']:  #  e.g. type=[ 'File','null' ]  (SBG)
-           argument_type='Input file'
-       elif isinstance(x['type'][0],dict) and x['type'][0]['type']=='array' and x['type'][0]['items']=='File':  # e.g. type=[{'type':'array','items':'File'}] (SBG)
-           argument_type='Input file'
-    elif x['type']=='File':     # e.g. type='File'
-           argument_type='Input file'
-    elif isinstance(x['type'],dict) and x['type']['type']=='array' and x['type']['items']=='File':  # e.g. type={'type':'array','items':'File'}
-           argument_type='Input file'
+    argument_type = Is_Input_file_or_parameter (x['type'])
+    workflow['arguments'].append({'workflow_argument_name':x['id'].strip('#'), 'argument_type':argument_type})
 
-    workflow['arguments'].append({'workflow_argument_name':x['id'], 'argument_type':argument_type})
-
-
-  ## parsing global output files (map to step arguments)
+  ## parsing global output files and storing to the workflow dictionary and mapping to step arguments 
+  ## (the mapping (source) information is in the same field in cwl since the global output is usually sourced from a step output )
   for x in cwl_dict['outputs']:
     source = parse_source(x['source'][0])
-    workflow['arguments'].append( 
-       {'workflow_argument_name':x['id'], 
-        'argument_type':'Output file', 
-        'argument_mapping':[{ 'workflow_step':source['step'],
-                              'step_argument_name':source['arg'],
-                              'step_argument_type':'Output_file' 
-                           }]
-       })  ## for now we assume that 1. a global output argument has a single source and 2. that it is an output of some step.
-
+    map_workflow_output_argument_to_step_argument ( workflow, source, x['id'].strip('#'), source['step'], source['arg'] )
 
   ## parsing steps (map 1. global output files to step arguments and 2. between arguments between steps that are not globally defined)
-  #for x in cwl_dict['steps']:
-    #for y in x['inputs']:
-      #source = parse_source(y['source'][0])
-      #if source.step=='':  ## PROBLEM!!! how do we match b
-    #fo.write( json.dumps(x,indent=4)+"\n" )
+  for x in cwl_dict['steps']:
+    for y in x['inputs']:
+      if y.has_key('source'):
+        source = parse_source(y['source'][0])
+  
+        ## case 1: global argument is the source
+        if source['step']=='': 
+          map_step_argument_to_workflow_input_argument( workflow, source, x['id'].strip('#'), parse_source(y['id'])['arg'] )
+   
+        ## case 2: no global argument (just passing between steps)
+        else:
+          map_step_argument_to_another_step_argument( workflow, source, x['id'].strip('#'), parse_source(y['id'])['arg'] )
+       
+        ## case 3 (no global argument, no passing between steps) - we assume this doesn't exist.
+
      
   with open(workflow_metadata_json,'w') as fo:
     fo.write ( json.dumps(workflow,indent=4) + "\n")
