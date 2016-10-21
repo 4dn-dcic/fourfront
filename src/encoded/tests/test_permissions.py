5583d574d5cb2a8cc1f97aed66632d9ec2346a76
Andy Schroeder <andrew_schroeder@hms.harvard.edu>
2016-10-21 15:32:20 -0400

    FF-331 #comment Added tests for submitter not allowed to patch import_items fields and wrangler can

--- src/encoded/tests/test_permissions.py
+++ src/encoded/tests/test_permissions.py
@@ -524,7 +524,35 @@ def test_noone_can_view_deleted_lab_item(lab_item, submitter_testapp, wrangler_t
         app.get(res.json['@graph'][0]['@id'], status=403)
 
 
-def test_lab_submitter_can_edit_lab_item(lab, submitter_testapp, wrangler_testapp):
+def test_lab_submitter_can_edit_lab(lab, submitter_testapp, wrangler_testapp):
     res = submitter_testapp.get(lab['@id'])
     wrangler_testapp.patch_json(res.json['@id'], {'status': 'current'}, status=200)
     submitter_testapp.patch_json(res.json['@id'], {'city': 'My fair city'}, status=200)
+
+
+def test_statuses_that_lab_submitter_cannot_edit_lab(lab, submitter_testapp, wrangler_testapp):
+    statuses = ['deleted', 'revoked', 'inactive']
+    res = submitter_testapp.get(lab['@id'])
+    for status in statuses:
+        wrangler_testapp.patch_json(res.json['@id'], {'status': status}, status=200)
+        submitter_testapp.patch_json(res.json['@id'], {'city': 'My fair city'}, status=403)
+
+
+def test_lab_submitter_cannot_edit_lab_name_or_title(lab, submitter_testapp, wrangler_testapp):
+    res = submitter_testapp.get(lab['@id'])
+    wrangler_testapp.patch_json(res.json['@id'], {'status': 'current'}, status=200)
+    submitter_testapp.patch_json(res.json['@id'], {'title': 'Test Lab, HMS'}, status=422)
+    submitter_testapp.patch_json(res.json['@id'], {'name': 'test-lab'}, status=422)
+
+def test_wrangler_can_edit_lab_name_or_title(lab, submitter_testapp, wrangler_testapp):
+    statuses = ['deleted', 'revoked', 'inactive', 'current']
+    new_name = 'test-lab'
+    new_id = '/labs/test-lab/'
+    res = submitter_testapp.get(lab['@id'])
+    original_id = res.json['@id']
+    original_name = res.json['name']
+    for status in statuses:
+        wrangler_testapp.patch_json(original_id, {'status': status}, status=200)
+        wrangler_testapp.patch_json(original_id, {'title': 'Test Lab, HMS'}, status=200)
+        wrangler_testapp.patch_json(original_id, {'name': new_name}, status=200)
+        wrangler_testapp.patch_json(new_id, {'name': original_name}, status=200)
