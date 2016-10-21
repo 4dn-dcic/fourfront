92262fc229b01cbcf69a7ed42909e4f6ecf504b7
j1z0 <jeremy_johnson@hms.harvard.edu>
2016-10-21 11:00:39 -0400

    fixed strange exception in tests from time to time

--- src/encoded/authentication.py
+++ src/encoded/authentication.py
@@ -165,7 +165,8 @@ class Auth0AuthenticationPolicy(CallbackAuthenticationPolicy):
                          'unathenticated_userid', request)
                 return None
 
-        if user_info['email_verified'] is True:
+        # for strange unknow reasons sometimes user_info comes back empty
+        if user_info and user_info['email_verified'] is True:
             email = request._auth0_authenticted = user_info['email'].lower()
             return email
         else:
