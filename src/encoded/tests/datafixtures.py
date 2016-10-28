305dfbe781b9a439d2ebf08afd496ec9babac5f9
Koray Kırlı <koray_kirli@hms.harvard.edu>
2016-10-28 14:46:00 -0400

    FF-271 #comment added tests for id conversion

--- src/encoded/tests/datafixtures.py
+++ src/encoded/tests/datafixtures.py
@@ -338,7 +338,40 @@ def publication(testapp, lab, award):
         'uuid': '8312fc0c-b241-4cb2-9b01-1438910550ad',
         'award': award['@id'],
         'lab': lab['@id'],
-        'ID': "PMID:12345678",
+        'ID': "PMID:22955616",
+    }
+    return testapp.post_json('/publication', item).json['@graph'][0]
+
+
+@pytest.fixture
+def publication_PMID(testapp, lab, award):
+    item = {
+        'uuid': '8312fc0c-b241-4cb2-9b01-143891055000',
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'ID': "PMID:26673895",
+    }
+    return testapp.post_json('/publication', item).json['@graph'][0]
+
+
+@pytest.fixture
+def publication_doi_pubmed(testapp, lab, award):
+    item = {
+        'uuid': '8312fc0c-b241-4cb2-9b01-143891055001',
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'ID': "doi:10.1093/nar/gkv1046",
+    }
+    return testapp.post_json('/publication', item).json['@graph'][0]
+
+
+@pytest.fixture
+def publication_doi_biorxiv(testapp, lab, award):
+    item = {
+        'uuid': '8312fc0c-b241-4cb2-9b01-143891055002',
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'ID': "doi:10.1101/000091"
     }
     return testapp.post_json('/publication', item).json['@graph'][0]
 
