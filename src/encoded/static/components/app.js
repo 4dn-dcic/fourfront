ba1a8f1906fb57339c1e2be5905a4c3286afcdf1
Alex Balashov <alexander_balashov@hms.harvard.edu>
2016-10-28 21:06:43 -0400

    Webpack dependency update

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -697,9 +697,9 @@ var App = React.createClass({
         return (
             <html lang="en">
                 <head>
-                    <meta charSet="utf-8" />
-                    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                    <meta charSet="utf-8"/>
+                    <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                     <Title>{title}</Title>
                     {base ? <base href={base}/> : null}
                     <link rel="canonical" href={canonical} />
