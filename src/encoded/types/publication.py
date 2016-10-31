c1cb8e7241affeb9e6246a9daa183591fbb01a34
Koray Kırlı <koray_kirli@hms.harvard.edu>
2016-10-31 12:07:02 -0400

    FF-271 #comment date added

--- src/encoded/types/publication.py
+++ src/encoded/types/publication.py
@@ -11,6 +11,7 @@ def fetch_pubmed(PMID):
     abstract = ''
     author_list = []
     authors = ''
+    date = ''
     NIHe = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/"
     NIHw = "https://www.ncbi.nlm.nih.gov/pubmed/"
     url = NIHw + PMID
@@ -26,6 +27,9 @@ def fetch_pubmed(PMID):
         # grab the abstract
         if key_pb == 'AB':
             abstract = data_pb.strip()
+        # grab the date
+        if key_pb == 'DA':
+            date = data_pb.strip()
         # accumulate authors
         if key_pb == 'AU':
             author_list.append(data_pb.strip())
@@ -33,14 +37,15 @@ def fetch_pubmed(PMID):
         if key_pb == 'CN':
             author_list.append(data_pb.strip())
         authors = ', '.join(author_list)
-    return title, abstract, authors, url
+    return title, abstract, authors, url, date
 
 
 def fetch_biorxiv(url):
     title = ''
     abstract = ''
     authors = ''
-    return title, abstract, authors, url
+    date = ''
+    return title, abstract, authors, url, date
 
 
 def map_doi_pmid(doi):
@@ -83,25 +88,30 @@ class Publication(Item):
         abstract = ''
         authors = ''
         url = ''
+        date = ''
         p_id = properties['ID']
         # parse if id is from pubmed
-        if p_id.startswith('PMID'):
-            pubmed_id = p_id[5:]
-            title, abstract, authors, url = fetch_pubmed(pubmed_id)
-        # if id is doi, first check if it maps to pubmed id, else see where it goes
-        elif p_id.startswith('doi'):
-            doi_id = p_id[4:]
-            if map_doi_pmid(doi_id):
-                pubmed_id = map_doi_pmid(doi_id)
+        try:
+            if p_id.startswith('PMID'):
+                pubmed_id = p_id[5:]
                 title, abstract, authors, url = fetch_pubmed(pubmed_id)
-            # if it goes to biorxiv fetch from biorxiv
-            elif map_doi_biox(doi_id):
-                biox_url = map_doi_biox(doi_id)
-                title, abstract, authors, url = fetch_biorxiv(biox_url)
-            else:
-                pass
+            # if id is doi, first check if it maps to pubmed id, else see where it goes
+            elif p_id.startswith('doi'):
+                doi_id = p_id[4:]
+                if map_doi_pmid(doi_id):
+                    pubmed_id = map_doi_pmid(doi_id)
+                    title, abstract, authors, url = fetch_pubmed(pubmed_id)
+                # if it goes to biorxiv fetch from biorxiv
+                elif map_doi_biox(doi_id):
+                    biox_url = map_doi_biox(doi_id)
+                    title, abstract, authors, url = fetch_biorxiv(biox_url)
+                else:
+                    pass
+        except:
+            pass
         properties['title'] = title
         properties['abstract'] = abstract
         properties['authors'] = authors
         properties['url'] = url
+        properties['date'] = date
         super(Publication, self)._update(properties, sheets)
