31b82c61a7f3cc6d8a68080680954c4be89df93b
Andy Schroeder <andrew_schroeder@hms.harvard.edu>
2016-11-09 09:13:00 -0500

    FF-390 #comment Fixed typos in CASTx129 cell line names. Added test to make sure tier is getting added in _update function

--- src/encoded/tests/test_types_biosource.py
+++ src/encoded/tests/test_types_biosource.py
@@ -17,7 +17,7 @@ def F123_biosource(testapp):
     item = {
         "accession": "4DNSR000AAQ2",
         "biosource_type": "stem cell",
-        "cell_line": "F123-CASKx129",
+        "cell_line": "F123-CASTx129",
     }
     return testapp.post_json('/biosource', item).json['@graph'][0]
 
@@ -62,16 +62,26 @@ def test_biosource_update_termids(testapp, cell_lines):
                 assert 'cell_line_termid' not in cell
 
 
-# data from test/datafixtures
+def test_biosource_update_cell_line_tier(testapp, biosources):
+    acc2tier = {'4DNSR000AAQ1': 'Tier 1', '4DNSR000AAQ2': 'Tier 2'}
+    for biosource in biosources:
+        acc = biosource.get('accession')
+        print(acc)
+        if acc in acc2tier:
+            print(biosource)
+            assert biosource['cell_line_tier'] == acc2tier[acc]
+        else:
+            assert 'cell_line_tier' not in biosource
+
+
 def test_calculated_biosource_name(testapp, biosources):
-    print(biosources)
     for biosource in biosources:
         biotype = biosource['biosource_type']
         name = biosource['biosource_name']
         if biotype == 'immortalized cell line':
             assert name == 'GM12878'
         if biotype == 'stem cell':
-            assert name == 'F123-CASKx129'
+            assert name == 'F123-CASTx129'
         if biotype == 'tissue':
             assert name == 'lung'
         if biotype == 'whole organisms':
--- src/encoded/types/biosource.py
+++ src/encoded/types/biosource.py
@@ -31,7 +31,7 @@ class Biosource(Item):
             'GM12878': ['Tier 1', 'EFO_0002784'],
             'IMR-90': ['Tier 1', 'EFO_0001196'],
             'HFF-hTERT': ['Tier 1', None],
-            'F121-9-CASKx129': ['Tier 1', None],
+            'F121-9-CASTx129': ['Tier 1', None],
             'K562': ['Tier 2', 'EFO_0002067'],
             'HEK293': ['Tier 2', 'EFO_0001182'],
             'HAP-1': ['Tier 2', 'EFO_0007598'],
@@ -39,7 +39,7 @@ class Biosource(Item):
             'U2OS': ['Tier 2', 'EFO_0002869'],
             'RPE-hTERT': ['Tier 2', None],
             'WTC-11': ['Tier 2', None],
-            'F123-CASKx129': ['Tier 2', None],
+            'F123-CASTx129': ['Tier 2', None],
         }
         if 'cell_line' in properties:
             if properties['cell_line'] in name2info:
