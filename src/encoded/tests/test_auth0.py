354ce10fc7daea220f948ce9a794c6764462c6a5
j1z0 <jeremy_johnson@hms.harvard.edu>
2016-11-15 15:42:03 -0500

    improved test coverage for auth

--- src/encoded/tests/test_auth0.py
+++ src/encoded/tests/test_auth0.py
@@ -32,6 +32,30 @@ def auth0_access_token():
 
 
 @pytest.fixture(scope='session')
+def auth0_access_token_no_email():
+    creds = {
+        'connection': 'Username-Password-Authentication',
+        'scope': 'openid',
+        'client_id': 'DPxEwsZRnKDpk0VfVAxrStRKukN14ILB',
+        'grant_type': 'password',
+        'username': 'test1@test.com',
+        'password': 'Testing123',
+    }
+    url = 'https://hms-dbmi.auth0.com/oauth/ro'
+    try:
+        res = requests.post(url, json=creds)
+        res.raise_for_status()
+    except Exception as e:
+        pytest.skip("Error retrieving auth0 test user access token: %r" % e)
+
+    data = res.json()
+    if 'id_token' not in data:
+        pytest.skip("Missing 'id_token' in auth0 test user access token: %r" % data)
+
+    return data['id_token']
+
+
+@pytest.fixture(scope='session')
 def auth0_4dn_user_token(auth0_access_token):
     return {'id_token': auth0_access_token}
 
@@ -40,25 +64,29 @@ def auth0_4dn_user_token(auth0_access_token):
 def auth0_4dn_user_profile():
     return {'email': '4dndcic@gmail.com'}
 
+
 @pytest.fixture(scope='session')
 def headers(auth0_access_token):
     return {'Accept': 'applicatin/json', 'Content-Type': 'application/json', 'Authorization': 'Bearer ' +
      auth0_access_token}
 
+
 @pytest.fixture(scope='session')
 def fake_request(headers):
 
+
     class FakeRequest(object):
-        
+
         def __init__(self):
             self.headers = headers
             self.cookies = {}
 
     return FakeRequest()
 
-def test_get_jwt_gets_bearer_auth(fake_request, auth0_access_token):
+
+def test_get_jwt_gets_bearer_auth(fake_request):
     jwt = get_jwt(fake_request)
-    assert jwt == auth0_access_token
+    assert jwt == fake_request.headers['Authorization'][7:]
 
 
 def test_get_jwt_skips_basic_auth(fake_request):
@@ -78,6 +106,20 @@ def test_login_unknown_user(anontestapp, auth0_4dn_user_token):
     res = anontestapp.post_json('/login', auth0_4dn_user_token, status=403)
 
 
+def test_login_token_no_email(anontestapp, auth0_access_token_no_email, headers):
+    headers1 = headers.copy()
+    headers1['Authorization'] = 'Bearer ' + auth0_access_token_no_email
+    # Log in without headers
+    res = anontestapp.post_json('/login', headers=headers1, status=403)
+
+
+def test_invalid_login(anontestapp, headers):
+    headers1=headers.copy()
+    headers1['Authorization'] = 'Bearer invalid token'
+    # Log in without headers
+    res = anontestapp.post_json('/login', headers=headers1, status=403)
+
+
 def test_login_logout(testapp, anontestapp, headers,
                       auth0_4dn_user_profile,
                       auth0_4dn_user_token):
@@ -93,7 +135,7 @@ def test_login_logout(testapp, anontestapp, headers,
     testapp.post_json(url, item, status=201)
 
     # Log in
-    res = anontestapp.post_json('/login', auth0_4dn_user_token, headers=headers)
+    res = anontestapp.post_json('/login', headers=headers)
 
     assert res.json.get('auth.userid') is None
     assert 'id_token' in res.json
@@ -105,6 +147,32 @@ def test_login_logout(testapp, anontestapp, headers,
     assert 'auth.userid' not in res.json
     assert 'id_token' not in res.json
     assert 'user_actions' not in res.json
+
+
+
+def test_login_logout_redirect(testapp, anontestapp, headers,
+                      auth0_4dn_user_profile,
+                      auth0_4dn_user_token):
+
+    # Create a user with the persona email
+    url = '/users/'
+    email = auth0_4dn_user_profile['email']
+    item = {
+        'email': email,
+        'first_name': 'Auth0',
+        'last_name': 'Test User',
+    }
+    testapp.post_json(url, item, status=201)
+
+    # Log in
+    res = anontestapp.post_json('/login', headers=headers)
+
+    assert res.json.get('auth.userid') is None
+    assert 'id_token' in res.json
+    assert 'user_actions' in res.json
+
+    # Log out
+    res = anontestapp.get('/logout?redirect=True', status=302)
 
 
 def test_jwt_is_stateless_so_doesnt_actually_need_login(testapp, anontestapp, auth0_4dn_user_token,
@@ -143,6 +211,14 @@ def test_jwt_works_without_keys(testapp, anontestapp, auth0_4dn_user_token,
 
     anontestapp.app.registry.settings['auth0.secret'] = old_key
     assert '@id' in res2.json['@graph'][0]
+
+
+
+def test_impersonate_invalid_user(anontestapp, admin):
+    anontestapp.post_json(
+        '/impersonate-user', {'userid': 'not@here.usr'},
+        extra_environ={'REMOTE_USER':
+                       str(admin['email'])}, status=422)
 
 
 def test_impersonate_user(anontestapp, admin, submitter):
