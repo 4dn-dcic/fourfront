c12e4d726bd277d7178f9ec723e45d69e54b4d82
Koray Kırlı <koray_kirli@hms.harvard.edu>
2016-11-22 15:51:46 -0500

    FF-136 #comment added update function to exp set rep

--- src/encoded/types/experiment_set.py
+++ src/encoded/types/experiment_set.py
@@ -2,6 +2,7 @@
 
 from snovault import (
     collection,
+    calculated_property,
     load_schema,
 )
 from .base import (
@@ -51,17 +52,22 @@ class ExperimentSetReplicate(Item):
     item_type = 'experiment_set_replicate'
     schema = load_schema('encoded:schemas/experiment_set_replicate.json')
     name_key = "accession"
-    embedded = ["replicate_exps.replicate_exp",
-                "replicate_exps.replicate_exp.protocol",
-                "replicate_exps.replicate_exp.protocol_variation",
-                "replicate_exps.replicate_exp.lab",
-                "replicate_exps.replicate_exp.award",
-                "replicate_exps.replicate_exp.biosample",
-                "replicate_exps.replicate_exp.biosample.biosource",
-                "replicate_exps.replicate_exp.biosample.modifications",
-                "replicate_exps.replicate_exp.biosample.treatments",
-                "replicate_exps.replicate_exp.biosample.biosource.individual.organism",
-                "replicate_exps.replicate_exp.files",
-                "replicate_exps.replicate_exp.filesets",
-                "replicate_exps.replicate_exp.filesets.files_in_set",
-                "replicate_exps.replicate_exp.digestion_enzyme"]
+    embedded = ["experiments_in_set",
+                "experiments_in_set.protocol",
+                "experiments_in_set.protocol_variation",
+                "experiments_in_set.lab",
+                "experiments_in_set.award",
+                "experiments_in_set.biosample",
+                "experiments_in_set.biosample.biosource",
+                "experiments_in_set.biosample.modifications",
+                "experiments_in_set.biosample.treatments",
+                "experiments_in_set.biosample.biosource.individual.organism",
+                "experiments_in_set.files",
+                "experiments_in_set.filesets",
+                "experiments_in_set.filesets.files_in_set",
+                "experiments_in_set.digestion_enzyme"]
+
+    def _update(self, properties, sheets=None):
+        all_experiments = [exp['replicate_exp'] for exp in properties['replicate_exps']]
+        properties['experiments_in_set'] = all_experiments
+        super(ExperimentSetReplicate, self)._update(properties, sheets)
