5e50d0c17c2348048a7961de2c4e3f267f303ef6
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-01-18 14:30:42 -0500

    Re-added the /facets/ route to search.py, which is used in exp set view.

--- src/encoded/search.py
+++ src/encoded/search.py
@@ -16,7 +16,7 @@ from collections import OrderedDict
 def includeme(config):
     config.add_route('search', '/search{slash:/?}')
     config.add_route('browse', '/browse{slash:/?}')
-    # config.add_route('available_facets', '/facets{slash:/?}')
+    config.add_route('available_facets', '/facets{slash:/?}')
     config.scan(__name__)
 
 sanitize_search_string_re = re.compile(r'[\\\+\-\&\|\!\(\)\{\}\[\]\^\~\:\/\\\*\?]')
@@ -150,6 +150,45 @@ def collection_view(context, request):
     Simply use search results for collections views (e.g./biosamples/)
     """
     return search(context, request, context.type_info.name)
+
+
+@view_config(route_name='available_facets', request_method='GET', permission='search', renderer='json')
+def get_available_facets(context, request):
+    """
+    Method to get available facets for a content/data type; built off of search() without querying Elasticsearch.
+    Unlike in search(), due to lack of ES query, does not return possible terms nor counts of experiments matching the terms.
+    """
+
+    requestTypes = request.params.getall('type')
+    if len(requestTypes) == 0:
+        doc_types = ['Item']
+    else:
+        doc_types = [requestTypes[0]]
+
+    types = request.registry[TYPES]
+
+    # Normalize to item_type
+    try:
+        doc_types = sorted({types[name].name for name in doc_types})
+    except KeyError:
+        # Check for invalid types
+        bad_types = [t for t in doc_types if t not in types]
+        msg = "Invalid type: {}".format(', '.join(bad_types))
+        raise HTTPBadRequest(explanation=msg)
+
+    facets = [] # [('type', {'title': 'Data Type'}),]
+    if len(doc_types) == 1 and 'facets' in types[doc_types[0]].schema:
+        facets.extend(types[doc_types[0]].schema['facets'].items())
+
+    ### Mini version of format_facets
+    result = []
+    for field, facet in facets:
+        result.append({
+            'field': field,
+            'title': facet.get('title', field)
+        })
+
+    return result
 
 
 def normalize_query(request):
