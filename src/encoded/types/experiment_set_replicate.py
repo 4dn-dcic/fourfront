24ab43f9d4fa45d2e11a60a79bb8a9af0b99ad61
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-01-19 16:52:45 -0500

    Broke out experiment_set_replicate into its own type file because it was overwriting the item_page view.

--- src/encoded/types/experiment_set_replicate.py
+++ src/encoded/types/experiment_set_replicate.py
@@ -14,19 +14,20 @@ from snovault.resource_views import item_view_page
 from snovault.calculated import calculate_properties
 
 @collection(
-    name='experiment-sets',
+    name='experiment-set-replicates',
     unique_key='accession',
     properties={
-        'title': 'Experiment Sets',
-        'description': 'Listing Experiment Sets',
+        'title': 'Replicate Experiment Sets',
+        'description': 'Experiment set covering biological and technical experiments',
     })
-class ExperimentSet(Item):
-    """The experiment set class."""
+class ExperimentSetReplicate(Item):
+    """The experiment set class for replicate experiments."""
 
-    item_type = 'experiment_set'
-    schema = load_schema('encoded:schemas/experiment_set.json')
+    item_type = 'experiment_set_replicate'
+    schema = load_schema('encoded:schemas/experiment_set_replicate.json')
     name_key = "accession"
-    embedded = ["award",
+    embedded = ["replicate_exps.replicate_exp",
+                "award",
                 "lab",
                 "experiments_in_set",
                 "experiments_in_set.protocol",
@@ -41,11 +42,17 @@ class ExperimentSet(Item):
                 "experiments_in_set.files",
                 "experiments_in_set.filesets",
                 "experiments_in_set.filesets.files_in_set",
-                "experiments_in_set.digestion_enzyme"]
+                "experiments_in_set.digestion_enzyme",
+                "replicate_exps.replicate_exp"]
+
+    def _update(self, properties, sheets=None):
+        all_experiments = [exp['replicate_exp'] for exp in properties['replicate_exps']]
+        properties['experiments_in_set'] = all_experiments
+        super(ExperimentSetReplicate, self)._update(properties, sheets)
 
 # Use the the page view as default for experiment sets. This means that embedding
 # IS relevant with regard to this specific object pages
-@view_config(context=ExperimentSet, permission='view', request_method='GET', name='page')
+@view_config(context=ExperimentSetReplicate, permission='view', request_method='GET', name='page')
 def item_page_view(context, request):
     """Return the frame=page view rather than object view by default."""
     return item_view_page(context, request)
