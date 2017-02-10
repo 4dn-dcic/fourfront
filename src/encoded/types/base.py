924b5bfa16fde1a51714dff9efc5f27313480dc2
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-02-10 14:13:45 -0500

    Added item_view_page changes back in (originally in FF-523)

--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -214,15 +214,14 @@ class Item(snovault.Item):
 @view_config(context=Item, permission='view', request_method='GET', name='page')
 def item_page_view(context, request):
     """Return the frame=embedded view with @ids for objects not embedded."""
-    properties_emb = item_view_page_object(context, request)
-    # properties_emb, properties_obj = item_view_page_object(context, request)
-    # for key, val in properties_emb.items():
-    #     if isinstance(val, dict) and '@id' not in val.keys() and key in properties_obj:
-    #         properties_emb[key]['@id'] = properties_obj[key]
-    #     elif isinstance(val, list):
-    #         for i in range(len(val)):
-    #             if isinstance(val, dict) and '@id' not in val.keys() and key in properties_obj:
-    #                 properties_emb[key][i]['@id'] = properties_obj[key][i]
+    properties_emb, properties_obj = item_view_page_object(context, request)
+    for key, val in properties_emb.items():
+        if isinstance(val, dict) and '@id' not in val.keys() and key in properties_obj:
+            properties_emb[key]['@id'] = properties_obj[key]
+        elif isinstance(val, list):
+            for i in range(len(val)):
+                if isinstance(val, dict) and '@id' not in val.keys() and key in properties_obj:
+                    properties_emb[key][i]['@id'] = properties_obj[key][i]
     return properties_emb
 
 class SharedItem(Item):
