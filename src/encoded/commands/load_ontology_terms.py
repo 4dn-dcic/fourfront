cd7586d1bc318b93bbc72147f4bc30ec990cbcef
aschroed <andrew_schroeder@hms.harvard.edu>
2017-03-29 09:15:32 -0400

    Restored old load_data.py and changed load_ontology_terms.py to use load_data mechanism

--- src/encoded/commands/load_ontology_terms.py
+++ src/encoded/commands/load_ontology_terms.py
@@ -1,71 +1,47 @@
 #!/usr/bin/env python3
-
-import os
 import argparse
-import json
+import logging
+
+from pyramid.path import DottedNameResolver
+from pyramid.paster import get_app
+from encoded import configure_dbsession
+import sys
+import os
 from datetime import datetime
-from wranglertools import fdnDCIC
 
 
-def getArgs():  # pragma: no cover
-    parser = argparse.ArgumentParser(
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-    )
-
-    parser.add_argument('infile',
-                        help="the datafile containing object data to import")
-    parser.add_argument('--key',
-                        default='default',
-                        help="The keypair identifier from the keyfile.  \
-                        Default is --key=default")
-    parser.add_argument('--keyfile',
-                        default=os.path.expanduser("~/keypairs.json"),
-                        help="The keypair file.  Default is --keyfile=%s" %
-                             (os.path.expanduser("~/keypairs.json")))
-    args = parser.parse_args()
-    return args
+logger = logging.getLogger(__name__)
+EPILOG = __doc__
 
 
-def main():  # pragma: no cover
+def main():
     start = datetime.now()
     print(str(start))
-    args = getArgs()
-    key = fdnDCIC.FDN_Key(args.keyfile, args.key)
-    if key.error:
-        sys.exit(1)
-    connection = fdnDCIC.FDN_Connection(key)
+    logging.basicConfig()
+    # Loading app will have configured from config file. Reconfigure here:
+    logging.getLogger('encoded').setLevel(logging.DEBUG)
 
-    phase2 = {}
-    # assumes a single line corresponds to json for single term
-    with open(args.infile) as terms:
-        for t in terms:
-            parents = None
-            term = json.loads(t)
-            tid = '/ontology-terms/' + term['term_id']
-            # look for parents and remove for phase 2 loading if they are there
-            if 'parents' in term:
-                parents = term['parents']
-                del term['parents']
+    parser = argparse.ArgumentParser(
+        description="Load Test Data", epilog=EPILOG,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument('--app-name', help="Pyramid app name in configfile")
+    parser.add_argument('config_uri', help="path to configfile")
+    parser.add_argument('--access-key', help="store local or copy to s3, will generate and store access key for admin user")
+    args = parser.parse_args()
 
-            dbterm = fdnDCIC.get_FDN(tid, connection)
-            if 'OntologyTerm' in dbterm['@type']:
-                e = fdnDCIC.patch_FDN(dbterm["uuid"], connection, term)
-            else:
-                e = fdnDCIC.new_FDN(connection, 'OntologyTerm', term)
-            if e and parents:
-                phase2[e['@graph'][0]['uuid']] = parents
+    # get the pyramids app
+    app = get_app(args.config_uri, args.app_name)
+    # create db schema
+    configure_dbsession(app)
 
-    print("START LOADING PHASE2 at ", str(datetime.now()))
-    for tid, parents in phase2.items():
-        puuids = []
-        for p in parents:
-            res = fdnDCIC.get_FDN('/ontology-terms/' + p, connection)
-            if res.get('uuid', None):
-                puuids.append(res['uuid'])
-        e = fdnDCIC.patch_FDN(tid, connection, {'parents': puuids})
+    load_term_data = 'encoded.loadxl:load_ontology_terms'
+    print("****** load test data is %s" % (load_term_data))
+    load_test_data = DottedNameResolver().resolve(load_term_data)
+    load_term_data(app)
     end = datetime.now()
     print("FINISHED - START: ", str(start), "\tEND: ", str(end))
 
 
-if __name__ == '__main__':
-        main()
+if __name__ == "__main__":
+    main()
