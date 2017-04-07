c3cd77b6c1687937e65beeccac444e64db9dc927
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-04-07 08:52:46 -0400

    re-implemented test_download.py

--- src/encoded/tests/test_download.py
+++ src/encoded/tests/test_download.py
@@ -22,7 +22,11 @@ def testing_download(testapp):
         'attachment': {
             'download': 'red-dot.png',
             'href': RED_DOT,
-        }
+        },
+        'attachment2': {
+            'download': 'blue-dot.png',
+            'href': BLUE_DOT,
+        },
     }
     res = testapp.post_json(url, item, status=201)
     return res.location
@@ -31,6 +35,7 @@ def testing_download(testapp):
 def test_download_create(testapp, testing_download):
     res = testapp.get(testing_download)
     attachment = res.json['attachment']
+    attachment2 = res.json['attachment2']
 
     assert attachment['href'] == '@@download/attachment/red-dot.png'
     assert attachment['type'] == 'image/png'
@@ -42,6 +47,16 @@ def test_download_create(testapp, testing_download):
     assert res.content_type == 'image/png'
     assert res.body == b64decode(RED_DOT.split(',', 1)[1])
 
+    assert attachment2['href'] == '@@download/attachment2/blue-dot.png'
+    assert attachment2['type'] == 'image/png'
+    assert attachment2['width'] == 10
+    assert attachment2['height'] == 10
+    assert attachment2['md5sum'] == '013f03aa088adb19aa226c3439bda179'
+    url = testing_download + '/' + attachment2['href']
+    res = testapp.get(url)
+    assert res.content_type == 'image/png'
+    assert res.body == b64decode(BLUE_DOT.split(',', 1)[1])
+
 
 def test_download_update(testapp, testing_download):
     from base64 import b64decode
@@ -50,16 +65,27 @@ def test_download_update(testapp, testing_download):
             'download': 'blue-dot.png',
             'href': BLUE_DOT,
         },
+        'attachment2': {
+            'download': 'red-dot.png',
+            'href': RED_DOT,
+        },
     }
     testapp.put_json(testing_download, item, status=200)
     res = testapp.get(testing_download)
     attachment = res.json['attachment']
+    attachment2 = res.json['attachment2']
 
     assert attachment['href'] == '@@download/attachment/blue-dot.png'
     url = testing_download + '/' + attachment['href']
     res = testapp.get(url)
     assert res.content_type == 'image/png'
     assert res.body == b64decode(BLUE_DOT.split(',', 1)[1])
+
+    assert attachment2['href'] == '@@download/attachment2/red-dot.png'
+    url = testing_download + '/' + attachment2['href']
+    res = testapp.get(url)
+    assert res.content_type == 'image/png'
+    assert res.body == b64decode(RED_DOT.split(',', 1)[1])
 
 
 def test_download_update_no_change(testapp, testing_download):
@@ -68,12 +94,18 @@ def test_download_update_no_change(testapp, testing_download):
             'download': 'red-dot.png',
             'href': '@@download/attachment/red-dot.png',
         },
+        'attachment2': {
+            'download': 'blue-dot.png',
+            'href': '@@download/attachment2/blue-dot.png',
+        },
     }
     testapp.put_json(testing_download, item, status=200)
 
     res = testapp.get(testing_download)
     attachment = res.json['attachment']
+    attachment2 = res.json['attachment2']
     assert attachment['href'] == '@@download/attachment/red-dot.png'
+    assert attachment2['href'] == '@@download/attachment2/blue-dot.png'
 
 
 def test_download_update_one(testapp, testing_download):
@@ -82,14 +114,25 @@ def test_download_update_one(testapp, testing_download):
             'download': 'red-dot.png',
             'href': '@@download/attachment/red-dot.png',
         },
+        'attachment2': {
+            'download': 'red-dot.png',
+            'href': RED_DOT,
+        },
     }
     testapp.put_json(testing_download, item, status=200)
 
     res = testapp.get(testing_download)
     attachment = res.json['attachment']
+    attachment2 = res.json['attachment2']
 
     assert attachment['href'] == '@@download/attachment/red-dot.png'
     url = testing_download + '/' + attachment['href']
+    res = testapp.get(url)
+    assert res.content_type == 'image/png'
+    assert res.body == b64decode(RED_DOT.split(',', 1)[1])
+
+    assert attachment2['href'] == '@@download/attachment2/red-dot.png'
+    url = testing_download + '/' + attachment2['href']
     res = testapp.get(url)
     assert res.content_type == 'image/png'
     assert res.body == b64decode(RED_DOT.split(',', 1)[1])
@@ -106,6 +149,10 @@ def test_download_remove_one(testapp, testing_download):
 
     res = testapp.get(testing_download)
     assert 'attachment' in res.json
+    assert 'attachment2' not in res.json
+
+    url = testing_download + '/@@download/attachment2/red-dot.png'
+    res = testapp.get(url, status=404)
 
 
 @pytest.mark.parametrize(
