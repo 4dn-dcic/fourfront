d4f3d4f51c38211f90b231ba39400c1c91eccd2f
aschroed <andrew_schroeder@hms.harvard.edu>
2017-04-13 10:42:20 -0400

    Changed kinda_unique_key and identifying property from preferred_name to term_name; added display_title properties to OntologyTerm and Ontology

--- src/encoded/types/ontology.py
+++ src/encoded/types/ontology.py
@@ -1,5 +1,6 @@
 """Collection for the Ontology and OntologyTerms objects."""
 from snovault import (
+    calculated_property,
     collection,
     load_schema,
 )
@@ -11,7 +12,7 @@ from .base import (
 @collection(
     name='ontology-terms',
     unique_key='ontology_term:term_id',
-    kinda_unique_key = 'preferred_name',
+    kinda_unique_key='term_name',
     properties={
         'title': 'Ontology Terms',
         'description': 'Listing of Ontology Terms',
@@ -24,6 +25,17 @@ class OntologyTerm(Item):
     embedded = []
     name_key = 'term_id'
 
+    @calculated_property(schema={
+        "title": "Display Title",
+        "description": "A calculated title for every object in 4DN",
+        "type": "string"
+    })
+    def display_title(self, request, term_id, term_name=None):
+        if term_name is not None:
+            return term_name
+        return term_id
+
+
 @collection(
     name='ontologys',
     unique_key='ontology:ontology_prefix',
@@ -32,11 +44,19 @@ class OntologyTerm(Item):
         'description': 'Listing of Ontologies',
     })
 class Ontology(Item):
-        """The Ontology class that holds info on an ontology."""
+    """The Ontology class that holds info on an ontology."""
 
-        item_type = 'ontology'
-        schema = load_schema('encoded:schemas/ontology.json')
-        embedded = [
-           'synonym_terms',
-           'definition_terms',
-        ]
+    item_type = 'ontology'
+    schema = load_schema('encoded:schemas/ontology.json')
+    embedded = [
+        'synonym_terms',
+        'definition_terms',
+    ]
+
+    @calculated_property(schema={
+        "title": "Display Title",
+        "description": "A calculated title for every object in 4DN",
+        "type": "string"
+    })
+    def display_title(self, request, ontology_name):
+        return ontology_name
