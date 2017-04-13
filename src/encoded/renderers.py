ae84c1b7632e2bc06750d7c7440fc8908c3ee989
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-04-13 11:14:42 -0400

    Hide testwarning on scroll

--- src/encoded/renderers.py
+++ src/encoded/renderers.py
@@ -361,6 +361,6 @@ debug_page_or_json = SubprocessTween(
     should_transform=should_transform_callable,
     after_transform=after_transform,
     reload_process=reload_process,
-    args=['node', resource_filename(__name__, 'static/build/renderer.js')],
+    args=['node', resource_filename(__name__, 'static/server.js')],
     env=node_env,
 )
