d5b27450e36255fa8f6eb8fedac11374c8bcd23d
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-04-24 11:19:44 -0400

    More analytics uuid aliasing

--- src/encoded/static/components/util/analytics.js
+++ src/encoded/static/components/util/analytics.js
@@ -116,21 +116,20 @@ export function registerPageView(href = null, context = {}, currentExpSetFilters
         var newPathName = null;
 
         // Remove Accession, UUID, and Name from URL and save it to 'name' dimension instead.
-        if (typeof context.accession === 'string'){
+        if (typeof context.accession === 'string' && pathParts[1] === context.accession){
             // We gots an accessionable Item. Lets remove its Accession from the path to get nicer Behavior Flows in GA.
             // And let product tracking / Shopping Behavior handle Item details.
-            if (pathParts[1] === context.accession){
-                pathParts[1] = 'accession';
-                newPathName = '/' + pathParts.join('/') + '/';
-                opts[GADimensionMap.name] = context.accession;
-            }
-        } else if ((context.last_name && context.first_name) || (context['@type'] && context['@type'].indexOf('User') > -1)){
+            pathParts[1] = 'accession';
+            newPathName = '/' + pathParts.join('/') + '/';
+            opts[GADimensionMap.name] = context.accession;
+        } else if (
+            (context.last_name && context.first_name) || (context['@type'] && context['@type'].indexOf('User') > -1) &&
+            (pathParts[0] === 'users' && (context.uuid && pathParts[1] === context.uuid))
+        ){
             // Save User name.
-            if (pathParts[0] === 'users' && (context.uuid && pathParts[1] === context.uuid)){
-                pathParts[1] = 'uuid';
-                newPathName = '/' + pathParts.join('/') + '/';
-                opts[GADimensionMap.name] = context.title || context.uuid;
-            }
+            pathParts[1] = 'uuid';
+            newPathName = '/' + pathParts.join('/') + '/';
+            opts[GADimensionMap.name] = context.title || context.uuid;
         } else if (typeof context.uuid === 'string' && pathParts[1] === context.uuid){
             pathParts[1] = 'uuid';
             newPathName = '/' + pathParts.join('/') + '/';
