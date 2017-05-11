3e0f463b96f6595c52c74cd951e0c8a3af2a73c2
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-05-11 12:37:51 -0400

    Parse nodes from step->outputs[]->step->outputs[]->step instead of step1->step2->step3->step4

--- src/encoded/static/components/viz/Workflow.js
+++ src/encoded/static/components/viz/Workflow.js
@@ -14,7 +14,7 @@ import { ItemDetailList } from './../item-pages/components';
 import { getTitleStringFromContext } from './../item-pages/item';
 
 
-export function parseAnalysisSteps(analysis_steps){
+export function parseAnalysisSteps(analysis_steps, parsingMethod = 'path'){
 
     /**** Outputs ****/
 
@@ -24,14 +24,30 @@ export function parseAnalysisSteps(analysis_steps){
 
     /**** Functions ****/
 
+    function stepNodeID   (step) { return step.uuid || step['@id'] || step.link_id; }
+    function stepNodeName (step) { return step.display_title || step.title || step['@id']; }
+
+
     function generateStepNode(step, column){
         return {
-            id : step.uuid,
+            id : stepNodeID(step),
             type : 'step',
-            name : step.display_title || step.title || step['@id'],
+            name : stepNodeName(step),
             meta : _.omit(step, 'inputs', 'outputs'),
             description : step.description,
             column : column
+        };
+    }
+
+
+    function generateOutputNode(stepOutput, column, outputOfNode){
+        return {
+            column      : column,
+            format      : stepOutput.target && stepOutput.target[0].type,
+            name        : stepOutput.name, 
+            type        : 'output',
+            meta        : _.omit(stepOutput, 'required', 'name'),
+            outputOf    : outputOfNode
         };
     }
 
@@ -43,17 +59,9 @@ export function parseAnalysisSteps(analysis_steps){
      * @param {Object} stepNode - Analysis Step Node Reference
      */
     function generateOutputNodes(step, column, stepNode){
-        var outputNodes = [];
-        step.outputs.forEach(function(stepOutput, j){
-            outputNodes.push({
-                column      : column,
-                format      : stepOutput.target && stepOutput.target[0].type,
-                name        : stepOutput.name, 
-                type        : 'output',
-                meta        : _.omit(stepOutput, 'required', 'name'),
-                outputOf    : stepNode
-            });
 
+        var outputNodes = step.outputs.map(function(stepOutput, j){
+            return generateOutputNode(stepOutput, column, stepNode);
         });
 
         outputNodes.forEach(function(n){
@@ -65,6 +73,7 @@ export function parseAnalysisSteps(analysis_steps){
         });
 
         nodes = nodes.concat(outputNodes);
+        return outputNodes;
     }
 
     function generateInputNode(stepInput, column, inputOfNode){
@@ -78,6 +87,7 @@ export function parseAnalysisSteps(analysis_steps){
             required    : stepInput.required || false,
         };
     }
+
 
     /**
      * Find existing or generate new input nodes for each input in step.inputs and
@@ -93,6 +103,9 @@ export function parseAnalysisSteps(analysis_steps){
             return false;
         });
 
+        var inputNodesMatched = [];
+        var inputNodesCreated = [];
+
         step.inputs.forEach(function(fullStepInput){
             if (!Array.isArray(fullStepInput.source)) return;
 
@@ -104,13 +117,16 @@ export function parseAnalysisSteps(analysis_steps){
                 return false;
             });
 
-            // Else create new one.
-            if (!inputNode){
+            
+            if (inputNode){ // Cool, associate this 1.
+                inputNodesMatched.push(inputNode);
+            } else { // Else create new one.
                 inputNode = generateInputNode(fullStepInput, column, stepNode);
                 nodes.push(inputNode);
+                inputNodesCreated.push(inputNode);
             }
 
-            // Finally, attach edge.
+            // Finally, attach edge. Add to return arr (inputNodesUsedOrCreated).
             if (inputNode){
                 edges.push({
                     'source' : inputNode,
@@ -120,18 +136,98 @@ export function parseAnalysisSteps(analysis_steps){
             }
 
         });
+
+        return { 'created' : inputNodesCreated, 'matched' : inputNodesMatched };
+    }
+
+    var processedSteps = {}; // Just for optimization
+
+    function findNextStepsFromOutputs(outputNodes){
+        
+        var remainingSteps = analysis_steps.filter(function(step){
+            if (typeof processedSteps[stepNodeID(step) || stepNodeName(step)] !== 'undefined'){
+                return false;
+            }
+            return true;
+        });
+
+        var nextSteps = new Set();
+
+        outputNodes.forEach(function(oN){
+            if (oN.meta && Array.isArray(oN.meta.target)){
+                var target = oN.meta.target[1] || oN.meta.target[0]; // Optimization. If input of another step, it'll be second item in array if a Workflow Output target also present.
+                if (typeof target.step === 'string'){
+                    var matchedStep = _.findWhere(remainingSteps, { 'uuid' : target.step });
+                    if (matchedStep) {
+                        nextSteps.add(matchedStep);
+                    }
+                }
+            }
+        });
+
+        return [...nextSteps];
+    }
+
+    /**
+     * Recursive function which generates step, output, and input nodes (if input not matched to existing node),
+     * starting at param 'step' and tracing path, splitting if needs to, of outputNodes->meta.target.step->outputNodes->meta.target.step->...
+     * 
+     * @param {Object} step - AnalysisStep object representation, extended with additional 'inputs' and 'outputs' arrays. 
+     * @param {number} [level=0] - Step level or depth. Used for nodes' column (precursor to X axis position).
+     */
+    function processStepInPath(step, level = 0){
+        var stepNode = generateStepNode(step, (level + 1) * 2 - 1);
+
+        var inputNodesUsedOrCreated  = generateInputNodes(step, (level + 1) * 2 - 2, stepNode);
+        var outputNodesCreated = generateOutputNodes(step, (level + 1) * 2, stepNode);
+
+        nodes.push(stepNode);
+
+        processedSteps[stepNode.id || stepNode.name] = step;
+
+        findNextStepsFromOutputs(outputNodesCreated).forEach(function(nextStep){
+            processStepInPath(nextStep, level + 1);
+        });
     }
 
     /***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****
      *  Process each Analysis Step as a node. Inputs & output nodes placed in alternating column.  *
      ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** ***** *****/
 
-    analysis_steps.forEach(function(step, i){
-        var stepNode = generateStepNode(step, (i + 1) * 2 - 1);
-        generateInputNodes(step, (i + 1) * 2 - 2, stepNode);
-        generateOutputNodes(step, (i + 1) * 2, stepNode);
-        nodes.push(stepNode);
-    });
+    /**
+     * Process step[]->output[]->step[]->output[] graph.
+     * Start at first analysis step in array.
+     */
+    function processStepOutputPath(){
+        if (Array.isArray(analysis_steps) && analysis_steps.length > 0){
+            processStepInPath(analysis_steps[0]);
+        }
+    }
+
+    /**
+     * Process steps linearly, by their order in list.
+     * @deprecated
+     * @param {Object} step - List of AnalysisSteps, each extended with 'inputs' and 'outputs' arrays.
+     * @param {number} index - Step index.
+     */
+    function processStepsLinearly(){
+        analysis_steps.forEach(function(step, i){
+            var stepNode = generateStepNode(step, (i + 1) * 2 - 1);
+            generateInputNodes(step, (i + 1) * 2 - 2, stepNode);
+            generateOutputNodes(step, (i + 1) * 2, stepNode);
+            nodes.push(stepNode);
+        });
+    }
+
+
+
+    /** Entry Point **/
+    
+    if (parsingMethod === 'linear'){
+        processStepsLinearly();
+    } else {
+        processStepOutputPath();
+    }
 
     return {
         'nodes' : nodes,
@@ -139,6 +235,7 @@ export function parseAnalysisSteps(analysis_steps){
     };
 
 }
+
 
 export function parseBasicIOAnalysisSteps(analysis_steps, workflowItem){
 
