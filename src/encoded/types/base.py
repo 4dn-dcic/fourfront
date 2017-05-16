d714e7e34e9ee4d87a9b78c3011eed9b08772178
aschroed <andrew_schroeder@hms.harvard.edu>
2017-05-16 14:03:49 -0400

    small variable name tweak

--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -117,7 +117,7 @@ def get_item_if_you_can(request, value, itype=None):
     except AttributeError:
         svalue = str(value)
         if not svalue.startswith('/'):
-            svalue = '/' + value
+            svalue = '/' + svalue
         item = request.embed(svalue, '@@object')
         try:
             item.get('uuid')
