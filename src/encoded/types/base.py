6f173ddbe6b31cbe140cda9afcc7ebee7eddb1a1
aschroed <andrew_schroeder@hms.harvard.edu>
2017-05-16 10:11:54 -0400

    Added pdbs for debugging

--- src/encoded/types/base.py
+++ src/encoded/types/base.py
@@ -110,7 +110,7 @@ def paths_filtered_by_status(request, paths, exclude=('deleted', 'replaced'), in
 
 
 def get_item_if_you_can(request, value):
-    # import pdb; pdb.set_trace()
+    import pdb; pdb.set_trace()
     try:
         value.get('uuid')
         return value
