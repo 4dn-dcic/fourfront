de37c6727e1bdcfea498c79aa026f94514813d1d
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-05-30 12:51:34 -0400

    Doc/comment

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -760,9 +760,13 @@ export default class App extends React.Component {
         return true;
     }
 
-    // check this.state.isSubmitting to prompt user if navigating away
-    // from the submissions page
-    stayOnSubmissionsPage(href) {
+    /** 
+     * Check this.state.isSubmitting to prompt user if navigating away
+     * from the submissions page.
+     * 
+     * @param {string} [href] - Href we are navigating to (in case of navigate, confirmNavigation) or have just navigated to (in case of popState event).
+     */
+    stayOnSubmissionsPage(href = null) {
         // can override state in options
         // override with replace, which occurs on back button navigation
         if(this.state.isSubmitting){
