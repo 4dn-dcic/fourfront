1b179c4375036500a219d36f7b801119e556be54
aschroed <andrew_schroeder@hms.harvard.edu>
2017-06-19 17:34:08 -0400

    FF-794 #comment minor bug fixes for errors in pub upgrader and experiment calc prop key error

--- src/encoded/upgrade/publication.py
+++ src/encoded/upgrade/publication.py
@@ -4,6 +4,9 @@ from snovault import upgrade_step
 @upgrade_step('publication', '1', '2')
 def publication_1_2(value, system):
     if 'authors' in value:
-        author_list = value['authors'].split(',')
-        author_list = [a.strip() for a in author_list]
-        value['authors'] = author_list
+        try:
+            author_list = value['authors'].split(',')
+            author_list = [a.strip() for a in author_list]
+            value['authors'] = author_list
+        except:
+            pass
