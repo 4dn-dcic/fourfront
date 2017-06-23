b5c57080a470979785e01bd9a0d8499b269da110
Carl Vitzthum <carl.vitzthum@gmail.com>
2017-06-23 09:23:21 -0400

    Fixed test_add_default_embeds in test_embeds.py

--- src/encoded/tests/test_embedding.py
+++ src/encoded/tests/test_embedding.py
@@ -76,13 +76,18 @@ def test_embedded_uuids_experiment(experiment, replicate, library, biosample, or
     assert biosample['uuid'] in embedded_uuids
     assert organism['uuid'] in embedded_uuids
 
+
 @pytest.mark.parametrize('item_type', ORDER)
 def test_add_default_embeds(registry, item_type):
     """
-    Ensure
+    Ensure default embedding matches the schema for each object
     """
-    from snovault.fourfront_utils import add_default_embeds
+    from snovault.fourfront_utils import add_default_embeds, confirm_embed_with_schemas
     from snovault import TYPES
     type_info = registry[TYPES].by_item_type[item_type]
     schema = type_info.schema
     embeds = add_default_embeds(item_type, registry[TYPES], type_info.embedded, schema)
+    for embed in embeds:
+        split_embed = embed.strip().split('.')
+        is_valid, error = confirm_embed_with_schemas(item_type, registry[TYPES], split_embed, schema['properties'])
+        assert is_valid or error == None
