16c00b20d7d52e1dbde71c166aebc5c5d3a2eca4
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-07-25 15:24:25 -0400

    Code cleanliness

--- src/encoded/batch_download.py
+++ src/encoded/batch_download.py
@@ -188,10 +188,12 @@ def metadata_tsv(context, request):
     param_list = parse_qs(request.matchdict['search_params'])
 
     accession_triples = None # If conditions are met (equal number of accession per Item type), will be a list with tuples: (ExpSetAccession, ExpAccession, FileAccession)
-    if param_list.get('accession') is not None and \
-        param_list.get('experiments_in_set.accession') is not None and \
-        param_list.get('experiments_in_set.files.accession') is not None and \
-        len(param_list['accession']) == len(param_list['experiments_in_set.accession']) == len(param_list['experiments_in_set.files.accession']):
+    if (
+        param_list.get('accession') is not None and
+        param_list.get('experiments_in_set.accession') is not None and
+        param_list.get('experiments_in_set.files.accession') is not None and
+        len(param_list['accession']) == len(param_list['experiments_in_set.accession']) == len(param_list['experiments_in_set.files.accession'])
+    ):
         accession_triples = list(zip(param_list['accession'], param_list['experiments_in_set.accession'], param_list['experiments_in_set.files.accession']))
 
     if 'referrer' in param_list:
@@ -250,7 +252,6 @@ def metadata_tsv(context, request):
 
     def format_experiment_set(exp_set):
         exp_set_row_vals = {}
-
         for column in header:
             if not _tsv_mapping[column][0].startswith('experiments_in_set'):
                 exp_set_row_vals[column] = get_value_for_column(exp_set, column, 0)
@@ -274,7 +275,7 @@ def metadata_tsv(context, request):
             if _tsv_mapping[column][0].startswith('experiments_in_set.files'):
                 exp_row_vals[column] = get_value_for_column(f, column, 25)
 
-        all_row_vals = dict(exp_set_row_vals, **exp_row_vals, **f_row_vals)
+        all_row_vals = dict(exp_set_row_vals, **exp_row_vals, **f_row_vals) # Combine data from ExpSet, Exp, and File
 
         # If our File object (all_row_vals) has Replicate Numbers, make sure they are corrected.
         if all_row_vals.get('Set Bio Rep No') is not None or all_row_vals.get('Set Tec Rep No') is not None:
@@ -287,6 +288,7 @@ def metadata_tsv(context, request):
             return None
 
     data_rows = map(
+        # Convert object to list of values in same order defined in tsvMapping & header.
         lambda file_row_object: [ file_row_object[column] for column in header ],
         filter(
             lambda val: val is not None,
