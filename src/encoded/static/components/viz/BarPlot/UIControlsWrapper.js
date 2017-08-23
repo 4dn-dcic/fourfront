2f01aad561873ef6b34a4ca94302f014f7fd1085
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-08-23 15:10:36 -0400

    Changing BarPlotChart field options to reflect changes; handling look-up by ExpSet property (vs Exp property)

--- src/encoded/static/components/viz/BarPlot/UIControlsWrapper.js
+++ src/encoded/static/components/viz/BarPlot/UIControlsWrapper.js
@@ -36,17 +36,17 @@ export class UIControlsWrapper extends React.Component {
             { title : "Biosource", field : "experiments_in_set.biosample.biosource_summary" },
             { title : "Biosource Type", field : 'experiments_in_set.biosample.biosource.biosource_type' },
             { title : "Organism", field : "experiments_in_set.biosample.biosource.individual.organism.name" },
-            { title : "Project", field : "experiments_in_set.award.project" },
-            { title : "Lab", field : "experiments_in_set.lab.title" }
+            { title : "Project", field : "award.project" },
+            { title : "Lab", field : "lab.title" }
         ],
         'availableFields_Subdivision' : [
-            { title : "Project", field : "experiments_in_set.award.project" },
+            { title : "Project", field : "award.project" },
             { title : "Organism", field : "experiments_in_set.biosample.biosource.individual.organism.name" },
             { title : "Experiment Type", field : 'experiments_in_set.experiment_type' },
             { title : "Digestion Enzyme", field : "experiments_in_set.digestion_enzyme.name" },
             { title : "Biosource", field : "experiments_in_set.biosample.biosource_summary" },
             { title : "Biosource Type", field : 'experiments_in_set.biosample.biosource.biosource_type' },
-            { title : "Lab", field : "experiments_in_set.lab.title" }
+            { title : "Lab", field : "lab.title" }
         ],
         'legend' : false,
         'chartHeight' : 300
