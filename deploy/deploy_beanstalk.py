9f7bb5ae5af4b3c79870afe0ecd88eb4a06105ce
j1z0 <jeremy_johnson@hms.harvard.edu>
2017-10-24 12:51:34 -0400

    fix

--- deploy/deploy_beanstalk.py
+++ deploy/deploy_beanstalk.py
@@ -30,7 +30,7 @@ def merge(source, merge_to):
 
 
 def get_git_version():
-version = os.environ.get("TRAVIS_COMMIT","")[:7]
+    version = os.environ.get("TRAVIS_COMMIT","")[:7]
     if not version:
         version = subprocess.check_output(
             ['git', '-C', os.path.dirname(__file__), 'describe']).decode('utf-8').strip()
@@ -110,15 +110,19 @@ if __name__ == "__main__":
     merge_to = os.environ.get("tibanna_merge")
 
     if not args.prod:
-        print("not production")
-        ver = get_git_version()
-        # checkout correct branch
-        subprocess.check_output(
-            ['git', 'checkout', branch])
-        update_version(ver, branch)
-        if merge_to:
-            merge(branch, merge_to)
-            tag(ver)
+        try:
+            ver = get_git_version()
+            # checkout correct branch
+            subprocess.check_output(
+                ['git', 'checkout', branch])
+            update_version(ver, branch)
+            if merge_to:
+                merge(branch, merge_to)
+                tag(ver)
+        except Exception e:
+            # this can all go wrong if somebody pushes during the build
+            # or what not, in which case we just won't update the tag / merge
+            rasie(e)
         deploy()
     if args.prod:
         print("args production")
