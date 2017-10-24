27e870d0d41e8976b386ac27d9bf95591e9d8a94
j1z0 <jeremy_johnson@hms.harvard.edu>
2017-10-23 22:49:26 -0400

    for testing

--- deploy/deploy_beanstalk.py
+++ deploy/deploy_beanstalk.py
@@ -109,7 +109,7 @@ if __name__ == "__main__":
         subprocess.check_output(
             ['git', 'config', "--replace-all", "remote.origin.fetch",
              "'+refs/heads/*:refs/remotes/origin/*'"])
-        res = subprocess.check_output(['git', 'fetch', '--no-tags', '--depth', '200'])
+        res = subprocess.check_output(['git', 'fetch'])
         print(res)
         subprocess.check_output(
             ['git', 'checkout', branch])
