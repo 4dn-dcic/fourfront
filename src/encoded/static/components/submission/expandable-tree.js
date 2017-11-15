95127694df3adb7b08d3621985686d9a5d0c159d
Alex Balashov <alexander_balashov@hms.harvard.edu>
2017-11-15 18:54:27 -0500

    Ongoing

--- src/encoded/static/components/submission/expandable-tree.js
+++ src/encoded/static/components/submission/expandable-tree.js
@@ -40,15 +40,8 @@ export default class SubmissionTree extends React.Component {
         } = this.props;
         return(
             <div className="submission-view-navigation-tree">
-                <h3 className="form-section-heading mb-08">
-                        Navigation
-                        <InfoIcon children={infoTip} />
-                </h3>
-                <div className="submission-nav-tree" style={{'marginTop':'10px'}}>
-                    <div>
-                        <SubmissionLeaf {...others} keyIdx={0} open={true}/>
-                    </div>
-                </div>
+                <h3 className="form-section-heading mb-08">Navigation <InfoIcon children={infoTip} /></h3>
+                <SubmissionLeaf {...others} keyIdx={0} open />
             </div>
         );
     }
@@ -60,52 +53,54 @@ on, either change the currKey to that object's key if a custom object, or
 open that object's page in a new tab if a pre-existing or submitted object.
 */
 class SubmissionLeaf extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            'open': this.props.open || true
-        };
+
+    static defaultProps = {
+        'depth' : 0
     }
 
-    handleToggle = (e) => {
+    constructor(props){
+        super(props);
+        this.handleToggle = _.throttle(this.handleToggle.bind(this), 500, { 'trailing' : false });
+        this.handleClick = _.throttle(this.handleClick.bind(this), 500, { 'trailing' : false });
+        this.generateChild = this.generateChild.bind(this);
+        this.generatePlaceholder = this.generatePlaceholder.bind(this);
+        this.state = { 'open' : typeof this.props.open === 'boolean' ? this.props.open : true };
+    }
+
+    handleToggle(e){
         e.preventDefault();
         this.setState({'open': !this.state.open});
     }
 
-    generateChild = (childKey) => {
-        if(!isNaN(childKey)){
-            childKey = parseInt(childKey);
-        }
+    generateChild(childKey){
+        if(!isNaN(childKey)) childKey = parseInt(childKey);
+
         // replace key and hierarchy in props
-        const{
-            keyIdx,
-            hierarchy,
-            ...others
-        } = this.props;
         return(
             <SubmissionLeaf
+                {...this.props}
                 key={childKey}
-                {...others}
                 keyIdx={childKey}
-                open={true}
                 hierarchy={this.props.hierarchy[this.props.keyIdx]}
+                open
+                depth={this.props.depth + 1}
             />
         );
     }
 
-    /*
-    Generate placeholders in the SubmissionTree for every linkTo name and
-    create a SubmissionLeaf for each child object under its corresponding
-    placholder.
-    */
-    generatePlaceholder = (bookmark) =>{
-        var children = Object.keys(this.props.hierarchy[this.props.keyIdx]).map(function(childKey){
-            if(this.props.keyLinks[childKey] == bookmark){
-                return this.generateChild(childKey);
-            }else{
-                return null;
-            }
-        }.bind(this));
+    /**
+     * Generate placeholders in the SubmissionTree for every linkTo name and
+     * create a SubmissionLeaf for each child object under its corresponding
+     * placholder.
+     * 
+     * @param {string} bookmark - Name of the leaf/view we're on.
+     * @returns {JSX.Element} Visible leaf/branch-representing element.
+     */
+    generatePlaceholder(bookmark){
+        var children = _.map(
+            _.keys(this.props.hierarchy[this.props.keyIdx]),
+            (childKey) => this.props.keyLinks[childKey] === bookmark ? this.generateChild(childKey) : null
+        );
         var style = {
             'marginLeft': '-15px',
             'overflow': 'hidden',
@@ -119,46 +114,32 @@ class SubmissionLeaf extends React.Component{
             'padding':'4px 7px 0px 3px'
         };
         return(
-            <div key={bookmark} className="submission-nav-leaf">
-                <div className="clearfix" style={style}>
-                    <Button style={buttonStyle} bsSize="xsmall" className="icon-container pull-left" disabled/>
-                    <span>
-                        {bookmark}
-                    </span>
+            <div key={bookmark} className={"submission-nav-leaf linked-item-type-name leaf-depth-" + this.props.depth}>
+                <div className="clearfix inner-title" style={style}>
+                    <span>{ bookmark }</span>
                 </div>
-                <div>
-                    {children}
-                </div>
+                <div className="children-container" children={children} />
             </div>
         );
     }
 
-    /*
-    Change the currKey of submissionView
-    */
-    callBack = (e) => {
+    /** Change the currKey of submissionView to that of props.keyIdx */
+    handleClick(e){
         e.preventDefault();
-        var intKey = this.props.keyIdx;
-        this.props.setSubmissionState('currKey', intKey);
+        this.props.setSubmissionState('currKey', this.props.keyIdx);
     }
 
     render() {
         var { keyValid, keyIdx, keyTypes, keyComplete } = this.props;
-        var key = keyIdx;
         var placeholders;
-        if(!isNaN(key)){
-            placeholders = this.props.keyLinkBookmarks[key].map((link) => this.generatePlaceholder(link));
-        }else{
+        if (!isNaN(keyIdx)) {
+            placeholders = this.props.keyLinkBookmarks[keyIdx].map((link) => this.generatePlaceholder(link));
+        } else {
             // must be a submitted object - plot directly
-            placeholders = _.keys(this.props.hierarchy[key]).map((child) => this.generateChild(child));
+            placeholders = _.keys(this.props.hierarchy[keyIdx]).map((child) => this.generateChild(child));
         }
         var style = {
-            'marginLeft': '-15px',
-            'overflow': 'hidden',
-            'whiteSpace': 'nowrap',
-            'textOverflow': 'ellipsis',
-            'fontSize': '0.8em',
-            'border' : '1px solid #fff'
+            
         };
         var buttonStyle = {
             'height': '15px',
@@ -170,20 +151,25 @@ class SubmissionLeaf extends React.Component{
             'verticalAlign': 'super'
         };
         var title;
-        var leftButton;
-        var titleText = this.props.keyDisplay[key] || key;
+        var leftButton = null;
+        var titleText = this.props.keyDisplay[keyIdx] || keyIdx;
+        var statusClass = null;
+        var isCurrentlySelected = false;
         // if key is not a number (i.e. path), the object is not a custom one.
         // format the leaf as the following if pre-existing obj or submitted
         // custom object.
-        if(isNaN(key) || (keyValid[key] == 4 && keyComplete[key])){
+        if(isNaN(keyIdx) || (keyValid[keyIdx] == 4 && keyComplete[keyIdx])){
+
+            statusClass = 'existing-item';
+
             // dark green bg with white text
             style.backgroundColor = '#4c994c';
             style.color = '#fff';
             var popDestination;
-            if(isNaN(key)){
-                popDestination = key;
+            if(isNaN(keyIdx)){
+                popDestination = keyIdx;
             }else{
-                popDestination = keyComplete[key];
+                popDestination = keyComplete[keyIdx];
             }
             // open a new tab on click
             title = (
@@ -198,50 +184,57 @@ class SubmissionLeaf extends React.Component{
                 }.bind(this)}>{ titleText }</span>
             );
         }else{
-            if(keyValid[key] == 0){
-                style.backgroundColor = '#fcd19c'; // orange
-            }else if(keyValid[key] == 1){
-                style.backgroundColor = '#acd1ec'; // blue
-            }else if(keyValid[key] == 2){
-                style.backgroundColor = '#e2b6b6'; // red
-            }else if(keyValid[key] == 3){
-                style.backgroundColor = '#b7e1bb'; // light green
+            switch (keyValid[keyIdx]){
+                case 0: statusClass = 'not-complete'; break;
+                case 1: statusClass = 'complete-not-validated'; break;
+                case 2: statusClass = 'failed-validation'; break;
+                case 3: statusClass = 'validated'; break;
+                default: statusClass = 'status-not-determined'; break;
             }
-            if(key === this.props.currKey){ // Current key selected
+            /*
+            if(keyValid[keyIdx] === 0){
+                statusClass = 'not-complete';
+                //style.backgroundColor = '#fcd19c'; // orange
+            }else if(keyValid[keyIdx] === 1){
+                statusClass = 'complete-not-validated';
+                //style.backgroundColor = '#acd1ec'; // blue
+            }else if(keyValid[keyIdx] === 2){
+                statusClass = 'failed-validation';
+                //style.backgroundColor = '#e2b6b6'; // red
+            }else if(keyValid[keyIdx] === 3){
+                statusClass = 'validated';
+                //style.backgroundColor = '#b7e1bb'; // light green
+            }
+            */
+            if(keyIdx === this.props.currKey){ // Current key selected
+                isCurrentlySelected = true;
                 style.fontWeight = "bold";
                 style.border = "1px solid #000";
                 title = (<span style={{'padding':'1px 5px'}} >
                             {titleText}
                         </span>);
             }else{
-                title = (<span style={{'padding':'1px 5px','cursor': 'pointer'}} onClick={this.callBack}>
+                title = (<span style={{'padding':'1px 5px','cursor': 'pointer'}} onClick={this.handleClick}>
                             {titleText}
                         </span>);
             }
         }
-        var dummyButton = <Button style={buttonStyle} bsSize="xsmall" className="icon-container pull-left" disabled></Button>;
-        if(key == 0){
-            style.marginLeft = 0;
-            leftButton = null;
-            dummyButton = null;
-        }else if (placeholders.length > 0){
-            leftButton = (<Button style={buttonStyle} bsSize="xsmall" className="icon-container pull-left" onClick={this.handleToggle}>
-                            <i style={iconStyle} className={"icon " + (this.state.open ? "icon-minus" : "icon-plus")}></i>
-                        </Button>);
-        }else{
-            leftButton = dummyButton;
+
+        if (keyIdx !== 0 && placeholders.length > 0) {
+            leftButton = (
+                <Button style={buttonStyle} bsSize="xsmall" className="icon-container pull-left" onClick={this.handleToggle}>
+                    <i style={iconStyle} className={"icon " + (this.state.open ? "icon-minus" : "icon-plus")}></i>
+                </Button>
+            );
         }
+
         return(
-            <div className="submission-nav-leaf">
-                <div className="clearfix" style={style}>
+            <div className={"submission-nav-leaf linked-item-title leaf-depth-" + (this.props.depth)}>
+                <div className={"clearfix inner-title " + statusClass + (isCurrentlySelected ? ' active' : '')} style={style}>
                     {leftButton}
                     {title}
                 </div>
-                <Collapse in={this.state.open}>
-                    <div style={{'paddingLeft':'15px'}}>
-                        {placeholders}
-                    </div>
-                </Collapse>
+                <Collapse in={this.state.open}><div className="list-of-properties">{ placeholders }</div></Collapse>
             </div>
         );
     }
