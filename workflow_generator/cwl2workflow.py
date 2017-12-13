113632b6c77aa4e43aa5f79b2d8902ea2048d72f
SooLee <duplexa@gmail.com>
2017-12-13 16:08:51 -0500

    cwl2workflow now more compatible with current workflow schema in detail

--- workflow_generator/cwl2workflow.py
+++ workflow_generator/cwl2workflow.py
@@ -1,12 +1,13 @@
 import uuid as u
 import json
 
+
 class Workflow (object):
     def __init__(self, uuid=None, steps=None, arguments=None):
         if uuid:
             self.uuid = uuid
         else:
-            self.uuid = u.uuid4()
+            self.uuid = str(u.uuid4())
 
         self.steps = steps
         self.arguments = arguments
@@ -14,17 +15,23 @@ class Workflow (object):
         self.lab = "4dn-dcic-lab"
         self.award = "1U01CA200059-01"
 
+
     def as_dict(self):
-        return self.__dict__
+        return rdict(self)
+
 
     def create_from_cwl(self, cwl):
         '''
         create from a Cwl object
         '''
-        self.steps = [Step().create_from_cwlstep(_, cwl.sourcetarget_list) for _ in cwl.steps]
+        self.steps = [Step().create_from_cwlstep(_, cwl) for _ in cwl.steps]
         self.arguments = [Argument().create_from_cwlinput(_) for _ in cwl.inputs]
         self.arguments.extend([Argument().create_from_cwloutput(_) for _ in cwl.outputs])
-
+        if cwl.fdn_meta:
+            self.data_types = cwl.fdn_meta.data_types
+            self.category = cwl.fdn_meta.category
+            self.workflow_type = cwl.fdn_meta.workflow_type
+            self.description = cwl.fdn_meta.description
         return self
 
 
@@ -35,14 +42,21 @@ class Step (object):
         self.inputs = inputs
         self.outputs = outputs
 
+
     def as_dict(self):
-        return self.__dict__
+        return rdict(self)
 
-    def create_from_cwlstep(self, cwlstep, cwl_sourcetarget_list):
+
+    def create_from_cwlstep(self, cwlstep, cwl):
+        '''
+        cwlstep: a CwlStep object (a single step element)
+        cwl: a Cwl object (for inputs, outputs, sourcetarget_list)
+        '''
         self.name = cwlstep.name
-        self.inputs = [StepInput().create_from_cwlstepinput(_) for _ in cwlstep.inputs]
-        self.outputs = [StepOutput().create_from_cwlstepoutput(self.name, _, cwl_sourcetarget_list) for _ in cwlstep.outputs]
-
+        self.inputs = [StepInput().create_from_cwlstepinput(_, cwl) for _ in cwlstep.inputs]
+        self.outputs = [StepOutput().create_from_cwlstepoutput(self.name, _, cwl) for _ in cwlstep.outputs]
+        if cwlstep.fdn_step_meta:
+            self.meta = cwlstep.fdn_step_meta.as_dict()
         return self
 
 
@@ -56,18 +70,22 @@ class Argument (object):
         if argument_format:
             self.argument_format = argument_format
 
-    def as_dict(self):
-        return self.__dict__
 
-    def create_from_cwloutput(self, cwl_output,
-                              type="processed"):  # "processed", "QC" or "report"
+    def as_dict(self):
+        return rdict(self)
+
+
+    def create_from_cwloutput(self, cwl_output):
         '''
         convert from a CwlOutput object
         '''
         self.workflow_argument_name = cwl_output.name
-        self.argument_type = "Output " + type + " file"
-
+        if cwl_output.fdn_format:
+            self.format = cwl_output.fdn_format
+        if cwl_output.fdn_output_type:
+            self.argument_type = "Output " + cwl_output.fdn_output_type + "file"
         return self
+
 
     def create_from_cwlinput(self, cwl_input):
         '''
@@ -78,7 +96,8 @@ class Argument (object):
             self.argument_type = "Input file"
         else:
             self.argument_type = "parameter"
-
+        if cwl_input.fdn_format:
+            self.format = cwl_input.fdn_format
         return self
 
 
@@ -98,29 +117,58 @@ class StepInput (object):
             self.meta.append({"argument_format": argument_format})
 
         if source_name or source_type:
-            self.source = { "name": source_name,
-                            "type": source_type }
+            self.source = [{ "name": source_name,
+                             "type": source_type }]
             if source_step:
-                self.source.append({"step": source_step})
+                self.source[0].append({"step": source_step})
+
 
     def as_dict(self):
-        return self.__dict__
+        return rdict(self)
 
-    def create_from_cwlstepinput(self, cwl_stepinput):
+
+    def create_from_cwlstepinput(self, cwl_stepinput, cwl):
         '''
         convert from a CwlStepInput object
+        cwl_stepinput: a CwlStepInput object (a single step input element)
+        cwl: a Cwl object (for inputs, outputs, sourcetarget_list)
         '''
+        # add name
         self.name = cwl_stepinput.arg_name
+
+        # add source
         if cwl_stepinput.source:
             if cwl_stepinput.source_step:
-                self.source = { "name": cwl_stepinput.source_arg,
-                                "type": "Output file",
-                                "step": cwl_stepinput.source_step }
+                self.source = [{ "name": cwl_stepinput.source_arg,
+                                 "type": "Output file",
+                                 "step": cwl_stepinput.source_step }]
             else:
-                self.source = { "name": cwl_stepinput.source_arg,
-                                "type": "Workflow Input file" }
+                self.source = [{ "name": cwl_stepinput.source_arg,
+                                 "type": "Workflow Input file" }]
+
+        # add format
+        self.add_fdn_format_from_cwl(cwl.inputs)
 
         return self
+
+
+    def add_fdn_format_from_cwl(self, cwl_inputs):
+        '''
+        add fdn_format info for the step inputs that source to global input
+        assumes self.source has already been filled in.
+        '''
+        is_workflow_input = False
+        for tg in self.source:
+            if tg.get('type') == "Workflow Input File":
+                is_workflow_input = True
+                workflow_arg_name = tg.get('name')
+                break
+
+        if is_workflow_input:
+            for op in cwl_inputs:
+                if op.name == workflow_arg_name:
+                    self.meta['argument_format'] = op.fdn_format
+                    break
 
 
 class StepOutput (object):
@@ -140,22 +188,28 @@ class StepOutput (object):
             self.meta.append({"argument_format": argument_format})
 
         if target_name or target_type:
-            self.target = { "name": target_name,
-                            "type": target_type }
+            self.target = [{ "name": target_name,
+                             "type": target_type }]
             if target_step:
-                self.target.append({"step": target_step})
+                self.target[0].append({"step": target_step})
+
 
     def as_dict(self):
-        return self.__dict__
+        return rdict(self)
 
-    def create_from_cwlstepoutput(self, parent_stepname, cwl_stepoutput, cwl_sourcetarget_list):
+
+    def create_from_cwlstepoutput(self, parent_stepname, cwl_stepoutput, cwl):
         '''
         convert from CwlStepOutput object
+        parent_stepname : name of the step where the step output belongs to
+        cwl_stepoutput : a CwlStepOutput object ( a single step output element)
+        cwl : a Cwl object
         '''
         self.name = cwl_stepoutput.arg_name
-        self.add_target_from_cwloutputs(parent_stepname, cwl_sourcetarget_list)
-
+        self.add_target_from_cwloutputs(parent_stepname, cwl.sourcetarget_list)
+        self.add_fdn_format_from_cwl(cwl.outputs)
         return self
+
 
     def add_target_from_cwloutputs(self, parent_stepname, cwl_sourcetarget_list):
         '''
@@ -163,17 +217,58 @@ class StepOutput (object):
         '''
         for st in cwl_sourcetarget_list:
             if st.source_step == parent_stepname and st.source_arg == self.name:
+                if not hasattr(self, 'target') or not self.target:
+                    self.target = []
                 if st.target_step:
                     target_type = "Input file"
+                    self.target.append({"name": st.target_arg,
+                                        "type": target_type,
+                                        "step": st.target_step})
                 else:
                     target_type = "Workflow Output File"
-                self.target = { "name": st.target_arg,
-                                "type": target_type }
+                    self.target.append({"name": st.target_arg,
+                                        "type": target_type})
+
+    def add_fdn_format_from_cwl(self, cwl_outputs):
+        '''
+        add fdn_format info for the step outputs that target to global output
+        assumes self.target has already been filled in.
+        '''
+        is_workflow_output = False
+        for tg in self.target:
+            if tg.get('type') == "Workflow Output File":
+                is_workflow_output = True
+                workflow_arg_name = tg.get('name')
                 break
 
+        if is_workflow_output:
+            for op in cwl_outputs:
+                if op.name == workflow_arg_name:
+                    self.meta['argument_format'] = op.fdn_format
+                    break
+
+
+    def add_fdn_output_type_from_cwl(self, cwl_outputs):
+        '''
+        add fdn_output_type info for the step outputs that target to global output
+        assumes self.target has already been filled in.
+        '''
+        is_workflow_output = False
+        for tg in self.target:
+            if tg.get('type') == "Workflow Output File":
+                is_workflow_output = True
+                workflow_arg_name = tg.get('name')
+                break
+
+        if is_workflow_output:
+            for op in cwl_outputs:
+                if op.name == workflow_arg_name:
+                    self.meta['argument_type'] = op.fdn_output_type
+                    break
 
 class CwlOutput (object):
-    def __init__(self, id=None, source=None, type=None):
+    def __init__(self, id=None, source=None, type=None,
+                 fdn_format=None, fdn_output_type=None):  # 4dn-specific custom fields
         '''
         take in elements of a cwl's 'outputs' field (dictionary) as kwargs
         '''
@@ -203,12 +298,17 @@ class CwlOutput (object):
             self.source = source
             self.source_step, self.source_arg = self.source.strip('#').split('.')
 
+        self.fdn_format = fdn_format
+        self.fdn_output_type = fdn_output_type
+
+
     def get_sourcetarget(self):
         return SourceTarget(self.source_step, self.source_arg, None, self.name)
 
 
 class CwlInput (object):
-    def __init__(self, id=None, type=None, default=None):
+    def __init__(self, id=None, type=None, default=None,
+                 fdn_format=None):  # 4dn-specific custom field
         '''
         take in elements of a cwl's 'inputs' field (dictionary) as kwargs
         '''
@@ -234,6 +334,7 @@ class CwlInput (object):
                                 self.isFile = True
 
         self.default = default
+        self.fdn_format = fdn_format
 
 
 class CwlStepOutput (object):
@@ -274,12 +375,14 @@ class CwlStepInput (object):
             self.source_step = None
             self.source_arg = None
 
+
     def get_sourcetarget(self):
         return SourceTarget(self.source_step, self.source_arg, self.step_name, self.arg_name)
 
 
 class CwlStep (object):
-    def __init__(self, id=None, run=None, outputs=None, inputs=None): 
+    def __init__(self, id=None, run=None, outputs=None, inputs=None,
+                 fdn_step_meta=None):  # 4dn-specific custom tag
         '''
         take in elements of a cwl's 'steps' field (dictionary) as kwargs
         '''
@@ -289,11 +392,14 @@ class CwlStep (object):
         self.run = run
         self.outputs = [CwlStepOutput(**_) for _ in outputs]
         self.inputs = [CwlStepInput(**_) for _ in inputs]
+        self.fdn_step_meta = CwlFdnStepMeta(**fdn_step_meta)
 
 
 class Cwl (object):
      def __init__(self, inputs=None, outputs=None, steps=None,
-                  cwlVersion=None, requirements=None, **kwargs):
+                  cwlVersion=None, requirements=None,
+                  fdn_meta=None,  # 4dn-specific custom tag
+                  **kwargs):  # kwargs includes field 'class'
         '''
         take in a cwl as kwargs
         '''
@@ -307,12 +413,41 @@ class Cwl (object):
         self.outputs = [CwlOutput(**_) for _ in outputs]
         self.steps = [CwlStep(**_) for _ in steps]
         self.requirements = requirements
+        if fdn_meta:
+          self.fdn_meta = CwlFdnMeta(**fdn_meta)
+        else:
+          self.fdn_meta = None
 
         # list of all SourceTarget objects in CWL
         self.sourcetarget_list = [_.get_sourcetarget() for _ in self.outputs]
         for step in self.steps:
             self.sourcetarget_list.extend([_.get_sourcetarget() for _ in step.inputs])
+
             
+class CwlFdnMeta (object):
+    def __init__(self, data_types=None, category=None, workflow_type=None, description=None):
+        '''
+        take in cwl's 'fdn_meta' field (dictionary) as kwargs
+        '''
+        self.data_types = data_types
+        self.category = category
+        self.workflow_type = workflow_type
+        self.description = description
+
+
+class CwlFdnStepMeta (object):
+    def __init__(self, software_used=None, description=None, analysis_step_types=None):
+        '''
+        take in cwl's 'steps::fdn_step_meta' field (dictionary) as kwargs
+        '''
+        self.software_used = software_used
+        self.description = description
+        self.analysis_step_types = analysis_step_types
+
+
+    def as_dict(self):
+        return rdict(self)
+
 
 class SourceTarget (object):
     def __init__(self, source_step=None, source_arg=None, target_step=None, target_arg=None):
@@ -321,11 +456,28 @@ class SourceTarget (object):
         self.target_step = target_step
         self.target_arg = target_arg
 
+
     def as_dict(self):
-        return self.__dict__
+        return rdict(self)
 
 
 def create_cwl_from_file(file):
     with open(file, 'r') as f:
         cwldict = json.load(f)
     return Cwl(**cwldict)
+
+
+def rdict(x):
+    '''
+    recursive conversion to dictionary
+    converts objects in list members to dictionary recursively
+    '''
+    if hasattr(x, '__dict__'):
+        d = x.__dict__
+        for k, v in d.items():
+            if isinstance(v, list):
+                d[k] = [rdict(_) for _ in v]
+        return d
+    else:
+        return x
+
