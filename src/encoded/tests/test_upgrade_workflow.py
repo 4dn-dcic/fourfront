5045b92fe139cd97eaf3a11cec1db328a6806711
SooLee <duplexa@gmail.com>
2018-01-03 15:31:54 -0500

    added required fields to test_upgrade_workflow

--- src/encoded/tests/test_upgrade_workflow.py
+++ src/encoded/tests/test_upgrade_workflow.py
@@ -3,16 +3,21 @@ import pytest
 
 
 @pytest.fixture
-def workflow_2(software_used):
+def workflow_2(software, award, lab):
     return{
         "schema_version": '2',
-        "steps": { "meta": { "software_used" : software_used['@id'] } }
+        "award": award['@id'],
+        "lab": lab['@id'],
+        "title": "some workflow",
+        "name": "some workflow",
+        "workflow_type": "Other",
+        "steps": { "meta": { "software_used" : software['@id'] } }
     }
 
 
 def test_workflow_convert_software_used_to_list(
-        app, workflow_2, software_used):
+        app, workflow_2, software):
     migrator = app.registry['upgrader']
     value = migrator.upgrade('workflow', workflow_2, current_version='2', target_version='3')
     assert value['schema_version'] == '3'
-    assert value['steps']['meta']['software_used'] == [software_used['@id']] 
+    assert value['steps']['meta']['software_used'] == [software['@id']] 
