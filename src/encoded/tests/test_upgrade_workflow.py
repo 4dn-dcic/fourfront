86e1c8b8723f67ff027b4c670a309cb25e873f90
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-01-17 14:01:35 -0500

    Codacy

--- src/encoded/tests/test_upgrade_workflow.py
+++ src/encoded/tests/test_upgrade_workflow.py
@@ -15,7 +15,7 @@ def workflow_2(software, award, lab):
     }
 
 
-def test_workflow_convert_software_used_to_list(
+def test_workflow_convert_software_used_to_list_2(
         app, workflow_2, software):
     migrator = app.registry['upgrader']
     value = migrator.upgrade('workflow', workflow_2, current_version='2', target_version='3')
@@ -40,7 +40,7 @@ def workflow_3(software, award, lab):
         ]
     }
 
-def test_workflow_convert_software_used_to_list(
+def test_workflow_convert_software_used_to_list_3(
         app, workflow_3, software):
     migrator = app.registry['upgrader']
     value = migrator.upgrade('workflow', workflow_3, current_version='3', target_version='4')
