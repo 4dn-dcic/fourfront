29cd8385c587e01490108494d82daf0ab147710a
aschroed <andrew_schroeder@hms.harvard.edu>
2018-02-01 14:39:03 -0500

    Updated all genomics experiment schemas that had antibody field to use antibody_info mixin. Wrote upgraders and upgrade tests for those schemas. Added CUT&RUN to experiment_type enum in experiment_seq Added reaction_time field to experiment_seq for recording digestion time in CUT&RUN (and others?)

--- src/encoded/upgrade/experiment.py
+++ src/encoded/upgrade/experiment.py
@@ -5,3 +5,40 @@ from snovault import upgrade_step
 def experiment_repliseq_1_2(value, system):
     if value['experiment_type'] == 'repliseq':
         value['experiment_type'] = 'Repli-seq'
+
+
+@upgrade_step('experiment_repliseq', '2', '3')
+def experiment_repliseq_2_3(value, system):
+    # sticking the string in antibody field into Notes
+    # will require subsequent manual fix to link to Antibody object
+    if value.get('antibody'):
+        if value.get('notes'):
+            value['notes'] = value['notes'] + '; ' + value['antibody']
+        else:
+            value['notes'] = value['antibody']
+        del value['antibody']
+    # if antibody_lot_id exists it should be fine in new field
+
+
+@upgrade_step('experiment_chiapet', '1', '2')
+def experiment_chiapet_1_2(value, system):
+    # sticking the string in antibody field into Notes
+    # will require subsequent manual fix to link to Antibody object
+    if value.get('antibody'):
+        if value.get('notes'):
+            value['notes'] = value['notes'] + '; ' + value['antibody']
+        else:
+            value['notes'] = value['antibody']
+        del value['antibody']
+
+
+@upgrade_step('experiment_seq', '1', '2')
+def experiment_seq_1_2(value, system):
+    # sticking the string in antibody field into Notes
+    # will require subsequent manual fix to link to Antibody object
+    if value.get('antibody'):
+        if value.get('notes'):
+            value['notes'] = value['notes'] + '; ' + value['antibody']
+        else:
+            value['notes'] = value['antibody']
+        del value['antibody']
