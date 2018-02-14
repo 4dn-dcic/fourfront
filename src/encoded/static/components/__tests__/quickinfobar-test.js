d58956c4d225a9f4a0b38f50748d55e9409a3ec5
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-02-14 14:26:19 -0500

    Update test

--- src/encoded/static/components/__tests__/quickinfobar-test.js
+++ src/encoded/static/components/__tests__/quickinfobar-test.js
@@ -9,12 +9,15 @@ jest.autoMockOff();
 jest.dontMock('react');
 jest.dontMock('underscore');
 
+import React from 'react';
+
+
+
 
 describe('Testing viz/QuickInfoBar.js', function() {
-    var React, TestUtils, page, context, Filters, _, Wrapper, QuickInfoBar, href, expSetFilters, contextFilters;
+    var TestUtils, page, context, Filters, _, Wrapper, QuickInfoBar, href, expSetFilters, contextFilters, barplot_data_unfiltered, barplot_data_filtered;
 
     beforeEach(function() {
-        React = require('react');
         var { Provider, connect } = require('react-redux');
         Filters = require('./../util/experiments-filters');
         TestUtils = require('react-dom/lib/ReactTestUtils');
@@ -44,8 +47,24 @@ describe('Testing viz/QuickInfoBar.js', function() {
                 "remove": "/browse/?type=ExperimentSetReplicate&experimentset_type=replicate&experiments_in_set.biosample.biosource.individual.organism.name=mouse"
             }
         ];
+
+        class QuickWrapper extends React.Component {
+
+            constructor(props){
+                super(props);
+                this.state = {
+                    barplot_data_unfiltered : props.barplot_data_unfiltered || null,
+                    barplot_data_filtered : props.barplot_data_filtered || null
+                };
+            }
+        
+            render(){
+                return <QuickInfoBar {...this.props} {...this.state} />;
+            }
+        }
+        
         expSetFilters = Filters.contextFiltersToExpSetFilters(contextFilters);
-        page = TestUtils.renderIntoDocument(<QuickInfoBar href={href} expSetFilters={expSetFilters} />);
+        page = TestUtils.renderIntoDocument(<QuickWrapper href={href} expSetFilters={expSetFilters} />);
     });
 
     it('Has elements for stats (file, exps, expsets)', function() {
@@ -59,16 +78,11 @@ describe('Testing viz/QuickInfoBar.js', function() {
         statValEls.forEach(function(el){ // Ensure all vals == 0
             expect(parseInt(el.innerHTML)).toBe(0);
         });
-        // Change those vals
-        page.updateCurrentAndTotalCounts({
-            experiments: 10,
-            experiment_sets : 10,
-            files : 10
-        },{
-            experiments: 211,
-            experiment_sets : 211,
-            files : 211
-        });
+
+        barplot_data_filtered = { 'total' : { 'experiment_sets' : 10, 'experiments' : 10, 'files' : 10 } };
+        barplot_data_unfiltered = { 'total' : { 'experiment_sets' : 211, 'experiments' : 211, 'files' : 211 } };
+
+        page.setState({ barplot_data_filtered, barplot_data_unfiltered });
 
         // Ensure they're changed
         statValEls.forEach(function(el){
