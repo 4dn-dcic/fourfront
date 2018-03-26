95f72f9ae2414cccee09110b78ed96f8db972e76
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-03-26 14:11:44 -0400

    Remove example tests, up sleep duration to allow for indexing to finish

---
+++ cypress/integration/test_browse_views.js
@@ -0,0 +1,40 @@
+
+
+describe('Browse Views', function () {
+
+    context('Test /browse/ page redirection from homepage', function(){
+        /*
+        before(function(){
+            cy.visit('/');
+        });
+        */
+
+        it('If start from home page, clicking on Browse nav menu item gets us to Browse page.', function(){
+            
+            cy.visit('/');
+            
+            cy.get('#sBrowse').click().then(()=>{
+                cy.get('#page-title-container .page-title span.title').should('have.text', 'Data Browser');
+            });
+
+        });
+
+        it('Only shows award.project=4DN results.', function(){
+
+            cy.location('search').should('include', 'award.project=4DN');
+
+        });
+
+
+        it('If point browser to /browse/ page (no URL params), we also get redirected to award.project=4DN correctly.', function(){
+
+            cy.visit('/');
+            cy.visit('/browse/', { "failOnStatusCode" : false });
+
+            cy.location('search').should('include', 'award.project=4DN');
+
+        });
+
+    });
+
+});
--- cypress/integration/test_search_views.js
+++ cypress/integration/test_search_views.js
@@ -66,4 +66,41 @@ describe('Search Views', function () {
 
     });
 
+    context('Test /browse/ page redirection from homepage', function(){
+        /*
+        before(function(){
+            cy.visit('/');
+        });
+        */
+
+        it('If start from home page, clicking on Browse nav menu item gets us to Browse page.', function(){
+            
+            cy.visit('/');
+            
+            cy.get('#sBrowse').click();
+
+            cy.get('#page-title-container .page-title span.title').then(($title)=>{
+                expect($title.text()).to.equal('Data Browser');
+            });
+
+        });
+
+        it('Only shows award.project=4DN results.', function(){
+
+            cy.location('search').should('include', 'award.project=4DN');
+
+        });
+
+
+        it('If point browser to /browse/ page (no URL params), we also get redirected to award.project=4DN correctly.', function(){
+
+            cy.visit('/');
+            cy.visit('/browse/');
+
+            cy.location('search').should('include', 'award.project=4DN');
+
+        });
+
+    });
+
 });
