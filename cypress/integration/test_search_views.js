41784ebaac595af7ae6fe06b8e9e91abf98f5b44
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-03-26 14:13:24 -0400

    Minor

--- cypress/integration/test_search_views.js
+++ cypress/integration/test_search_views.js
@@ -3,13 +3,6 @@
 describe('Search Views', function () {
 
     context('For /search/?type=Item', function () {
-/*
-        beforeEach(function () {
-            cy.visit('https://example.cypress.io')
-            cy.get('.navbar-nav').contains('Commands').click()
-            cy.get('.dropdown-menu').contains('Navigation').click()
-        });
-*/  
 
         before(function(){ // beforeAll
             cy.visit('/search/');
@@ -62,43 +55,6 @@ describe('Search Views', function () {
             cy.get('.search-results-container .search-result-row').then(($searchResultElems)=>{
                 expect($searchResultElems.length).to.be.greaterThan(20);
             });
-        });
-
-    });
-
-    context('Test /browse/ page redirection from homepage', function(){
-        /*
-        before(function(){
-            cy.visit('/');
-        });
-        */
-
-        it('If start from home page, clicking on Browse nav menu item gets us to Browse page.', function(){
-            
-            cy.visit('/');
-            
-            cy.get('#sBrowse').click();
-
-            cy.get('#page-title-container .page-title span.title').then(($title)=>{
-                expect($title.text()).to.equal('Data Browser');
-            });
-
-        });
-
-        it('Only shows award.project=4DN results.', function(){
-
-            cy.location('search').should('include', 'award.project=4DN');
-
-        });
-
-
-        it('If point browser to /browse/ page (no URL params), we also get redirected to award.project=4DN correctly.', function(){
-
-            cy.visit('/');
-            cy.visit('/browse/');
-
-            cy.location('search').should('include', 'award.project=4DN');
-
         });
 
     });
