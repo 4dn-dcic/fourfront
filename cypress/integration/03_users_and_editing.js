b21d1c3690e0dda730fee2a255f5f30422fcd2de
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-04-01 16:33:29 -0400

    Adjust user profile edit test re: callbacks, forgot .json in prev commit

--- cypress/integration/03_users_and_editing.js
+++ cypress/integration/03_users_and_editing.js
@@ -4,7 +4,7 @@
  */
 
 
-describe('Test logging in, navigating to profile page, editing last_name, & editing it back.', function () {
+describe('Impersonate user JWT, navigate to profile, edit last_name to & back.', function () {
 
 
     context('Wrangler User Profile', function () {
@@ -24,45 +24,34 @@ describe('Test logging in, navigating to profile page, editing last_name, & edit
 
             cy.login4DN({ 'email' : 'wrangler@wrangler.com', 'useEnvToken' : false }).end().get('ul.navbar-acct li.user-account-item').should('have.class', 'is-logged-in').then((accountListItem)=>{
                 expect(accountListItem.children('#user_actions_dropdown').text()).to.contain('Wrangler');
-            }).end().get("#user_actions_dropdown").click().then(()=>{
-                cy.get('ul.dropdown-menu[aria-labelledby="user_actions_dropdown"] a#profile').click().then(()=>{
-                    cy.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").then((title)=>{
-                        // Edit wrangler last name
-                        cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click();
-                        cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').clear().type('SuperWrangler').then((inputfield)=>{
-                            //inputfield.type('SuperWrangler');
-                            cy.wait(100);
-                            cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click().then(()=>{
-                                cy.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler");
-
-                                // After reloading on datastore=database, last name stays edited. Then change back.
-                                cy.url().then((currUrl)=>{
-                                    cy.visit(currUrl + '?datastore=database').then(()=>{
-                                        cy.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler").then(()=>{
-
-                                            // Cleanup & test again
-                                            cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click();
-                                            cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').should('have.value', 'SuperWrangler').clear().type('Wrangler').then((inputfield)=>{
-                                                cy.wait(100);
-                                                cy.get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click().then(()=>{
-                                                    cy.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").then(()=>{
-                                                        cy.wait(100);
-                                                        cy.visit(currUrl + '?datastore=database').then(()=>{
-                                                            cy.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler");
-                                                        });
-                                                    });
-                                                });
-                                            });
-                                        });
-                                    });
-                                });
-
-
-                            });
-                        });
+            }).end()
+            .get("#user_actions_dropdown").click().wait(100).end()
+            .get('ul.dropdown-menu[aria-labelledby="user_actions_dropdown"] a#profile').click().end()
+            .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").end()
+            // Edit wrangler last name
+            .get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click().end()
+            .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').clear().type('SuperWrangler').then((inputfield)=>{
+                //inputfield.type('SuperWrangler');
+                return cy.wait(100).get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click().end()
+                .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler").end()
+                .url().then((currUrl)=>{ // After reloading on datastore=database, last name stays edited. Then change back.
+                    return cy.visit(currUrl + '?datastore=database').end()
+                    .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler").end()
+                    // Cleanup & test again
+                    .get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click().end()
+                    .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').should('have.value', 'SuperWrangler').clear().type('Wrangler').then((inputfield)=>{
+                        return cy.wait(100)
+                        .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click().end()
+                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").end()
+                        .visit(currUrl + '?datastore=database').end()
+                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").end();
                     });
+        
                 });
+
+
             });
+
 
         });
 
