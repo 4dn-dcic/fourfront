1f305f68f3be0884ca4b0cf830fcd79d15905fbe
Sarah <sarah_reiff@hms.harvard.edu>
2018-04-04 12:03:53 -0400

    Edited test_types_treatment.py for display_title changes

--- src/encoded/tests/test_types_treatment.py
+++ src/encoded/tests/test_types_treatment.py
@@ -22,13 +22,13 @@ def test_calculated_chemical_treatment_display_title(testapp, drug_treatment):
     res = testapp.patch_json(
         drug_treatment['@id'],
         {'duration': 3.5, 'duration_units': 'hour'})
-    assert res.json['@graph'][0]['display_title'] == 'Drug treatment (3.5 M 3.5 hour)'
+    assert res.json['@graph'][0]['display_title'] == 'Drug treatment (3.5 M, 3.5h)'
     res = testapp.patch_json(drug_treatment['@id'], {'temperature': 3.5})
-    assert res.json['@graph'][0]['display_title'] == 'Drug treatment (3.5 M 3.5 hour at 3.5 °C)'
+    assert res.json['@graph'][0]['display_title'] == 'Drug treatment (3.5 M, 3.5h at 3.5°C)'
 
 
 def test_calculated_rnai_treatment_display_title(testapp, rnai, target_w_genes):
     # import pdb; pdb.set_trace()
     assert rnai['display_title'] == 'shRNA treatment'
     res = testapp.patch_json(rnai['@id'], {'target': target_w_genes['@id']})
-    assert res.json['@graph'][0]['display_title'] == 'shRNA treatment for Gene:eeny,meeny'
+    assert res.json['@graph'][0]['display_title'] == 'shRNA of Gene:eeny,meeny'
