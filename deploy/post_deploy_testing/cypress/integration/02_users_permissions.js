4e9b486263f4b05cf2231ab34359e0f7ee6039bc
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-04-04 20:55:51 -0400

    Move user editing tests to post-deploy, add test user to inserts

--- deploy/post_deploy_testing/cypress/integration/02_users_permissions.js
+++ deploy/post_deploy_testing/cypress/integration/02_users_permissions.js
@@ -1,9 +1,4 @@
 
-/**
- * This file of tests only gets executed by Travis CI
- */
-
-
 describe('Impersonate user JWT, navigate to profile, edit last_name to & back.', function () {
 
 
@@ -22,12 +17,12 @@ describe('Impersonate user JWT, navigate to profile, edit last_name to & back.',
 
         it('Ensure logged in, visit profile page, add & remove access key', function(){
 
-            cy.login4DN({ 'email' : 'wrangler@wrangler.com', 'useEnvToken' : false }).end().get('ul.navbar-acct li.user-account-item').should('have.class', 'is-logged-in').then((accountListItem)=>{
-                expect(accountListItem.children('#user_actions_dropdown').text()).to.contain('Wrangler');
+            cy.login4DN({ 'email' : 'ud4dntest@gmail.com', 'useEnvToken' : false }).end().get('ul.navbar-acct li.user-account-item').should('have.class', 'is-logged-in').then((accountListItem)=>{
+                expect(accountListItem.children('#user_actions_dropdown').text()).to.contain('Frontend');
             }).end()
                 .get("#user_actions_dropdown").click().wait(100).end()
                 .get('ul.dropdown-menu[aria-labelledby="user_actions_dropdown"] a#profile').click().end()
-                .get('.page-container .user-title-row-container h1.user-title').should('contain', "Wrangler").end() // Test only for first name as we're editing last name & it may change re: delayed indexing, etc.
+                .get('.page-container .user-title-row-container h1.user-title').should('contain', "Frontend").end() // Test only for first name as we're editing last name & it may change re: delayed indexing, etc.
                 .get('.page-container .access-keys-container h3').should('contain', "Access Keys").end()
                 .get('.page-container .access-keys-container #add-access-key').click().wait(100).end()
                 .get('.modal-body').should('contain', 'Access Key ID').should('contain', 'Secret Access Key').end()
@@ -42,31 +37,30 @@ describe('Impersonate user JWT, navigate to profile, edit last_name to & back.',
 
         it('Ensure logged in, visit profile page, edit last name 2x.', function(){
 
-            cy.login4DN({ 'email' : 'wrangler@wrangler.com', 'useEnvToken' : false }).end().get('ul.navbar-acct li.user-account-item').should('have.class', 'is-logged-in').then((accountListItem)=>{
-                expect(accountListItem.children('#user_actions_dropdown').text()).to.contain('Wrangler');
+            cy.login4DN({ 'email' : 'ud4dntest@gmail.com', 'useEnvToken' : false }).end().get('ul.navbar-acct li.user-account-item').should('have.class', 'is-logged-in').then((accountListItem)=>{
+                expect(accountListItem.children('#user_actions_dropdown').text()).to.contain('Frontend');
             }).end()
                 .get("#user_actions_dropdown").click().wait(100).end()
                 .get('ul.dropdown-menu[aria-labelledby="user_actions_dropdown"] a#profile').click().end()
-                .get('.page-container .user-title-row-container h1.user-title').invoke('text').should('include', "Wrangler").end() // Test only for first name as we're editing last name & it may change re: delayed indexing, etc.
-            // Edit wrangler last name
+                .get('.page-container .user-title-row-container h1.user-title').invoke('text').should('include', "Frontend").end() // Test only for first name as we're editing last name & it may change re: delayed indexing, etc.
+                // Edit last name
                 .get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click().end()
-                .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').clear().type('SuperWrangler').then((inputfield)=>{
-                    //inputfield.type('SuperWrangler');
-                    return cy.wait(100).get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click()
+                .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').clear().type('SuperTest').then((inputfield)=>{
+                    return cy.wait(100).screenshot().get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click()
                         .should('have.length', 0).wait(100).end()
-                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler").end()
+                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Frontend SuperTest").end()
                         .url().then((currUrl)=>{ // After reloading on datastore=database, last name stays edited. Then change back.
                             return cy.visit(currUrl + '?datastore=database').end()
-                                .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler SuperWrangler").end()
+                                .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Frontend SuperTest").end()
                                 // Cleanup & test again
                                 .get('.page-container .user-title-row-container h1.user-title .last_name .value.saved a.edit-button').click().end()
-                                .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').should('have.value', 'SuperWrangler').clear().type('Wrangler').then((inputfield)=>{
+                                .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing input').should('have.value', 'SuperTest').clear().type('Test').then((inputfield)=>{
                                     return cy.wait(100)
                                         .get('.page-container .user-title-row-container h1.user-title .last_name .value.editing .save-button').click()
                                         .should('have.length', 0).wait(100).end()
-                                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").end();
-                                        //.reload()
-                                        //.get('.page-container .user-title-row-container h1.user-title').should('have.text', "Wrangler Wrangler").end();
+                                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Frontend Test").end()
+                                        .reload()
+                                        .get('.page-container .user-title-row-container h1.user-title').should('have.text', "Frontend Test").end();
                                 });
                         });
 
