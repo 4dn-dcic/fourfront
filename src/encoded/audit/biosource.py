7a6811dd36533ba30bcd1c10e6331ff9e969c20c
Sarah <sarah_reiff@hms.harvard.edu>
2018-04-12 17:46:24 -0400

    Added more terms to cell_types in audit/biosource.py to reflect updated biosource.json schema

--- src/encoded/audit/biosource.py
+++ src/encoded/audit/biosource.py
@@ -11,8 +11,9 @@ def audit_biosource_type_cell_lines_have_cell_line_value(value, system):
     a value in the cell_line field - if it is filled the validator ensures it's
     a valid cellish ontology term so don't need to check that here
     '''
-    cell_types = ["primary cell", "immortalized cell line", "in vitro differentiated cells",
-                  "induced pluripotent stem cell line", "stem cell"]
+    cell_types = ["primary cell", "primary cell line", "immortalized cell line",
+                  "in vitro differentiated cells", "induced pluripotent stem cell line",
+                  "stem cell", "stem cell derived cell line"]
     bstype = value.get('biosource_type')  # required
     if bstype in cell_types:
         if not value.get('cell_line'):
