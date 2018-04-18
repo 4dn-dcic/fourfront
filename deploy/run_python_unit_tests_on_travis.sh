9d2e2c2077ed7e82b5a2f7cedee2151a6f2938de
j1z0 <jeremy_johnson@hms.harvard.edu>
2018-04-18 10:39:26 -0400

    take out temp hack

--- deploy/run_python_unit_tests_on_travis.sh
+++ deploy/run_python_unit_tests_on_travis.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-pip show pytest
-pip install pytest==2.9.1
-pip show pytest
-bin/test -v -v -m "working and not setone" --durations=40 --cov src/encoded
+bin/test -v -v -m "working and not setone" --durations=10 --cov src/encoded
