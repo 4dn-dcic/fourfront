dbef7ce55e9fa223ee1cd77df449b494073f4281
Sarah <sarah_reiff@hms.harvard.edu>
2018-05-02 13:56:56 -0400

    Added test_types_imaging.py for imaging_path display_title tests

---
+++ src/encoded/tests/test_types_imaging.py
@@ -0,0 +1,39 @@
+import pytest
+pytestmark = [pytest.mark.working, pytest.mark.schema]
+
+
+@pytest.fixture
+def histone_target(testapp, lab, award):
+    item = {
+        'targeted_proteins': ['H2B1C_HUMAN'],
+        'award': award['@id'],
+        'lab': lab['@id'],
+    }
+    return testapp.post_json('/target', item).json['@graph'][0]
+
+
+@pytest.fixture
+def img_path_blank(testapp, lab, award):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+    }
+    return testapp.post_json('/imaging_path', item).json['@graph'][0]
+
+
+def test_imgpath_displaytitle_target_probe(testapp, img_path_blank, histone_target):
+    res = testapp.patch_json(img_path_blank['@id'], {'target': [histone_target['@id']],
+                                                     'labeled_probe': 'secondary antibody'}).json['@graph'][0]
+    assert res['display_title'] == 'Protein:H2B1C_HUMAN targeted by secondary antibody'
+
+
+def test_imgpath_displaytitle(testapp, img_path_blank, histone_target):
+    #res = testapp.patch_json(img_path_blank['@id'], {}).json['@graph'][0]
+    assert img_path_blank['display_title'] == 'not enough information'
+    res = testapp.patch_json(img_path_blank['@id'], {'target': [histone_target['@id']],
+                                                     'labels': ['GFP']}).json['@graph'][0]
+    assert res['display_title'] == 'Protein:H2B1C_HUMAN targeted by GFP'
+    res = testapp.patch_json(img_path_blank['@id'], {'labeled_probe': 'secondary antibody'}).json['@graph'][0]
+    assert res['display_title'] == 'Protein:H2B1C_HUMAN targeted by GFP-labeled secondary antibody'
+    res = testapp.patch_json(img_path_blank['@id'], {'other_probes': ['primary Ab 1', 'primary Ab 2']}).json['@graph'][0]
+    assert res['display_title'] == 'Protein:H2B1C_HUMAN targeted by GFP-labeled secondary antibody (with primary Ab 1, primary Ab 2)'
