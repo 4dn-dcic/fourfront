fc47a92cf6add3c3d1528f33e290de46898c9532
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-05-04 14:13:46 -0400

    Codacy

--- src/encoded/static/components/util/navigate.js
+++ src/encoded/static/components/util/navigate.js
@@ -190,8 +190,8 @@ navigate.deregisterCallbackFunction = function(fxn){
 /** Useful for param lists */
 navigate.mergeObjectsOfLists = function(){
     if (arguments.length < 2) throw Error('Expecting multiple objects as params');
-    let targetObj = arguments[1];
-    let sourceObjs = Array.prototype.slice.call(arguments, 1);
+    const targetObj = arguments[1];
+    const sourceObjs = Array.prototype.slice.call(arguments, 1);
     _.forEach(sourceObjs, function(o){
         _.forEach(_.keys(o), function(oKey){
             if (typeof targetObj[oKey] === 'undefined') targetObj[oKey] = [];
