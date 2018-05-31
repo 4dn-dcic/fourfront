90eb2fbda47c8429cc185abae20161689ca618ee
Sarah <sarah_reiff@hms.harvard.edu>
2018-05-31 10:49:18 -0400

    Added test for treatment_agent display_title in test_types_treatment.py

--- src/encoded/tests/test_types_treatment.py
+++ src/encoded/tests/test_types_treatment.py
@@ -7,12 +7,34 @@ def drug_treatment(testapp, lab, award):
     item = {
         'award': award['@id'],
         'lab': lab['@id'],
+        'treatment_type': 'Chemical',
         'chemical': 'Drug',
     }
     return testapp.post_json('/treatment_chemical', item).json['@graph'][0]
 
 
-def test_calculated_chemical_treatment_display_title(testapp, drug_treatment):
+@pytest.fixture
+def heatshock_treatment(testapp, lab, award):
+    item = {
+        'award': award['@id'],
+        'lab': lab['@id'],
+        'treatment_type': 'Heat Shock',
+    }
+    return testapp.post_json('/treatment_agent', item).json['@graph'][0]
+
+
+def test_calculated_chemical_treatment_display_title(testapp, heatshock_treatment):
+    # import pdb; pdb.set_trace()
+    assert heatshock_treatment['display_title'] == 'Heat Shock'
+    res = testapp.patch_json(
+        heatshock_treatment['@id'],
+        {'duration': 3.5, 'duration_units': 'hour'})
+    assert res.json['@graph'][0]['display_title'] == 'Heat Shock (3.5h)'
+    res = testapp.patch_json(heatshock_treatment['@id'], {'temperature': 42})
+    assert res.json['@graph'][0]['display_title'] == 'Heat Shock (3.5h at 42°C)'
+
+
+def test_calculated_agent_treatment_display_title(testapp, drug_treatment):
     # import pdb; pdb.set_trace()
     assert drug_treatment['display_title'] == 'Drug treatment'
     res = testapp.patch_json(
