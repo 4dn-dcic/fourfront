70290d5a08f18cdc5a5979bcc7566935c2fdb1df
Sarah <sarah_reiff@hms.harvard.edu>
2018-07-19 11:03:39 -0400

    Started writing tests for target.py

---
+++ src/encoded/tests/test_types_target.py
@@ -0,0 +1,14 @@
+import pytest
+pytestmark = [pytest.mark.working, pytest.mark.schema]
+
+# test target_summary
+def test_target_summary():
+    pass
+
+# test target_type
+def test_target_type():
+    pass
+
+# test display_title
+def test_target_display_title():
+    pass
