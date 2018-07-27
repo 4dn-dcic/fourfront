cf47d211fb7383030abbb6a26064d3501d64924b
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-07-27 17:55:58 -0400

    Ongoing - First chart (rough) working.

--- src/encoded/static/components/static-pages/StatisticsPage.js
+++ src/encoded/static/components/static-pages/StatisticsPage.js
@@ -3,6 +3,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
+import { stringify } from 'query-string';
 import { console, layout, navigate, ajax } from'./../util';
 import * as globals from './../globals';
 import StaticPage from './StaticPage';
@@ -15,7 +16,9 @@ export default class StatisticsPageView extends StaticPage {
         return (
             <StaticPage.Wrapper>
                 <StatisticsViewController>
-                    <StatisticsChartsView />
+                    <layout.WindowResizeUpdateTrigger>
+                        <StatisticsChartsView />
+                    </layout.WindowResizeUpdateTrigger>
                 </StatisticsViewController>
             </StaticPage.Wrapper>
         );
@@ -32,7 +35,9 @@ export class StatisticsViewController extends React.PureComponent {
 
     static CHART_SEARCH_URIS = {
         'File'                      : '/search/?type=File&experiments.display_title!=No%20value&limit=0',
-        'ExperimentSetReplicate'    : '/search/?type=ExperimentSetReplicate&award.project=4DN&limit=0'
+        'ExperimentSetReplicate'    : function(props) {
+            return '/search/?' + stringify(navigate.getBrowseBaseParams()) + '&limit=0';
+        }
     };
 
     constructor(props){
@@ -76,6 +81,7 @@ export class StatisticsViewController extends React.PureComponent {
         }
 
         _.forEach(_.pairs(chartUris), ([key, uri]) => {
+            if (typeof uri === 'function') uri = uri(this.props);
             ajax.load(uri, uponSingleRequestsCompleteCallback.bind(this, key, uri), 'GET', failureCallback);
         });
 
@@ -104,7 +110,7 @@ export class StatisticsViewController extends React.PureComponent {
 
 
 
-export class StatisticsChartsView extends React.PureComponent {
+export class StatisticsChartsView extends React.Component {
 
     loadingIcon(){
         return (
@@ -131,7 +137,7 @@ export class StatisticsChartsView extends React.PureComponent {
         return (
             <div className="stats-charts-container row">
                 <div className="col-xs-12 col-lg-6">
-                    <AreaChart/>
+                    <AreaChart data={aggregationsToChartData[0].function(this.props.respExperimentSetReplicate)} />
                 </div>
             </div>
         );
@@ -144,7 +150,30 @@ export const aggregationsToChartData = [
         'requires'  : 'ExperimentSetReplicate',
         'title'     : '4DN Experiment Sets Public Releases',
         'function'  : function(resp){
+            if (!resp.aggregations) return null;
+            var weeklyIntervalBuckets = resp.aggregations.weekly_interval_public_release && resp.aggregations.weekly_interval_public_release.buckets;
+            if (!Array.isArray(weeklyIntervalBuckets) || weeklyIntervalBuckets.length < 2) return null;
 
+            var total = 0;
+            var subTotals = {};
+            var aggsList = _.map(weeklyIntervalBuckets, function(bucket, index){
+                total += bucket.doc_count;
+                return {
+                    'date'     : bucket.key_as_string.split('T')[0], // Sometimes we get a time back with date when 0 doc_count; correct it to date only.
+                    'count'    : bucket.doc_count,
+                    'total'    : total,
+                    'byAward' : _.map((bucket.group_by_award && bucket.group_by_award.buckets) || [], function(subBucket){
+                        subTotals[subBucket.key] = (subTotals[subBucket.key] || 0) + subBucket.doc_count;
+                        return {
+                            'term'  : subBucket.key,
+                            'count' : subBucket.doc_count,
+                            'total' : subTotals[subBucket.key]
+                        };
+                    })
+                };
+            });
+
+            return aggsList;
         }
     }
 ];
@@ -153,13 +182,16 @@ export const aggregationsToChartData = [
 export class AreaChart extends React.PureComponent {
 
     static defaultProps = {
-        'margin' : { 'top': 20, 'right': 20, 'bottom': 30, 'left': 50 },
-        'data'   : null
+        'margin'        : { 'top': 20, 'right': 20, 'bottom': 30, 'left': 50 },
+        'data'          : null,
+        'd3TimeFormat'  : '%Y-%m-%d'
     };
 
     constructor(props){
         super(props);
         this.drawNewChart = this.drawNewChart.bind(this);
+        this.parseTime = d3.timeParse(props.d3TimeFormat);
+        this.drawnD3Elements = {};
         this.state = {
             'drawingError' : false,
             'drawn' : false
@@ -170,7 +202,13 @@ export class AreaChart extends React.PureComponent {
         this.drawNewChart();
     }
 
-    componentDidUpdate(){
+    componentWillReceiveProps(nextProps){
+        if (nextProps.d3TimeFormat !== this.props.d3TimeFormat){
+            this.parseTime = d3.timeParse(nextProps.d3TimeFormat);
+        }
+    }
+
+    componentDidUpdate(pastProps, pastState){
         // TODO: this.updateExistingChart();
     }
 
@@ -185,21 +223,62 @@ export class AreaChart extends React.PureComponent {
             this.setState({ 'drawingError' : true });
             return;
         }
-        var { margin } = this.props;
-        var svg     = d3.select(this.refs.svg),
-            width   = (this.props.width  || parseInt(svg.style('width' ))) - margin.left - margin.right,
-            height  = (this.props.height || parseInt(svg.style('height'))) - margin.top - margin.bottom;
+        var { margin, data } = this.props;
+        var svg         = d3.select(this.refs.svg),
+            width       = (this.props.width  || parseInt(svg.style('width' ))) - margin.left - margin.right,
+            height      = (this.props.height || parseInt(svg.style('height'))) - margin.top - margin.bottom,
+            drawn       = { svg };
 
         var x       = d3.scaleTime().rangeRound([0, width]),
-            y       = d3.scaleLinear.rangeRound([height, 0]);
+            y       = d3.scaleLinear().rangeRound([height, 0]);
+
+        // Convert timestamps to D3 date objects.
+        data = _.map(data, (d) => {
+            var formattedDate = (new Date(d.date.slice(0,10))).toISOString().slice(0,10);
+            return _.extend({}, d, {
+                'date' : this.parseTime(formattedDate),
+                'origDate' : formattedDate
+            });
+        });
+
+        x.domain(d3.extent(data, function(d){ return d.date; }));
+        y.domain([ 0, d3.max(data, function(d) { return d.total; }) ]);
 
         var area = d3.area()
             .x(function(d){ return x(d.date); })
-            .y1(function(d){ return y(d.close); });
+            .y1(function(d){ return y(d.total); })
+            .y0(y(0));
 
-        var g = svg.append("g").attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+        var bottomAxisGenerator = d3.axisBottom(x).ticks(d3.timeMonth.every(1)); // One tick a month
 
-        window.svg = svg;
+        drawn.g = svg.append("g").attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+        drawn.path = drawn.g.append('path')
+            .datum(data)
+            .attr('fill', 'steelblue')
+            .attr('d', area);
+
+        drawn.xAxis = drawn.g.append('g')
+            .attr("transform", "translate(0," + height + ")")
+            .call(bottomAxisGenerator);
+
+        drawn.yAxis = drawn.g.append('g')
+            .call(d3.axisLeft(y))
+            .append('text')
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", "0.71em")
+            .attr("text-anchor", "end")
+            .text("Count");
+
+        this.drawnD3Elements = drawn;
+    }
+
+    updateExistingChart(){
+
+        // TODO:
+        // If width or height has changed, transition existing DOM elements to larger dimensions
+        // If data has changed.... decide whether to re-draw graph or try to transition it.
 
     }
 
