80288086a364b51f1dd4316a913c7238691f7c50
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-07-30 12:14:27 -0400

    Ongoing - initial viz charts

--- src/encoded/static/components/static-pages/StatisticsPage.js
+++ src/encoded/static/components/static-pages/StatisticsPage.js
@@ -5,6 +5,7 @@ import PropTypes from 'prop-types';
 import _ from 'underscore';
 import { stringify } from 'query-string';
 import { console, layout, navigate, ajax } from'./../util';
+import { requestAnimationFrame } from './../viz/utilities';
 import * as globals from './../globals';
 import StaticPage from './StaticPage';
 import * as d3 from 'd3';
@@ -116,7 +117,7 @@ export class StatisticsChartsView extends React.Component {
         return (
             <div className="mt-5 mb-5 text-center">
                 <i className="icon icon-fw icon-spin icon-circle-o-notch icon-2x" style={{ opacity : 0.5 }}/>
-                <h5 className="text-400">Loading Data</h5>
+                <h5 className="text-400">Loading Chart Data</h5>
             </div>
         );
     }
@@ -131,13 +132,38 @@ export class StatisticsChartsView extends React.Component {
     }
 
     render(){
-        var { loadingStatus, mounted } = this.props;
+        var { loadingStatus, mounted, respFile, respExperimentSetReplicate } = this.props;
         if (!mounted || loadingStatus === 'loading')    return this.loadingIcon();
         if (loadingStatus === 'failed')                 return this.errorIcon();
         return (
-            <div className="stats-charts-container row">
-                <div className="col-xs-12 col-lg-6">
-                    <AreaChart data={aggregationsToChartData[0].function(this.props.respExperimentSetReplicate)} />
+            <div className="stats-charts-container">
+                <div className="row">
+
+                    <div className="col-xs-12 col-lg-6">
+                        <h4 className="text-300">
+                            <span className="text-500">Experiment Sets</span> released over time
+                        </h4>
+                        <AreaChart data={aggregationsToChartData[0].function(respExperimentSetReplicate)} />
+                    </div>
+
+                    <div className="col-xs-12 col-lg-6">
+                        <h4 className="text-300">
+                            <span className="text-500">Files</span> released over time
+                        </h4>
+                        <AreaChart data={aggregationsToChartData[1].function(respFile)} />
+                    </div>
+
+                </div>
+
+                <div className="row mt-3">
+
+                    <div className="col-xs-12 col-lg-6">
+                        <h4 className="text-300">
+                            <span className="text-500">Total File Size</span> released over time
+                        </h4>
+                        <AreaChart data={aggregationsToChartData[2].function(respFile)} yAxisScale="Pow" yAxisPower={2} yAxisLabel="MB" />
+                    </div>
+
                 </div>
             </div>
         );
@@ -148,7 +174,7 @@ export class StatisticsChartsView extends React.Component {
 export const aggregationsToChartData = [
     {
         'requires'  : 'ExperimentSetReplicate',
-        'title'     : '4DN Experiment Sets Public Releases',
+        'title'     : 'Experiment Set Public Releases',
         'function'  : function(resp){
             if (!resp.aggregations) return null;
             var weeklyIntervalBuckets = resp.aggregations.weekly_interval_public_release && resp.aggregations.weekly_interval_public_release.buckets;
@@ -162,7 +188,7 @@ export const aggregationsToChartData = [
                     'date'     : bucket.key_as_string.split('T')[0], // Sometimes we get a time back with date when 0 doc_count; correct it to date only.
                     'count'    : bucket.doc_count,
                     'total'    : total,
-                    'byAward' : _.map((bucket.group_by_award && bucket.group_by_award.buckets) || [], function(subBucket){
+                    'children' : _.map((bucket.group_by_award && bucket.group_by_award.buckets) || [], function(subBucket){
                         subTotals[subBucket.key] = (subTotals[subBucket.key] || 0) + subBucket.doc_count;
                         return {
                             'term'  : subBucket.key,
@@ -175,6 +201,47 @@ export const aggregationsToChartData = [
 
             return aggsList;
         }
+    },
+    {
+        'requires'  : 'File',
+        'title'     : 'File Public Releases',
+        'function'  : function(resp){
+            // Same as for ExpSets
+            return aggregationsToChartData[0].function(resp);
+        }
+    },
+    {
+        'requires'  : 'File',
+        'title'     : 'File Volume Released',
+        'function'  : function(resp){
+            if (!resp.aggregations) return null;
+            var weeklyIntervalBuckets = resp.aggregations.weekly_interval_public_release && resp.aggregations.weekly_interval_public_release.buckets;
+            if (!Array.isArray(weeklyIntervalBuckets) || weeklyIntervalBuckets.length < 2) return null;
+
+            var gigabyte = 1024 * 1024 * 1024,
+                total = 0,
+                subTotals = {},
+                aggsList = _.map(weeklyIntervalBuckets, function(bucket, index){
+                    var fileSizeVol = ((bucket.file_size_volume && bucket.file_size_volume.value) || 0) / gigabyte;
+                    total += fileSizeVol;
+                    return {
+                        'date'     : bucket.key_as_string.split('T')[0], // Sometimes we get a time back with date when 0 doc_count; correct it to date only.
+                        'count'    : fileSizeVol,
+                        'total'    : total,
+                        'children' : _.map((bucket.group_by_award && bucket.group_by_award.buckets) || [], function(subBucket){
+                            var subFileSizeVol = ((subBucket.file_size_volume && subBucket.file_size_volume.value) || 0) / gigabyte;
+                            subTotals[subBucket.key] = (subTotals[subBucket.key] || 0) + subFileSizeVol;
+                            return {
+                                'term'  : subBucket.key,
+                                'count' : subFileSizeVol,
+                                'total' : subTotals[subBucket.key]
+                            };
+                        })
+                    };
+                });
+
+            return aggsList;
+        }
     }
 ];
 
@@ -182,16 +249,20 @@ export const aggregationsToChartData = [
 export class AreaChart extends React.PureComponent {
 
     static defaultProps = {
-        'margin'        : { 'top': 20, 'right': 20, 'bottom': 30, 'left': 50 },
+        'margin'        : { 'top': 30, 'right': 20, 'bottom': 30, 'left': 50 },
         'data'          : null,
-        'd3TimeFormat'  : '%Y-%m-%d'
+        'd3TimeFormat'  : '%Y-%m-%d',
+        'stackChildren' : true,
+        'height'        : 300,
+        'yAxisLabel'    : 'Count',
+        'yAxisScale'    : 'Linear', // Must be one of 'Linear', 'Log', 'Pow'
+        'yAxisPower'    : null
     };
 
     constructor(props){
         super(props);
         this.drawNewChart = this.drawNewChart.bind(this);
         this.parseTime = d3.timeParse(props.d3TimeFormat);
-        this.drawnD3Elements = {};
         this.state = {
             'drawingError' : false,
             'drawn' : false
@@ -219,59 +290,122 @@ export class AreaChart extends React.PureComponent {
      * But this can probably wait (?).
      */
     drawNewChart(){
-        if (!this.refs || !this.refs.svg){
+        if (!this.refs || !this.refs.svg) {
             this.setState({ 'drawingError' : true });
             return;
         }
-        var { margin, data } = this.props;
-        var svg         = d3.select(this.refs.svg),
-            width       = (this.props.width  || parseInt(svg.style('width' ))) - margin.left - margin.right,
-            height      = (this.props.height || parseInt(svg.style('height'))) - margin.top - margin.bottom,
-            drawn       = { svg };
+        if (this.drawnD3Elements) {
+            console.error('Drawn chart already exists. Exiting.');
+            this.setState({ 'drawingError' : true });
+            return;
+        }
 
-        var x       = d3.scaleTime().rangeRound([0, width]),
-            y       = d3.scaleLinear().rangeRound([height, 0]);
+        requestAnimationFrame(()=>{
 
-        // Convert timestamps to D3 date objects.
-        data = _.map(data, (d) => {
-            var formattedDate = (new Date(d.date.slice(0,10))).toISOString().slice(0,10);
-            return _.extend({}, d, {
-                'date' : this.parseTime(formattedDate),
-                'origDate' : formattedDate
+            var { margin, data, stackChildren, yAxisLabel, yAxisScale, yAxisPower } = this.props;
+            var svg         = d3.select(this.refs.svg),
+                width       = (this.props.width  || parseInt(svg.style('width' ))) - margin.left - margin.right,
+                height      = (this.props.height || parseInt(svg.style('height'))) - margin.top - margin.bottom,
+                drawn       = { svg };
+
+            var x       = d3.scaleTime().rangeRound([0, width]),
+                y       = d3['scale' + yAxisScale]().rangeRound([height, 0]),
+                area    = d3.area()
+                    .x ( function(d){ return x(d.date || d.data.data);  } )
+                    .y1( function(d){ return Array.isArray(d) ? y(d[1]) : y(d.total || d.data.total); } )
+                    .y0( function(d){ return Array.isArray(d) ? y(d[0]) : y(0); } ),
+                stack   = d3.stack().value(function(d){ return d.total; }),
+                childKeys;
+
+            if (yAxisScale === 'Pow' && yAxisPower !== null){
+                y.exponent(yAxisPower);
+            }
+
+            // Convert timestamps to D3 date objects.
+            data = _.map(data, (d) => {
+                var formattedDate = (new Date(d.date.slice(0,10))).toISOString().slice(0,10);
+                return _.extend({}, d, {
+                    'date' : this.parseTime(formattedDate),
+                    'origDate' : formattedDate
+                });
             });
+
+            // Get child keys if needed.
+            childKeys = Array.from(_.reduce(data, function(m,d){
+                _.forEach(d.children || [], function(child){ m.add(child.term); });
+                return m;
+            }, new Set()));
+
+            x.domain(d3.extent(data, function(d){ return d.date; }));
+            y.domain([ 0, d3.max(data, function(d) { return d.total; }) ]);
+            if (childKeys.length > 0){
+                stack.keys(childKeys);
+                console.log('CXCCC', childKeys, stack(data));
+            }
+
+            var bottomAxisGenerator = d3.axisBottom(x)
+                .ticks(d3.timeMonth.every(2)); // One tick every 2 months
+
+            var rightAxisGenerator = d3.axisRight(y).tickSize(width),
+                rightAxisFxn = function(g){
+                    g.call(rightAxisGenerator);
+                    g.select('.domain').remove();
+                    g.selectAll('.tick > text').remove();
+                    g.selectAll('.tick > line')
+                        .attr('opacity', 0.2)
+                        .attr("stroke", "#777")
+                        .attr("stroke-dasharray", "2,2");
+                };
+
+            drawn.g = svg.append("g").attr('transform', "translate(" + margin.left + "," + margin.top + ")");
+
+            console.log('CXCCC', stack(data));
+        
+            //drawn.layer = drawn.g.selectAll('.layer')
+            //    .data(data)
+            //    .enter().append('g').attr('class', 'layer');
+
+            drawn.path = drawn.g.append('path')
+                .datum(data)
+                .attr('fill', 'steelblue')
+                .attr('d', area);
+
+            drawn.xAxis = drawn.g.append('g')
+                .attr("transform", "translate(0," + height + ")")
+                .call(bottomAxisGenerator);
+
+            drawn.yAxis = drawn.g.append('g')
+                .call(d3.axisLeft(y))
+                .append('text')
+                .attr("fill", "#000")
+                .attr("x", 0)
+                //.attr("y", -12)
+                //.attr('transform', 'rotate(-45)')
+                .attr("y", -20)
+                .attr("dy", "0.71em")
+                //.attr("text-anchor", "start")
+                .attr("text-anchor", "end")
+                .text(yAxisLabel);
+
+            drawn.rightAxis = drawn.g.append('g').call(rightAxisFxn);
+
+            this.drawnD3Elements = drawn;
+
         });
+    }
 
-        x.domain(d3.extent(data, function(d){ return d.date; }));
-        y.domain([ 0, d3.max(data, function(d) { return d.total; }) ]);
-
-        var area = d3.area()
-            .x(function(d){ return x(d.date); })
-            .y1(function(d){ return y(d.total); })
-            .y0(y(0));
-
-        var bottomAxisGenerator = d3.axisBottom(x).ticks(d3.timeMonth.every(1)); // One tick a month
-
-        drawn.g = svg.append("g").attr('transform', "translate(" + margin.left + "," + margin.top + ")");
-        drawn.path = drawn.g.append('path')
-            .datum(data)
-            .attr('fill', 'steelblue')
-            .attr('d', area);
-
-        drawn.xAxis = drawn.g.append('g')
-            .attr("transform", "translate(0," + height + ")")
-            .call(bottomAxisGenerator);
-
-        drawn.yAxis = drawn.g.append('g')
-            .call(d3.axisLeft(y))
-            .append('text')
-            .attr("fill", "#000")
-            .attr("transform", "rotate(-90)")
-            .attr("y", 6)
-            .attr("dy", "0.71em")
-            .attr("text-anchor", "end")
-            .text("Count");
-
-        this.drawnD3Elements = drawn;
+    /**
+     * Use to delete SVG before drawing a new one,
+     * e.g. in response to a _big_ change that can't easily 'update'.
+     */
+    destroyExistingChart(){
+        var drawn = this.drawnD3Elements;
+        if (!drawn || !drawn.svg) {
+            console.error('No D3 SVG to clear.');
+            return;
+        }
+        drawn.svg.selectAll('*').remove();
+        delete this.drawnD3Elements;
     }
 
     updateExistingChart(){
@@ -283,6 +417,7 @@ export class AreaChart extends React.PureComponent {
     }
 
     render(){
+        var { data, width, height } = this.props;
         if (!this.props.data || this.state.drawingError) {
             return <div>Error</div>;
         }
