ca004ca300953d44ee0f22aa0735a3a036ce6d28
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-08-15 14:40:41 -0400

    Added a couple small index-data tests to test_embedding

--- src/encoded/tests/test_embedding.py
+++ src/encoded/tests/test_embedding.py
@@ -39,6 +39,7 @@ def test_linked_uuids_object(content, dummy_request, threadlocals):
     # needed to track _linked_uuids
     dummy_request._indexing_view = True
     dummy_request.embed('/testing-link-sources/', sources[0]['uuid'], '@@object')
+    # only object visited here is the source itself, hence one _linked_uuid
     assert dummy_request._linked_uuids == {'16157204-8c8f-4672-a1a4-14f4b8021fcd'}
 
 
@@ -47,6 +48,20 @@ def test_linked_uuids_embedded(content, dummy_request, threadlocals):
     dummy_request._indexing_view = True
     dummy_request.embed('/testing-link-sources/', sources[0]['uuid'], '@@embedded')
     assert dummy_request._linked_uuids == {'16157204-8c8f-4672-a1a4-14f4b8021fcd', '775795d3-4410-4114-836b-8eeecf1d0c2f'}
+
+
+def test_target_rev_linked_uuids_indexing_view(content, dummy_request, threadlocals):
+    res_target = dummy_request.embed('/testing-link-targets/', targets[0]['uuid'], '@@index-data', as_user='INDEXER')
+    # should have the itself and the rev link to source in the _linked_uuids
+    assert dummy_request._linked_uuids == {'16157204-8c8f-4672-a1a4-14f4b8021fcd', '775795d3-4410-4114-836b-8eeecf1d0c2f'}
+    assert res_target['uuids_rev_linked_to_me'] == []
+
+
+def test_source_rev_linked_uuids_indexing_view(content, dummy_request, threadlocals):
+    res_target = dummy_request.embed('/testing-link-sources/', sources[0]['uuid'], '@@index-data', as_user='INDEXER')
+    # should have the itself and the rev link to source in the _linked_uuids
+    assert dummy_request._linked_uuids == {'16157204-8c8f-4672-a1a4-14f4b8021fcd', '775795d3-4410-4114-836b-8eeecf1d0c2f'}
+    assert res_target['uuids_rev_linked_to_me'] == ['775795d3-4410-4114-836b-8eeecf1d0c2f']
 
 
 def test_linked_uuids_experiment(experiment, lab, award, human_biosample, human_biosource, mboI, dummy_request, threadlocals):
