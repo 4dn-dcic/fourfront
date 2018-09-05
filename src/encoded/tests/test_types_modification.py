9b725042d29cc5063161f5849987b50b45edc35a
Sarah <sarah_reiff@hms.harvard.edu>
2018-09-05 12:56:12 -0400

    Edited test for modification_name

--- src/encoded/tests/test_types_modification.py
+++ src/encoded/tests/test_types_modification.py
@@ -16,12 +16,15 @@ def modifications(basic_modification, mod_w_genomic_change, mod_w_target,
 def test_calculated_modification_name(testapp, modifications):
     for name in modifications:
         modname = modifications[name]['modification_name']
-        assert modifications[name]['modification_name_short'] == modname
+        short = modifications[name]['modification_name_short']
+        # assert modifications[name]['modification_name_short'] == modname
         if name == 'basic_mod':
-            assert modname == 'Crispr'
+            assert modname == 'Crispr' and short == 'Crispr'
         if name == 'mod_w_gen_chg':
-            assert modname == 'Crispr deletion'
+            assert modname == 'Crispr deletion' and short == 'deletion'
         if name == 'mod_w_target':
             assert modname == 'Crispr for Gene:eeny,meeny'
+            assert short == 'Gene:eeny,meeny Crispr'
         if name == 'mod_w_both':
             assert modname == 'Crispr deletion for Gene:eeny,meeny'
+            assert short == 'Gene:eeny,meeny deletion'
