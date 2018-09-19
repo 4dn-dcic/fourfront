85163fd9e52fc1ec3bfb0983a75ba52db06a03d0
Chad Serrant <chad_serrant@hms.harvard.edu>
2018-09-19 11:40:46 -0400

    text_external_sets_count: Changed text size for External Sets count.

--- src/encoded/static/components/browse/BrowseView.js
+++ src/encoded/static/components/browse/BrowseView.js
@@ -332,8 +332,8 @@ class ExternaDataExpSetsCount extends React.PureComponent {
         return (
             <div className="above-results-table-row text-right">
                 <span className="inline-block mt-18">
-                    <span className="text-500">{ countExternalSets }</span> { browseBaseState === 'all' ? 'fewer' : 'more' } { "set" + (countExternalSets > 1 ? 's' : '') }{ browseBaseState === 'all' ? '' : ' available' } in <a href="#" onClick={this.onBrowseStateToggle}>{ browseBaseState === 'all' ? '4DN-only Data' : 'External Data' }</a>.
-                </small>
+                    <span className="text-600 text-large">{ countExternalSets }</span> { browseBaseState === 'all' ? 'fewer' : 'more' } { "set" + (countExternalSets > 1 ? 's' : '') }{ browseBaseState === 'all' ? '' : ' available' } in <a href="#" onClick={this.onBrowseStateToggle}>{ browseBaseState === 'all' ? '4DN-only Data' : 'External Data' }</a>.
+                </span>
             </div>
         );
     }
