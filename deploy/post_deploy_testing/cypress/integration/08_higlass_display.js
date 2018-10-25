a4ee786fa538d80fd7d78dc3154e7480591fa5db
Chad Serrant <chad_serrant@hms.harvard.edu>
2018-10-25 15:33:37 -0400

    viewconf_item: Moving inserts to all servers, removing unit tests.

--- deploy/post_deploy_testing/cypress/integration/08_higlass_display.js
+++ deploy/post_deploy_testing/cypress/integration/08_higlass_display.js
@@ -6,17 +6,12 @@ import _ from 'underscore';
 * Test you can visit the Higlass Display page.
 */
 describe("HiGlass Display pages", function(){
-
     context('Higlass Display summary page', function(){
         it('Can visit HiGlass Display summary page without login', function(){
             // Visit the page and confirm you can see the table and facet properties.
             cy.visit('/higlass-view-configs').wait(100).end()
                 .get(".search-headers-column-block span.column-title").should('have.text', ['Title', 'Creator', 'Date Created', 'Date Modified'].join('')).end()
                 .get(".facets-header .facets-title").should('have.text', 'Properties');
-
-            // You should be able to see the released higlass view in the table.
-            const releasedUrl = "/higlass-view-configs/00000000-1111-0000-1111-000000000001/";
-            cy.get("a[href='" + releasedUrl + "']");
 
             // You should NOT be able to see the draft higlass view.
             const draftUrl = "/higlass-view-configs/00000000-1111-0000-1111-000000000002/";
@@ -27,10 +22,6 @@ describe("HiGlass Display pages", function(){
             // Log in, visit the page and look for the create button
             cy.visit('/higlass-view-configs').login4DN().wait(500).end()
                 .get(".create-add-button .btn").should('have.text', 'Create');
-
-            // You should be able to see the released higlass view in the table.
-            const releasedUrl = "/higlass-view-configs/00000000-1111-0000-1111-000000000001/";
-            cy.get("a[href='" + releasedUrl + "']");
 
             // You should be able to see the draft higlass view.
             const draftUrl = "/higlass-view-configs/00000000-1111-0000-1111-000000000002/";
@@ -74,6 +65,9 @@ describe("HiGlass Display pages", function(){
 
                     // Delete the newly created record.
                     _.forEach(response.body["@graph"], (higlassViewConf)=>{
+                        if (higlassViewConf.submitted_by && higlassViewConf.submitted_by.display_title !== "4dn DCIC") {
+                            return;
+                        }
                         if (['00000000-1111-0000-1111-000000000001', '00000000-1111-0000-1111-000000000002'].indexOf(higlassViewConf.uuid) !== -1) {
                             return;
                         }
