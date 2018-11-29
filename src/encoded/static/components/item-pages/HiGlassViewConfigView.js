fc6ecce5d3b937a2c59a04260d8d3889adb903a2
Alexander Balashov <alexander_balashov@hms.harvard.edu>
2018-11-29 18:17:00 -0500

    Update HiGlassViewConfigView.js

--- src/encoded/static/components/item-pages/HiGlassViewConfigView.js
+++ src/encoded/static/components/item-pages/HiGlassViewConfigView.js
@@ -628,7 +628,7 @@ class AddFileButton extends React.PureComponent {
             tooltip         = "Search for a file and add it to the display.",
             dropMessage     = "Drop a File here.",
             searchURL       = (
-                '/search/?type=FileProcessed&higlass_uid!=No+value'
+                '/search/?type=File&higlass_uid!=No+value'
                 + (genome_assembly? '&genome_assembly=' + encodeURIComponent(genome_assembly) : '' )
                 + '#!selection'
             );
