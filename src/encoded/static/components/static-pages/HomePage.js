42f50afec7d26dc7deb10377592aa692ba2bffb6
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-12-01 02:40:24 -0500

    Disabling wrap-around re: flickering, styling, handling < 6 slides

--- src/encoded/static/components/static-pages/HomePage.js
+++ src/encoded/static/components/static-pages/HomePage.js
@@ -71,7 +71,7 @@ export default class HomePage extends React.PureComponent {
             <div className="home-content-area">
                 { this.midHeader() }
                 {/* <h2 className="homepage-section-title">Introduction</h2> */}
-                <HomePageCarousel {..._.pick(this.props, 'windowWidth')} />
+                <HomePageCarousel  />
                 <div className="row">
                     <div className="col-xs-12 col-md-8">
                         <h2 className="homepage-section-title">Introduction</h2>
@@ -82,7 +82,7 @@ export default class HomePage extends React.PureComponent {
                     </div>
                 </div>
                 <div className="mt-4">
-                    { this.announcements() }
+                    {/* this.announcements() */}
                 </div>
             </div>
         );
