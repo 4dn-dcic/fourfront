64f0a96ee8bff27165bdcf9b15b0396bb93bac29
Carl Vitzthum <carl.vitzthum@gmail.com>
2018-12-03 14:23:56 -0500

    Moved JupyterHub email updating for user to _update, so that the email can be updated on an edit

--- src/encoded/types/user.py
+++ src/encoded/types/user.py
@@ -26,9 +26,11 @@ from snovault.schema_utils import validate_request
 from snovault.crud_views import collection_add
 from snovault.calculated import calculate_properties
 from snovault.resource_views import item_view_page
-
+from dcicutils.s3_utils import s3Utils
+import requests
 import logging
-logging.getLogger('boto3').setLevel(logging.WARNING)
+
+logging.getLogger('boto3').setLevel(logging.INFO)
 log = logging.getLogger(__name__)
 
 ONLY_ADMIN_VIEW_DETAILS = [
@@ -142,6 +144,27 @@ class User(Item):
             }
         # sort alphabetically by title
         properties['subscriptions'] = sorted(list(curr_subs_dict.values()), key= lambda v:v['title'])
+
+        # if we are on webprod/webprod2, make sure there is an account for the
+        # user that reflects any email changes
+        ff_env = self.registry.settings.get('env.name')
+        # compare previous and updated emails, respectively
+        if self.properties.get('email') != properties.get('email'):
+            update_email = properties.get('email')
+        else:
+            update_email = None
+        if ff_env is not None and update_email is not None and 'webprod' in ff_env:
+            s3Obj = s3Utils(env='data')
+            jh_key = s3Obj.get_jupyterhub_key()
+            jh_endpoint = ''.join([jh_key['server'], '/hub/api/users/', update_email])
+            jh_headers = {'Authorization': 'token %s' % jh_key['secret']}
+            try:
+                res = requests.post(jh_endpoint, headers=jh_headers)
+            except Exception as jh_exc:
+                log.warning('Error posting user %s to JupyterHub' % update_email,
+                            error=str(jh_exc))
+            else:
+                log.info('Updating user %s on JupyterHub. Result: %s' % (update_email, res.text))
         super(User, self)._update(properties, sheets)
 
 
@@ -167,7 +190,6 @@ def user_add(context, request):
     if we have a password in our request, create and auth entry
     for the user as well
     '''
-    import pdb; pdb.set_trace()
     # do we have valid data
     pwd = request.json.get('password', None)
     pwd_less_data = request.json.copy()
@@ -193,24 +215,6 @@ def user_add(context, request):
 
             import transaction
             transaction.commit()
-
-    # if we are on webprod/webprod2, also create a Jupyterhub account for user.
-    # this needs to be done because otherwise the user won't show up on JH
-    # until the server is restarted
-    ff_env = request.registry.settings.get('env.name')
-    if 'webprod' in ff_env and request.json.get('email'):
-        import requests
-        from dcicutils.s3_utils import s3Utils
-        s3Obj = s3Utils(env='data')
-        jh_key = s3Utils.get_jupyterhub_key()
-        jh_endpoint = ''.join([jh_key['server'], '/hub/api/users/', request.json['email']])
-        jh_headers = {'Authorization': 'token %s' % jh_key['secret']}
-        try:
-            requests.post(jh_endpoint, headers=jh_headers)
-        except Exception as jh_exc:
-            log.warning('Error posting user %s to Jupyterhub' % request.json['email'],
-                        error=str(jh_exc))
-
     return result
 
 
