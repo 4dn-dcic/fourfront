544d32b8b304a358f7cc92820a1102dec688889e
Alex Balashov <alexander_balashov@hms.harvard.edu>
2018-12-12 09:57:10 -0500

    Update impersonate-user form URI

--- src/encoded/static/components/viz/QuickInfoBar.js
+++ src/encoded/static/components/viz/QuickInfoBar.js
@@ -33,6 +33,7 @@ export default class QuickInfoBar extends React.PureComponent {
         // If have href, only show for /browse/, /search/, and / & /home
         var urlParts = url.parse(href);
         if (urlParts.hash && urlParts.hash.indexOf('!impersonate-user') > -1) return true;
+        if (urlParts.search && urlParts.search.indexOf('currentAction=impersonate-user') > -1) return true;
         // Doing replace twice should be faster than one time with /g regex flag (3 steps each or 15 steps combined w/ '/g')
         var pathParts = urlParts.pathname.replace(/^\//, "").replace(/\/$/, "").split('/');
         if (pathParts[0] === 'browse') return false;
