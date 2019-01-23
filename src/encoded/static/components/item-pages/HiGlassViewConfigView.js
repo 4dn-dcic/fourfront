0e3f8dfb9837e3c498026148641894b4d1fea920
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-01-23 16:48:58 -0500

    Export/cache viewConf upon tab change (unfinished-ish)

--- src/encoded/static/components/item-pages/HiGlassViewConfigView.js
+++ src/encoded/static/components/item-pages/HiGlassViewConfigView.js
@@ -87,14 +87,29 @@ export class HiGlassViewConfigTabView extends React.PureComponent {
     }
 
     componentWillReceiveProps(nextProps){
-        // TODO: Improve, use var = nextState and conditionally set if have been updated,
-        // then setState(nextState) if keys.length > 0.
+        var nextState = {};
+
         if (nextProps.viewConfig !== this.props.viewConfig){
-            this.setState({
-                'originalViewConfig' : null, //object.deepClone(nextProps.viewConfig)
+            _.extend(nextState, {
+                'originalViewConfig' : null, //object.deepClone(nextProps.viewConfig) // Not currently used.
                 'viewConfig' : nextProps.viewConfig,
                 'genome_assembly' : (nextProps.context && nextProps.context.genome_assembly) || this.state.genome_assembly || null
             });
+        } else if (nextProps.href !== this.props.href){
+            // If component is still same instance, then is likely that we're changing
+            // the URI hash as a consequence of changing tabs.
+            // Export & save viewConfig from HiGlassComponent internal state to our own to preserve contents.
+            var hgc                 = this.getHiGlassComponent(),
+                currentViewConfStr  = hgc && hgc.api.exportAsViewConfString(),
+                currentViewConf     = currentViewConfStr && JSON.parse(currentViewConfStr);
+
+            currentViewConf && _.extend(nextState, {
+                'viewConfig' : currentViewConf
+            });
+        }
+
+        if (_.keys(nextState).length > 0){
+            this.setState(nextState);
         }
     }
 
