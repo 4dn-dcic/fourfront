3df92c2d6d227d5299dd04c9aa9e8fc898b96f1f
aschroed <andrew_schroeder@hms.harvard.edu>
2019-01-28 11:13:38 -0500

    tweaks to gene schema; gene _update method

--- src/encoded/types/gene.py
+++ src/encoded/types/gene.py
@@ -3,6 +3,8 @@
     most data fetched from ncbi entrez gene"""
 import requests
 import json
+import os
+from pyramid.path import DottedNameResolver
 from snovault import (
     collection,
     load_schema,
@@ -18,12 +20,42 @@ from html.parser import HTMLParser
 ################################################
 # Outside methods for online data fetch
 ################################################
+def get_gene_info_from_4dn_gene_info_file(geneid):
+
+
+def get_gene_info_from_response_text(response):
+    # just get the stuff between the pre tag
+    _, info, _ = response.split('pre>')
+    info = info.replace('</', '')
+    lines = info.split('\n')
+    if len(lines) != 2:
+        return {}
+    fields = lines[0].split('\t')
+    values = lines[1].split('\t')
+    return dict(zip(fields, values))
+
+
+def fetch_gene_info_from_ncbi(geneid):
+    NCBI = "https://www.ncbi.nlm.nih.gov/gene/{id}?report=tabular&format=text".format(id=geneid)
+    for count in range(5):
+        resp = requests.get(NCBI)
+        if resp.status_code == 200:
+            break
+        if resp.status_code == 429:
+            time.sleep(5)
+            continue
+        if count == 4:
+            return {}
+    text = resp.text
+    if '<pre>' not in text:
+        return {}
+    return get_gene_dict_from_response_text(text)
 
 
 @collection(
     name='genes',
     unique_key='gene:geneid',
-    lookup_key='preferred_symbol'
+    lookup_key='preferred_symbol',
     properties={
         'title': 'Genes',
         'description': 'Entrez gene items',
@@ -41,6 +73,12 @@ class Gene(Item):
             if symbol:
                 properties['preferred_symbol'] = termname
 
+        # fetch info from ncbi gene based on id provided
+        geneid = properties.get('geneid')
+        try:
+            gene_info = fetch_gene_info_from_ncbi(geneid)
+        except Exception as e:
+            pass
         super(Gene, self)._update(properties, sheets)
         return
 
