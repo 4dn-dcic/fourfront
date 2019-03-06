7e24358555177e04a288035d51189da44b18b67d
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-03-06 14:58:38 -0500

    Ongoing command fixes

--- src/encoded/commands/update_inserts_from_server.py
+++ src/encoded/commands/update_inserts_from_server.py
@@ -12,13 +12,14 @@ logger = structlog.getLogger(__name__)
 EPILOG = __doc__
 
 
-def read_local_inserts_dir(path, target_types=[]):
+def read_local_inserts_dir(dir, path, target_types=[]):
     """
     Given path string path, read local inserts directory and return a
     dictionary of all inserts keyed by item type, as well as a list of all
     found uuids
 
     Args:
+        dir (str): string name of the inserts directory
         path (str): string path to the inserts directory
         target_types (list): list of item types to load. Empty means all types
 
@@ -36,24 +37,17 @@ def read_local_inserts_dir(path, target_types=[]):
         if bad_item_types:
             raise Exception('Specified item type(s) %s are not found in the inserts '
                             'dir. Found: %s' % (bad_item_types, item_types))
-    # find item types that are represented in the given inserts path
-    for (dirpath, dirnames, filenames) in walk(inserts_path):
-        item_types = [it[:-5] for it in filenames if it.endswith('.json')]
-    if args.item_type:
-        bad_item_types = [it for it in args.item_type if it not in item_types]
-        if bad_item_types:
-            raise Exception('Specified item type(s) %s are not found in the inserts '
-                            'dir. Found: %s' % (bad_item_types, item_types))
     # update item_types if user specified specific ones
-    fetch_item_types = args.item_type if args.item_type else item_types
+    fetch_item_types = target_types if target_types else item_types
     # load current insert contents from json file
     for item_type in item_types:
         local_inserts[item_type] = {}  # key these by uuid for now
-        for it_item in get_inserts(args.dest, item_type):
+        for it_item in get_inserts(dir, item_type):
             # only fetch items for specified fetch_item_types
             if item_type in fetch_item_types:
                 item_uuids.append(it_item['uuid'])
             local_inserts[item_type][it_item['uuid']] = it_item
+    return local_inserts, item_uuids
 
 
 def main():
@@ -79,16 +73,16 @@ def main():
 
     args = parser.parse_args()
     # this will work since bin/ commands are run from root FF directory
-    inserts_dir = 'src/encoded/tests/data'
+    inserts_location = 'src/encoded/tests/data'
     # hardcode these to prevent accidental creation of inserts files
     inserts_files = ['inserts', 'master-inserts', 'perf-testing',
                      'workbook-inserts', 'temp-local-inserts']
     if args.dest not in inserts_files:
         raise Exception('Specified inserts destination %s must be one of: %s'
                         % (args.dest, inserts_files))
-    inserts_path = '/'.join([inserts_dir, args.dest])
+    inserts_path = '/'.join([inserts_location, args.dest])
 
-    local_inserts, item_uuids = read_local_inserts_dir(inserts_path, args.item_type)
+    local_inserts, item_uuids = read_local_inserts_dir(args.dest, inserts_path, args.item_type)
 
     # now find uuids and all linked from the given server
     # ignore attachments and add workflow runs related to file_fastq/file_processed
@@ -103,8 +97,8 @@ def main():
     # if we are updating `inserts`, must make sure that items don't conflict
     # with those in `master-inserts`
     if args.dest == 'inserts':
-        master_path = inserts_path = '/'.join([inserts_dir, 'master-inserts'])
-        master_inserts, master_uuids = read_local_inserts_dir(master_path)
+        master_path = '/'.join([inserts_location, 'master-inserts'])
+        master_inserts, master_uuids = read_local_inserts_dir('master-inserts', master_path)
         item_conflict_report = {}
         del master_uuids  # for codacy
         for item_type in master_inserts:
@@ -123,13 +117,13 @@ def main():
                     del svr_inserts[item_type][conflict]
                     itype_okay.append(conflict)
             item_conflict_report[item_type] = {'error': itype_err, 'okay': itype_okay}
-        if any([it for it in item_conflict_report if it['error']]):
-            error_report = {it: it['error'] for it in item_conflict_report}
+        if any([it for it in item_conflict_report if item_conflict_report[it]['error']]):
+            error_report = {it: item_conflict_report[it]['error'] for it in item_conflict_report}
             raise Exception('Cannot update the following items in "inserts" directory,'
                             ' since there are conflicting items with different values'
                             'in the master-inserts. Update those first. Conflicts: %s' % error_report)
-        elif any([it for it in item_conflict_report if it['okay']):
-            conflict_report = {it: it['okay'] for it in item_conflict_report}
+        elif any([it for it in item_conflict_report if item_conflict_report[it]['okay']]):
+            conflict_report = {it: item_conflict_report[it]['okay'] for it in item_conflict_report}
             logger.warning('The following items are already in "master-inserts".'
                            ' Will not add to "inserts". Items:' % conflict_report)
 
