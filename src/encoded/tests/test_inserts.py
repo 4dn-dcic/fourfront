b47bb79f26a032e76a8725e28f6d643f1c7d1bab
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-03-07 13:30:27 -0500

    Reformated print statements in test_inserts.py

--- src/encoded/tests/test_inserts.py
+++ src/encoded/tests/test_inserts.py
@@ -8,14 +8,14 @@ def test_check_wf_links():
     software_inserts = list(get_inserts('inserts', 'software'))
     fileformat_inserts = list(get_inserts('master-inserts', 'file_format'))
     check_wf_links(workflow_inserts, software_inserts, fileformat_inserts)
-   
+
 
 def check_wf_links(workflow_inserts, software_inserts, fileformat_inserts):
     # get all sf inserts uuid
     all_sf = []
     for sf in software_inserts:
         all_sf.append(sf['uuid'])
-    
+
     # get all file format inserts uuid and format name
     all_ff = []
     for ff in fileformat_inserts:
@@ -23,36 +23,38 @@ def check_wf_links(workflow_inserts, software_inserts, fileformat_inserts):
             all_ff.append(ff['uuid'])
         if 'file_format' in ff:
             all_ff.append(ff['file_format'])
-    
+
     # check workflow inserts
     for wf in workflow_inserts:
         # compare software
         for st in wf.get('steps', []):
             if 'software_used' in st.get('meta', {}):
                 for sf in st['meta']['software_used']:
-                    assert parse_software_uuid(sf) in all_sf
-                    # if parse_software_uuid(sf) not in all_sf:
-                    #     print(parse_software_uuid(sf))
+                    parsed_sf = parse_software_uuid(sf)
+                    if parsed_sf not in all_sf:
+                        print('Could not find software %s from workflow %s' % (parsed_sf, wf['uuid']))
+                    assert parsed_sf in all_sf
         # compare file format
         # file format in arguments
         for arg in wf.get('arguments', []):
             if 'argument_format' in arg:
+                if arg['argument_format'] not in all_ff:
+                    print('Could not find file_format %s from workflow %s' % (arg['argument_format'], wf['uuid']))
                 assert arg['argument_format'] in all_ff
-                # if arg['argument_format'] not in all_ff:
-                #     print(arg['argument_format'])
         # file format in step input/output
         for st in wf.get('steps', []):
             for ip in st.get('inputs', []):
                 if 'file_format' in ip.get('meta', {}):
+                    if ip['meta']['file_format'] not in all_ff:
+                        print('Could not find file_format %s from workflow %s' % (ip['meta']['file_format'], wf['uuid']))
                     assert ip['meta']['file_format'] in all_ff
-                    # if ip['meta']['file_format'] not in all_ff:
-                    #     print(ip['meta']['file_format'])
             for op in st.get('outputs', []):
                 if 'file_format' in op.get('meta', {}):
+                    if op['meta']['file_format'] not in all_ff:
+                        print('Could not find file_format %s from workflow %s' % (op['meta']['file_format'], wf['uuid']))
                     assert op['meta']['file_format'] in all_ff
-                    # if op['meta']['file_format'] not in all_ff:
-                    #     print(op['meta']['file_format'])
-                
+
+
 def parse_software_uuid(s):
     """parses '/software/lalala/' or '/software/lalala' into 'lalala'
     if input is already 'lalala', returns 'lalala' as well.
@@ -65,4 +67,3 @@ def parse_software_uuid(s):
         return(ss)
     else:
         return(None)
-
