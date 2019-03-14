c49ce06ee9040c302c317ea015438815a47382dd
Sarah <sarah_reiff@hms.harvard.edu>
2019-03-14 17:06:50 -0400

    Removed commented out lines

--- src/encoded/static/components/item-pages/ExperimentTypeView.js
+++ src/encoded/static/components/item-pages/ExperimentTypeView.js
@@ -47,55 +47,8 @@ export default class ExperimentTypeView extends DefaultItemView {
             <Protocols.SopBelowHeaderRow sop={this.props.context.sop} />
         ];
     }
-
+    
 }
-
-
-// /**
-//  * This is rendered in middle of ExperimentView, between Item header and TabbedView.
-//  * @see ExperimentTypeView.itemMidSection()
-//  */
-// class OverviewHeading extends React.Component {
-//     render(){
-//         var { context, schemas } = this.props,
-//             tips = object.tipsFromSchema(schemas || Schemas.get(), context), // In form of { 'description' : {'title', 'description', 'type'}, 'experiment_type' : {'title', 'description', ...}, ... }
-//             commonProps = {
-//                 'tips'          : tips,                 // Object containing 'properties' from Schema for Experiment ItemType. Informs the property title (from schema) & tooltip you get when hover over property title. Obtained from schemas.
-//                 'result'        : context,              // The Item from which are getting value for 'property'.
-//                 'wrapInColumn'  : "col-xs-6 col-md-3"   // Optional. Size of the block. @see http://getbootstrap.com/docs/3.3/examples/grid/.
-//             };
-//
-//         return (
-//             <OverviewHeadingContainer>
-//                 <OverViewBodyItem {...commonProps} property='sop' fallbackTitle="4DN-Approved SOP" />
-//                 <OverViewBodyItem {...commonProps} property='raw_file_types' fallbackTitle="Raw Files Available" fallbackValue="No" />
-//                 <OverViewBodyItem {...commonProps} property='reference_pubs' fallbackTitle="Reference Publication" />
-//             </OverviewHeadingContainer>
-//         );
-//     }
-// }
-
-
-// /**
-//  * This is rendered in middle of ExperimentView, between Item header and TabbedView.
-//  * @see ExperimentTypeView.itemMidSection()
-//  */
-// class OverviewHeading extends React.Component {
-//     render(){
-//         if (!this.props.context.sop) return null;
-//         return (
-//             <div className="row mb-2">
-//                 <div className="col-sm-12">
-//                     <DetailBlock protocol={this.props.context.sop} singularTitle="Approved SOP" >
-//                         <div className="more-details">
-//                             <ShortAttribution protocol={this.props.context.sop} />
-//                         </div>
-//                     </DetailBlock>
-//                 </div>
-//             </div>
-//         );
-//     }
-// }
 
 
 class ExperimentTypeViewOverview extends React.Component {
@@ -140,9 +93,7 @@ class ExperimentTypeViewOverview extends React.Component {
                 ) : null }
             </div>
         );
-
     }
-
 }
 
 
@@ -151,16 +102,17 @@ class OverViewBody extends React.Component {
     render(){
         var result = this.props.result;
         var tips = object.tipsFromSchema(this.props.schemas || Schemas.get(), result);
+        var commonProps = { 'result' : result, 'tips' : tips, 'wrapInColumn' : 'col-sm-6 col-md-3' };
 
         return (
             <div className="row">
                 <div className="col-md-12 col-xs-12">
                     <div className="row overview-blocks">
 
-                        <OverViewBodyItem {...{ result, tips }} property='experiment_category' fallbackTitle="Experiment Category" wrapInColumn />
-                        <OverViewBodyItem {...{ result, tips }} property='assay_classification' fallbackTitle="Assay Classification" wrapInColumn />
-                        <OverViewBodyItem {...{ result, tips }} property='assay_subclassification' fallbackTitle="Experimental Purpose" wrapInColumn />
-                        <OverViewBodyItem {...{ result, tips }} property='raw_file_types' fallbackTitle="Raw Files Available" wrapInColumn />
+                        <OverViewBodyItem {...commonProps} property='experiment_category' fallbackTitle="Experiment Category" wrapInColumn />
+                        <OverViewBodyItem {...commonProps} property='assay_classification' fallbackTitle="Assay Classification" wrapInColumn />
+                        <OverViewBodyItem {...commonProps} property='assay_subclassification' fallbackTitle="Experimental Purpose" wrapInColumn />
+                        <OverViewBodyItem {...commonProps} property='raw_file_types' fallbackTitle="Raw Files Available" wrapInColumn />
 
                     </div>
                 </div>
