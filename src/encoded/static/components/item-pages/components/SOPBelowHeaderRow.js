b728bf66485c5dd2b98fc9c1e31458de536d3102
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-03-15 15:51:25 -0400

    Styling

--- src/encoded/static/components/item-pages/components/SOPBelowHeaderRow.js
+++ src/encoded/static/components/item-pages/components/SOPBelowHeaderRow.js
@@ -12,9 +12,9 @@ export class SOPBelowHeaderRow extends React.PureComponent {
         if (!sop) return null;
         return (
             <div className="mb-2">
-                <FormattedInfoWrapper singularTitle="Approved SOP" isSingleItem>
-                    <h5 className="block-title">{ object.itemUtil.generateLink(sop) }</h5>
-                    <div className="details text-ellipsis-container">{ sop.description }</div>
+                <FormattedInfoWrapper singularTitle="Approved SOP" isSingleItem noDetails={!sop.description}>
+                    <h5 className={"block-title" + ( sop.description ? '' : ' mt-08' )}>{ object.itemUtil.generateLink(sop) }</h5>
+                    { sop.description && <div className="more-details">{ sop.description }</div> }
                 </FormattedInfoWrapper>
             </div>
         );
