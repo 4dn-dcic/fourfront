3c2a589d19165ae4515f9403098812000024a318
Alex Balashov <alex.balashov@gmail.com>
2019-03-20 12:49:44 -0400

    Ongoing...

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp        = require('gulp'),
     PluginError = require('plugin-error'),
     log         = require('fancy-log'),
-    webpack     = require('webpack');
+    webpack     = require('webpack'),
+    sass        = require('node-sass');
 
 
 var setProduction = (done) => {
@@ -45,14 +46,42 @@ var watch = () => {
 };
 
 
+
+var doSassBuild = (done) => {
+    sass.render({
+          file: './src/encoded/static/scss/style.scss',
+          outFile: './src/encoded/static/css/style.css',
+          outputStyle: 'compressed'
+    }, function(error, result) { // node-style callback from v3.0.0 onwards
+      if (error) {
+            console.log(error.status); // used to be "code" in v2x and below
+            console.log(error.column);
+            console.log(error.message);
+            console.log(error.line);
+      } else {
+            console.log(result.css.toString());
+
+            console.log(result.stats);
+
+            console.log(result.map.toString());
+            // or better
+            console.log(JSON.stringify(result.map)); // note, JSON.stringify accepts Buffer too
+        }
+        done();
+    });
+}
+
+
 const devSlow       = gulp.series(setDevelopment, doWebpack, watch);
 const devQuick      = gulp.series(setQuick, doWebpack, watch);
 const build         = gulp.series(setProduction, doWebpack);
 const buildQuick    = gulp.series(setQuick, doWebpack);
+const buildSass     = gulp.series(setProduction, doSassBuild);
 
 gulp.task('dev', devSlow);
 gulp.task('default', devQuick);
 gulp.task('dev-quick', devQuick);
 gulp.task('build', build);
 gulp.task('build-quick', buildQuick);
+gulp.task('build-scss', buildSass);
 
