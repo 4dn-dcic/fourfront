cd08f13440d584b9c881890812101860722705fc
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-04-22 14:47:45 -0400

    add item type list to create_mapping_on_deploy cmd

--- src/encoded/commands/create_mapping_on_deploy.py
+++ src/encoded/commands/create_mapping_on_deploy.py
@@ -10,6 +10,114 @@ from dcicutils.beanstalk_utils import whodaman
 log = structlog.getLogger(__name__)
 EPILOG = __doc__
 
+# This order determines order that items will be mapped + added to the queue
+# Can use item type (e.g. file_fastq) or class name (e.g. FileFastq)
+INDEX_ORDER = [
+    'Award',
+    'Lab',
+    'AccessKey',
+    'User',
+    'Sysinfo',
+
+    'Ontology',
+    'OntologyTerm',
+
+    'StaticSection',
+    'Document',
+    'Protocol',
+
+    'FileFormat',
+    'ExperimentType',
+
+    'Vendor',
+    'Organism',
+
+    'Gene',
+    'GenomicRegion',
+    'BioFeature',
+    'Target',
+
+    'Construct',
+    'Enzyme',
+    'Antibody',
+
+    'FileReference',
+
+    'IndividualChicken',
+    'IndividualFly',
+    'IndividualHuman',
+    'IndividualMouse',
+
+    'Image',
+    'Modification',
+
+    'Biosource',
+    'BiosampleCellCulture',
+    'Biosample',
+
+    'Workflow',
+    'WorkflowMapping',
+
+    'PublicationTracking',
+    'Software',
+    'AnalysisStep',
+    'Badge',
+    'SopMap',
+    'SummaryStatistic',
+    'SummaryStatisticHiC',
+    'TrackingItem',
+
+    'TreatmentAgent',
+    'TreatmentChemical',
+    'TreatmentRnai',
+
+    'ImagingPath',
+    'MicroscopeSettingA1',
+    'MicroscopeSettingA2',
+    'MicroscopeSettingD1',
+    'MicroscopeSettingD2',
+
+    'HiglassViewConfig',
+    'QualityMetricAtacseq',
+    'QualityMetricBamqc',
+    'QualityMetricChipseq',
+    'QualityMetricDedupqcRepliseq',
+    'QualityMetricFastqc',
+    'QualityMetricFlag',
+    'QualityMetricPairsqc',
+
+    'ExperimentAtacseq',
+    'ExperimentCaptureC',
+    'ExperimentChiapet',
+    'ExperimentDamid',
+    'ExperimentHiC',
+    'ExperimentMic',
+    'ExperimentRepliseq',
+    'ExperimentSeq',
+    'ExperimentTsaseq',
+    'ExperimentSet',
+    'ExperimentSetReplicate',
+
+    'Publication',
+
+    'FileCalibration',
+    'FileFastq',
+    'FileMicroscopy',
+    'FileProcessed',
+    'FileSet',
+    'FileSetCalibration',
+    'FileSetMicroscopeQc',
+    'FileVistrack',
+
+    'DataReleaseUpdate',
+
+    'WorkflowRun',
+    'WorkflowRunAwsem',
+    'WorkflowRunSbg',
+
+    'Page',
+]
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -37,7 +145,7 @@ def main():
         # handle mastertest ... by blowing away all data first
         if 'mastertest' in env:
             log.info("looks like we are on mastertest, run create mapping without check first")
-            run_create_mapping(app, check_first=False, purge_queue=True)
+            run_create_mapping(app, check_first=False, purge_queue=True, item_order=ITEM_INDEX_ORDER)
             return
         log.info("looks like we are NOT on staging or mastertest so run create mapping with check first")
     except Exception:
@@ -45,4 +153,4 @@ def main():
         log.warning("error checking whodaman: %s " % traceback.format_exc())
         log.warning("couldn't get wodaman, so assuming NOT staging")
     log.info("... using default create mapping case")
-    run_create_mapping(app, check_first=True, purge_queue=True)
+    run_create_mapping(app, check_first=True, purge_queue=True, item_order=ITEM_INDEX_ORDER)
