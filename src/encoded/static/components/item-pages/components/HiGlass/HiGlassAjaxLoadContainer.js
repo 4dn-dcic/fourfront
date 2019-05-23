ef5e8d2eb255a75c2470e8e4e02ea9fbb527f2e3
Chad Serrant <chad_serrant@hms.harvard.edu>
2019-05-23 14:02:31 -0400

    js-resize-higlass: Resize Higlass viewconfigs to fit the container height.

--- src/encoded/static/components/item-pages/components/HiGlass/HiGlassAjaxLoadContainer.js
+++ src/encoded/static/components/item-pages/components/HiGlass/HiGlassAjaxLoadContainer.js
@@ -1,11 +1,11 @@
 'use strict';
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import _ from 'underscore';
-import { isServerSide, ajax, console, object } from './../../../util';
+import { ajax, object } from './../../../util';
 import { HiGlassPlainContainer, HiGlassLoadingIndicator } from './HiGlassPlainContainer';
-
+import memoize from 'memoize-one';
+import { deepClone } from "../../../util/object";
 
 /**
  * Accepts `higlassItem` (HiglassViewConfig Item JSON) as a prop and loads in the full
@@ -13,6 +13,64 @@ import { HiGlassPlainContainer, HiGlassLoadingIndicator } from './HiGlassPlainCo
  * instantiating a HiGlassPlainContainer.
  */
 export class HiGlassAjaxLoadContainer extends React.PureComponent {
+
+    /**
+     * Dynamically scale the 1-dimensional top tracks in the Higlass viewconf.
+     * Memoized function.
+     * @param {object} The Higlass view configuration. This is not modified.
+     * @param {integer} The target height for the Higlass config, in pixels.
+     * @returns {object}
+     */
+    static scaleViewconfToHeight = memoize(function(originalViewconf, givenHeight){
+        var viewconf = deepClone(originalViewconf);
+
+        // Check parameters.
+        if (!("views" in viewconf)) { return viewconf; }
+        if (givenHeight === null || givenHeight <= 0) {return viewconf; }
+
+        // Are there any views with 1D tracks? If not, stop
+        const has1DTracks = function(view) {
+            return (
+                "tracks" in view &&
+                "top" in view["tracks"] &&
+                view["tracks"]["top"].length > 0
+            );
+        };
+        if (!(_.some(viewconf["views"], has1DTracks))) {
+            return viewconf;
+        }
+
+        // Determine the height for each view. There may be so many views they must be on multiple rows.
+        const heightPerView = viewconf["views"] <= 2 ? givenHeight : (givenHeight / 2) | 0;
+
+        _.each(viewconf["views"], function(view){
+            if (!(has1DTracks(view))) { return; }
+
+            // 2D tracks scale automatically, so we will let it take about 2/3s of the total height.
+            let scaledHeightFor1DTracks = heightPerView;
+            if (
+                "tracks" in view &&
+                "center" in view["tracks"] &&
+                view["tracks"]["center"].length > 0
+            ) {
+                scaledHeightFor1DTracks = (heightPerView / 3) | 0;
+            }
+
+            const sumOfOriginal1DTracks = _.reduce(
+                _.filter(view["tracks"]["top"], function(track) { return ("height" in track);}),
+                function(memo, track) { return memo + track["height"];},
+                0
+            );
+            // Resize each 1D track to fit the given display height (round to the nearest integer so Higlass can use them)
+            _.each(
+                _.filter(view["tracks"]["top"], function(track) { return ("height" in track);}),
+                function (track) {
+                    track["height"] = (track["height"] * scaledHeightFor1DTracks / sumOfOriginal1DTracks) | 0;
+                }
+            );
+        });
+        return viewconf;
+    });
 
     constructor(props){
         super(props);
@@ -59,7 +117,7 @@ export class HiGlassAjaxLoadContainer extends React.PureComponent {
         this.setState({ 'loading': true }, ()=>{
             // Use the @id to get the item, then remove the loading message
             ajax.load(object.itemUtil.atId(higlassItem), (r)=>{
-                this.setState({ 'higlassItem' : r, 'loading': false });
+                this.setState({'higlassItem' : r,'loading': false});
             });
         });
     }
@@ -87,7 +145,9 @@ export class HiGlassAjaxLoadContainer extends React.PureComponent {
             );
         }
 
+        // Scale the higlass config so it fits the given container.
+        const adjustedViewconfig = HiGlassAjaxLoadContainer.scaleViewconfToHeight(higlassItem.viewconfig, height);
         // Pass the viewconfig to the HiGlassPlainContainer
-        return <HiGlassPlainContainer {..._.omit(this.props, 'higlassItem')} viewConfig={higlassItem.viewconfig} ref={this.containerRef}/>;
+        return <HiGlassPlainContainer {..._.omit(this.props, 'higlassItem')} viewConfig={adjustedViewconfig} ref={this.containerRef}/>;
     }
 }
