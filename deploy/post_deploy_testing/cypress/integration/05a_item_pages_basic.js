741642f3e8e770702a6772d38d78f87e5ebe25a9
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-07-16 11:21:34 -0400

    Speed up cypress test

--- deploy/post_deploy_testing/cypress/integration/05a_item_pages_basic.js
+++ deploy/post_deploy_testing/cypress/integration/05a_item_pages_basic.js
@@ -12,7 +12,7 @@ import { schemasToItemTypeHierarchy } from './../support/macros';
  */
 
 // Config
-const itemsPerTypeToCheck = 2;
+const itemsPerTypeToCheck = 3;
 
 
 
@@ -32,6 +32,7 @@ const currIDs = [];
 describe('Each Item View Works (most public recent only)', function () {
 
     before(function(){
+        cy.visit('/'); // Start at home page
         cy.request('/profiles/?format=json', {
             'method' : "GET",
             'headers' : { 'Content-Type' : "application/json; charset=UTF-8", 'Accept' : "application/json" },
@@ -53,7 +54,7 @@ describe('Each Item View Works (most public recent only)', function () {
     it("Can load up list of @ids to check per item type (backend)", function(){
         Cypress._.forEach(typesToCheck, function(itemType, idx){
             cy.request({
-                url : "/search/?type=" + itemType + "&limit=" + (itemsPerTypeToCheck * 2),
+                url : "/search/?type=" + itemType + "&limit=" + itemsPerTypeToCheck,
                 method: "GET",
                 failOnStatusCode : false,
                 headers: {
@@ -81,22 +82,43 @@ describe('Each Item View Works (most public recent only)', function () {
     });
 
     it("Each most-recent item can be navigated w.o. errors", function(){
+
+        let currPagePath = "/";
+
         cy.wrap(currIDs).each(function(currentAtID, currentAtIDIndex){
             cy.log("Visiting", currentAtID, currentAtIDIndex + " of " + currIDs.length);
-            cy.visit(currentAtID).wait(1000).end()
-                .get('h1.page-title').should('not.be.empty').end()
-                .get('div.rc-tabs span[data-tab-key="details"]').should('contain', 'Details').end()
-                .get('.rc-tabs .rc-tabs-nav div.rc-tabs-tab:not(.rc-tabs-tab-active):not(.rc-tabs-tab-disabled)').each(function($tab){
-                    var tabKey = $tab.children('span.tab').attr('data-tab-key');
-                    return cy.wrap($tab).click({ 'force' : true }).end()
-                        .wait(50)
-                        .get('.rc-tabs-content .rc-tabs-tabpane-active')
-                        .should('have.id', "tab:" + tabKey).end()
-                        .root().should('not.contain', "client-side error")
-                        .end().then(function(){
-                            Cypress.log({ 'name' : "Successfully Visited", "message" : currentAtID });
-                        });
+
+            cy.window().then(function(w){
+
+                return new Promise(function(resolve){
+
+                    //cy.visit(currentAtID).wait(1000).end()
+                    w.fourfront.navigate(currentAtID, {}, function(){
+                        cy.location('pathname').should('not.equal', currPagePath)
+                            .then(function(pathName){
+                                currPagePath = pathName;
+                                console.log(currPagePath);
+                            }).wait(150).end()
+                            .get('h1.page-title').should('not.be.empty').end()
+                            .get('div.rc-tabs span[data-tab-key="details"]').should('contain', 'Details').end()
+                            .get('.rc-tabs .rc-tabs-nav div.rc-tabs-tab:not(.rc-tabs-tab-active):not(.rc-tabs-tab-disabled)').each(function($tab){
+                                var tabKey = $tab.children('span.tab').attr('data-tab-key');
+                                return cy.wrap($tab).click({ 'force' : true }).end()
+                                    .wait(50)
+                                    .get('.rc-tabs-content .rc-tabs-tabpane-active')
+                                    .should('have.id', "tab:" + tabKey).end()
+                                    .root().should('not.contain', "client-side error")
+                                    .end().then(function(){
+                                        Cypress.log({ 'name' : "Successfully Visited", "message" : currentAtID });
+                                    });
+                            }).end();
+                        resolve();
+                    });
+
                 });
+
+            });
+
         });
     });
 
