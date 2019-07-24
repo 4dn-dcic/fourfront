b5b4db961dcba8ab9349b3e3b86931065933275f
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-07-24 16:44:09 -0400

    Various cleanup, as well as importing some functions in __init__ from snovault.app. Pin snovault 1.2.0

--- src/encoded/__init__.py
+++ src/encoded/__init__.py
@@ -23,16 +23,17 @@ from pyramid.settings import (
 from sqlalchemy import engine_from_config
 from webob.cookies import JSONSerializer
 from snovault.json_renderer import json_renderer
-STATIC_MAX_AGE = 0
+from snovault.app import (
+    STATIC_MAX_AGE,
+    session,
+    json_from_path,
+    configure_dbsession,
+    changelogs,
+    json_asset
+)
 from dcicutils.log_utils import set_logging
 import structlog
 import logging
-
-
-def json_asset(spec, **kw):
-    utf8 = codecs.getreader("utf-8")
-    asset = AssetResolver(caller_package()).resolve(spec)
-    return json.load(utf8(asset.stream()), **kw)
 
 
 def static_resources(config):
@@ -78,81 +79,6 @@ def static_resources(config):
     config.add_view(robots_txt, route_name='robots.txt-conditional')
 
 
-
-def changelogs(config):
-    config.add_static_view(
-        'profiles/changelogs', 'schemas/changelogs', cache_max_age=STATIC_MAX_AGE)
-
-
-def configure_engine(settings):
-    engine_url = settings['sqlalchemy.url']
-
-    engine_opts = {}
-    if engine_url.startswith('postgresql'):
-        if settings.get('indexer_worker'):
-            application_name = 'indexer_worker'
-        elif settings.get('indexer'):
-            application_name = 'indexer'
-        else:
-            application_name = 'app'
-        engine_opts = dict(
-            isolation_level='REPEATABLE READ',
-            json_serializer=json_renderer.dumps,
-            connect_args={'application_name': application_name}
-        )
-    engine = engine_from_config(settings, 'sqlalchemy.', **engine_opts)
-    if engine.url.drivername == 'postgresql':
-        timeout = settings.get('postgresql.statement_timeout')
-        if timeout:
-            timeout = int(timeout) * 1000
-            set_postgresql_statement_timeout(engine, timeout)
-    return engine
-
-
-def set_postgresql_statement_timeout(engine, timeout=20 * 1000):
-    """ Prevent Postgres waiting indefinitely for a lock.
-    """
-    from sqlalchemy import event
-    import psycopg2
-
-    @event.listens_for(engine, 'connect')
-    def connect(dbapi_connection, connection_record):
-        cursor = dbapi_connection.cursor()
-        try:
-            cursor.execute("SET statement_timeout TO %d" % timeout)
-        except psycopg2.Error:
-            dbapi_connection.rollback()
-        finally:
-            cursor.close()
-            dbapi_connection.commit()
-
-
-def configure_dbsession(config, clear_data=False):
-    from snovault import DBSESSION
-    settings = config.registry.settings
-    DBSession = settings.pop(DBSESSION, None)
-    if DBSession is None:
-        engine = configure_engine(settings)
-
-        from snovault.storage import Base
-        # useful for test instances where we want to clear the data
-        if clear_data:
-            Base.metadata.drop_all(engine)
-
-        if asbool(settings.get('create_tables', False)):
-            Base.metadata.create_all(engine)
-
-        import snovault.storage
-        import zope.sqlalchemy
-        from sqlalchemy import orm
-
-        DBSession = orm.scoped_session(orm.sessionmaker(bind=engine))
-        zope.sqlalchemy.register(DBSession)
-        snovault.storage.register(DBSession)
-
-    config.registry[DBSESSION] = DBSession
-
-
 def load_workbook(app, workbook_filename, docsdir):
     from .loadxl import load_all
     from webtest import TestApp
@@ -162,40 +88,6 @@ def load_workbook(app, workbook_filename, docsdir):
     }
     testapp = TestApp(app, environ)
     load_all(testapp, workbook_filename, docsdir)
-
-
-def json_from_path(path, default=None):
-    if path is None:
-        return default
-    return json.load(open(path))
-
-
-def session(config):
-    """ To create a session secret on the server:
-
-    $ cat /dev/urandom | head -c 256 | base64 > session-secret.b64
-    """
-    settings = config.registry.settings
-    if 'session.secret' in settings:
-        secret = settings['session.secret'].strip()
-        if secret.startswith('/'):
-            secret = open(secret).read()
-            secret = base64.b64decode(secret)
-    else:
-        secret = os.urandom(256)
-    # auth_tkt has no timeout set
-    # cookie will still expire at browser close
-    if 'session.timeout' in settings:
-        timeout = int(settings['session.timeout'])
-    else:
-        timeout = 60 * 60 * 24
-    session_factory = SignedCookieSessionFactory(
-        secret=secret,
-        timeout=timeout,
-        reissue_time=2**32,  # None does not work
-        serializer=JSONSerializer(),
-    )
-    config.set_session_factory(session_factory)
 
 
 def app_version(config):
