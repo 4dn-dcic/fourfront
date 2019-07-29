e55ad374dc363a27c70e36c7074b49cbac47d24d
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-07-29 12:08:36 -0400

    Some renaming/cleanup changes. Also added dbxref mixin

--- src/encoded/types/__init__.py
+++ src/encoded/types/__init__.py
@@ -178,7 +178,7 @@ class Sample(Item):
         'description': 'Listing of Disorders',
     })
 class Disorder(Item):
-    item_type = 'disease'
+    item_type = 'disorder'
     schema = load_schema('encoded:schemas/disorder.json')
     embedded_list = [
         'associated_phenotypes.term_name',
@@ -197,7 +197,7 @@ class Disorder(Item):
 
 @collection(
     name='genes',
-    unique_key='gene:geneid',
+    unique_key='gene:gene_id',
     lookup_key='preferred_symbol',
     properties={
         'title': 'Genes',
@@ -206,19 +206,19 @@ class Disorder(Item):
 class Gene(Item):
     """Gene class."""
     item_type = 'gene'
-    name_key = 'geneid'
+    name_key = 'gene_id'
     schema = load_schema('encoded:schemas/gene.json')
-    embedded_list = lab_award_attribution_embed_list + ["organism.scientific_name"]
+    embedded_list = []
 
     @calculated_property(schema={
         "title": "Display Title",
         "description": "A calculated title for every object in 4DN",
         "type": "string"
     })
-    def display_title(self, request, geneid, preferred_symbol=None):
+    def display_title(self, request, gene_id, preferred_symbol=None):
         if preferred_symbol:
             return preferred_symbol
-        return 'GENE ID:{}'.format(geneid)
+        return 'GENE ID:{}'.format(gene_id)
 
 
 @collection(
@@ -231,27 +231,27 @@ class Phenotype(Item):
     item_type = 'phenotype'
     schema = load_schema('encoded:schemas/phenotype.json')
     rev = {
-        'associated_diseases': ('Disease', 'associated_phenotypes')
+        'associated_disorders': ('Disorder', 'associated_phenotypes')
     }
     embedded_list = [
-        'associated_diseases.term_name',
-        'associated_diseases.term_id',
-        'associated_diseases.associated_genes'
+        'associated_disorders.term_name',
+        'associated_disorders.term_id',
+        'associated_disorders.associated_genes'
     ]
 
     @calculated_property(schema={
-        "title": "Associated Diseases",
-        "description": "Diseases associated with this phenotype",
+        "title": "Associated Disorders",
+        "description": "Disorders associated with this phenotype",
         "type": "array",
         "exclude_from": ["submit4dn", "FFedit-create"],
         "items": {
-            "title": "Disease",
+            "title": "Disorder",
             "type": "string",
-            "linkTo": "Disease"
+            "linkTo": "Disorder"
         }
     })
-    def associated_diseases(self, request):
-        return self.rev_link_atids(request, "associated_diseases")
+    def associated_disorders(self, request):
+        return self.rev_link_atids(request, "associated_disorders")
 
     @calculated_property(schema={
         "title": "Display Title",
