34d536cdb43edeaad4e38005068b38b50c541321
Koray Kırlı <koray_kirli@hms.harvard.edu>
2019-08-26 11:59:17 -0400

    fix file_format schema and types

--- src/encoded/types/__init__.py
+++ src/encoded/types/__init__.py
@@ -295,6 +295,30 @@ class Document(ItemWithAttachment, Item):
 
 
 @collection(
+    name='file-formats',
+    unique_key='file_format:file_format',
+    lookup_key='file_format',
+    properties={
+        'title': 'File Formats',
+        'description': 'Listing of file formats used by 4DN'
+    }
+)
+class FileFormat(Item, ItemWithAttachment):
+    """The class to store information about 4DN file formats"""
+    item_type = 'file_format'
+    schema = load_schema('encoded:schemas/file_format.json')
+    name_key = 'file_format'
+
+    @calculated_property(schema={
+        "title": "Display Title",
+        "description": "A calculated title",
+        "type": "string"
+    })
+    def display_title(self, file_format):
+        return file_format
+
+
+@collection(
     name='sysinfos',
     unique_key='sysinfo:name',
     properties={
