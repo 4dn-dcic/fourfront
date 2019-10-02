9e50255bb514d8bc4da3a656930c6d8242b6ac42
utku-ozturk <utku.ozturk@karyasmd.com>
2019-10-02 19:21:19 +0300

    facet's '!=' condition rendering fixed

--- src/encoded/static/components/browse/BrowseView.js
+++ src/encoded/static/components/browse/BrowseView.js
@@ -187,7 +187,7 @@ class ResultTableContainer extends React.PureComponent {
 
     constructor(props){
         super(props);
-        this.isTermSelected = this.isTermSelected.bind(this);
+        this.getTermStatus = this.getTermStatus.bind(this);
         this.onFilter = this.onFilter.bind(this);
         this.handleClearFilters = this.handleClearFilters.bind(this);
         this.getColumnDefinitions = this.getColumnDefinitions.bind(this);
@@ -224,8 +224,8 @@ class ResultTableContainer extends React.PureComponent {
         );
     }
 
-    isTermSelected(term, facet){
-        return searchFilters.determineIfTermFacetSelected(term, facet, this.props);
+    getTermStatus(term, facet){
+        return searchFilters.getTermFacetStatus(term, facet, this.props);
     }
 
     handleClearFilters(evt){
@@ -258,7 +258,7 @@ class ResultTableContainer extends React.PureComponent {
                         <ExternaDataExpSetsCount {...{ countExternalSets, browseBaseState, href }} />
                         <FacetList {...{ session, browseBaseState, schemas, windowWidth, windowHeight, facets, showClearFiltersButton }}
                             orientation="vertical" className="with-header-bg" filters={context.filters}
-                            isTermSelected={this.isTermSelected} onFilter={this.onFilter}
+                            getTermStatus={this.getTermStatus} onFilter={this.onFilter}
                             itemTypeForSchemas="ExperimentSetReplicate" href={href} termTransformFxn={Schemas.Term.toName}
                             onClearFilters={this.handleClearFilters} />
                     </div>
