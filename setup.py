5b410d173c8eb0aa219d52fc174ba83296d5d872
Carl Vitzthum <carl.vitzthum@gmail.com>
2019-10-17 10:04:24 -0400

    Slight setup.py changes

--- setup.py
+++ setup.py
@@ -70,15 +70,15 @@ tests_require = [
 setup(
     name='encoded',
     version='0.1',
-    description='Metadata database for ENCODE',
+    description='4DN-DCIC Data Portal',
     long_description=README + '\n\n' + CHANGES,
     packages=find_packages('src'),
     package_dir={'': 'src'},
     include_package_data=True,
     zip_safe=False,
-    author='Laurence Rowe',
-    author_email='lrowe@stanford.edu',
-    url='http://encode-dcc.org',
+    author='4DN Team at Harvard Medical School',
+    author_email='carl_vitzthum@hms.harvard.edu',
+    url='http://data.4dnucleome.org',
     license='MIT',
     install_requires=requires,
     tests_require=tests_require,
@@ -87,7 +87,6 @@ setup(
     },
     entry_points='''
         [console_scripts]
-        batchupgrade = snovault.batchupgrade:main
         create-mapping = snovault.elasticsearch.create_mapping:main
         dev-servers = encoded.dev_servers:main
         es-index-listener = snovault.elasticsearch.es_index_listener:main
