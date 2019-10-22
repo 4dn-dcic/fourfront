199d175ddedbe500c9a99f523a67e8bcef2a620f
William Ronchetti <wrr33@cornell.edu>
2019-10-22 13:36:44 -0400

    fix typo

--- src/encoded/tests/features/conftest.py
+++ src/encoded/tests/features/conftest.py
@@ -21,7 +21,7 @@ def app_settings(wsgi_server_host_port, elasticsearch_server, postgresql_server,
     settings['collection_datastore'] = 'elasticsearch'
     settings['item_datastore'] = 'elasticsearch'
     settings['indexer'] = True
-    app_settings['indexer.namespace'] = os.environ.get('TRAVIS_JOB_ID', '') # set namespace for tests
+    settings['indexer.namespace'] = os.environ.get('TRAVIS_JOB_ID', '') # set namespace for tests
 
     # use aws auth to access elasticsearch
     if aws_auth:
