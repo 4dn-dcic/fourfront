d3a3a6c68fd4b39bb5a0fb386652f801a0037011
William Ronchetti <wrr33@cornell.edu>
2019-10-28 10:00:29 -0400

    remove flaky from search

--- src/encoded/tests/test_search.py
+++ src/encoded/tests/test_search.py
@@ -12,7 +12,7 @@ def delay_rerun(*args):
     return True
 
 
-pytestmark = [pytest.mark.working, pytest.mark.schema, pytest.mark.indexing, pytest.mark.flaky(rerun_filter=delay_rerun)]
+pytestmark = [pytest.mark.working, pytest.mark.schema, pytest.mark.indexing)]
 
 ### IMPORTANT
 # uses the inserts in ./data/workbook_inserts
