1b1746a867561271fbeaaefd042c7b8be6ec7b4f
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-11-19 12:01:34 -0500

    Touchups

--- src/encoded/static/components/static-pages/placeholders/BasicCarousel.js
+++ src/encoded/static/components/static-pages/placeholders/BasicCarousel.js
@@ -5,7 +5,25 @@ import PropTypes from 'prop-types';
 import _ from 'underscore';
 import Carousel from 'nuka-carousel';
 
+/** @see https://www.npmjs.com/package/nuka-carousel for documentation of all props/options allowed */
+const defaultCarouselOptions = {
+    'initialSlideHeight' : 540,
+    'initialSlideWidth' : 720,
+    'autoplay': false,
+    'autoplayInterval': 5000,
+    'cellSpacing': 20,
+    'heightMode' : "max",
+    'slidesToShow': 1,
+    'slidesToScroll': 1,
+    'slideIndex': 0,
+    'speed': 700,
+    'dragging': true,
+    'easing': 'easeLinear',
+    'transitionMode': 'scroll'
+};
+
 export class BasicCarousel extends React.PureComponent {
+
     static defaultProps = {
         'slides': [
             {
@@ -21,18 +39,28 @@ export class BasicCarousel extends React.PureComponent {
                 'alt': 'Slide 3 alt text'
             }
         ],
-        'slideWidth': 720,
-        'slideHeight': 540,
         'showSlideCount': true,
-        'autoPlay': false
+        'carouselOptions' : defaultCarouselOptions
     };
 
     static propTypes = {
         'slides': PropTypes.array.isRequired,
-        'slideWidth': PropTypes.number.isRequired,
-        'slideHeight': PropTypes.number.isRequired,
         'showSlideCount': PropTypes.bool.isRequired,
-        'autoPlay': PropTypes.bool.isRequired
+        'autoPlay': PropTypes.bool.isRequired,
+        /** @see https://www.npmjs.com/package/nuka-carousel for documentation of all props/options allowed */
+        'carouselOptions' : PropTypes.shape({
+            'autoplay' : PropTypes.bool,
+            'autoplayInterval' : PropTypes.number,
+            'autoplayReverse' : PropTypes.bool,
+            'cellAlign' : PropTypes.oneOf(['left', 'center', 'right']),
+            'cellSpacing' : PropTypes.number,
+            'dragging' : PropTypes.bool,
+            'easing' : PropTypes.string,
+            'edgeEasing' : PropTypes.string,
+            'heightMode' : PropTypes.oneOf(['first', 'current', 'max']),
+            'slidesToShow' : PropTypes.number,
+            'slidesToScroll' : PropTypes.number,
+        })
     };
 
     static refFunc(elem) {
@@ -42,41 +70,47 @@ export class BasicCarousel extends React.PureComponent {
         }, 10);
     }
 
+    constructor(props){
+        super(props);
+        this.renderCenterLeftControls = this.renderCenterLeftControls.bind(this);
+        this.renderCenterRightControls = this.renderCenterRightControls.bind(this);
+        this.renderBottomLeftControls = this.renderBottomLeftControls.bind(this);
+    }
+
+    renderCenterLeftControls(sliderProps){
+        const { previousSlide, currentSlide } = sliderProps;
+        if (currentSlide === 0) return null;
+        return <i className="icon icon-fw fas icon-angle-left icon-3x" onClick={previousSlide} />;
+    }
+
+    renderCenterRightControls(sliderProps){
+        const { nextSlide, currentSlide, slideCount, slidesToShow } = sliderProps;
+        if (currentSlide >= slideCount - slidesToShow) return null;
+        return <i className="icon icon-fw fas icon-angle-right icon-3x" onClick={nextSlide} />;
+    }
+
+    renderBottomLeftControls({ currentSlide, slideCount }){
+        return <div>Slide <strong>{currentSlide + 1}</strong> of <strong>{slideCount}</strong></div>;
+    }
+
     render() {
-        const { slides, slideWidth, slideHeight, showSlideCount, autoPlay } = this.props;
-        const carouselProps = {
-            'slidesToShow': 1,
-            'slidesToScroll': 1,
-            'slideIndex': 0,
-            'cellSpacing': 20,
-            'speed': 700,
-            //'cellAlign': 'center',
-            'dragging': false,
-            'easing': 'easeLinear',
-            'transitionMode': 'scroll',
-            'autoplay': autoPlay,
-            'autoplayInterval': 5000,
-            'initialSlideHeight': slideHeight,
-            'initialSlideWidth': slideWidth,
-            'renderCenterLeftControls': function (sliderProps) {
-                const { previousSlide, currentSlide } = sliderProps;
-                if (currentSlide === 0) return null;
-                return <i className="icon icon-fw fas icon-angle-left icon-3x" onClick={previousSlide} />;
+        const { slides, showSlideCount, carouselOptions } = this.props;
+        const carouselProps = _.extend(
+            {
+                renderCenterLeftControls: this.renderCenterLeftControls,
+                renderCenterRightControls: this.renderCenterRightControls
             },
-            'renderCenterRightControls': function (sliderProps) {
-                const { nextSlide, currentSlide, slideCount, slidesToShow } = sliderProps;
-                if (currentSlide >= slideCount - slidesToShow) return null;
-                return <i className="icon icon-fw fas icon-angle-right icon-3x" onClick={nextSlide} />;
-            }
-        };
+            defaultCarouselOptions,
+            carouselOptions
+        );
 
         if (showSlideCount === true) {
-            carouselProps.renderBottomLeftControls = function ({ currentSlide, slideCount }) {
-                return <div>Slide <strong>{currentSlide + 1}</strong> of <strong>{slideCount}</strong></div>;
-            };
+            carouselProps.renderBottomLeftControls = this.renderBottomLeftControls;
         }
+
         //style
-        const style = { 'width': slideWidth, 'height': slideHeight };
+        //const style = { 'width': slideWidth, 'height': slideHeight };
+        const style = {};
 
         return (
             <div className="basic-carousel-wrapper" ref={BasicCarousel.refFunc} style={{ opacity: '0' }}>
