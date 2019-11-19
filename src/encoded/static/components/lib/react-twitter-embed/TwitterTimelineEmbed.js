ef18bfaa364d3322b54ce266f656141560c96767
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-11-19 10:30:48 -0500

    ONGOING

---
+++ src/encoded/static/components/lib/react-twitter-embed/TwitterTimelineEmbed.js
@@ -0,0 +1,169 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import url from 'url';
+import { console, isServerSide } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
+
+
+let hasScriptBeenLoaded = false;
+
+/**
+ * THIS WAS COPIED OVER FROM `react-twitter-embed` and extended.
+ * At some point in future might want to finalize "Fade In" additions and PR it to main repo.
+ *
+ * @see https://github.com/saurabhnemade/react-twitter-embed/blob/master/src/components/TwitterTimelineEmbed.js
+ */
+export default class TwitterTimelineEmbed extends React.PureComponent {
+
+    static defaultProps = {
+        twitterJSURL : "https://platform.twitter.com/widgets.js"
+    };
+    
+    constructor(props) {
+        super(props)
+        this.state = {
+            isLoading: true
+        }
+        
+        this.embedContainerRef = React.createRef();
+    }
+    
+    buildChromeOptions(options) {
+        const { noHeader = false, noFooter = false, noBorders = false, noScrollbar = false, transparent = false } = this.props;
+        
+        options.chrome = ''
+        
+        if (noHeader)     options.chrome += ' noheader'
+        if (noFooter)     options.chrome += ' nofooter'
+        if (noBorders)    options.chrome += ' noborders'
+        if (noScrollbar)  options.chrome += ' noscrollbar'
+        if (transparent)  options.chrome += ' transparent'
+        
+        return options
+    }
+    
+    buildOptions() {
+        const { options: propOptions, autoHeight = false, theme, linkColor, borderColor, lang } = this.props;
+        let options = Object.assign({}, propOptions, { theme, linkColor, borderColor, lang })
+        if (autoHeight) {
+            options.height = this.embedContainerRef.current.parentNode.offsetHeight
+        }
+        return options
+    }
+    
+    /** Experimental and probably against Twitter's Developer Policy / Terms. Not used b.c. of this. */
+    cleanupTwitterScripts(){
+        // Spread to array for faster lookups, else is HTMLLiveCollection and gets from
+        // DOM on each reference call/check/traversal.
+        const iframesOnPage = [ ...window.document.getElementsByTagName("iframe") ];
+        const iframesToCleanup = iframesOnPage.filter(function(iframeElem){
+            const parentElemName = (iframeElem.parentElement && (iframeElem.parentElement.localName || iframeElem.parentElement.nodeName).toLowerCase()) || null;
+            // In future we could gather maybe more accurately with https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver
+            if (parentElemName !== "body") {
+                // Might be widget iframe itself. Which would get removed when this component dismounts, so we can skip.
+                return false;
+            }
+            if (iframeElem.title) {
+                if (iframeElem.title.indexOf("Twitter") > -1) {
+                    return true;
+                }
+            }
+            if (iframeElem.src) {
+                const iframeSrcParts = url.parse(iframeElem.src);
+                if (iframeSrcParts.hostname.indexOf("twitter.com") > -1) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        while (iframesToCleanup.length > 0) {
+            const iframeElem = iframesToCleanup.pop();
+            window.document.body.removeChild(iframeElem);
+        }
+    }
+    
+    renderWidget(options) {
+        const { onLoad, sourceType, screenName, userId, ownerScreenName, slug, id, widgetId, url } = this.props
+        if (!this.isMountCanceled) {
+            window.twttr.widgets.createTimeline(
+                {
+                    sourceType,
+                    screenName,
+                    userId,
+                    ownerScreenName,
+                    slug,
+                    id: id || widgetId,
+                    url
+                },
+                this.embedContainerRef.current,
+                options
+                ).then((element) => {
+                    // `element` here is the iframe that TwitterJS created.
+                    this.setState({
+                        isLoading: false
+                    }, () => {
+                        this.embedContainerRef.current.style.opacity = 1;
+                        // Cleanup analytics iframes from Twitter
+                        //this.cleanupTwitterScripts();
+                    });
+                    if (onLoad) {
+                        onLoad(element)
+                    }
+                    // Prevent contents of iframe from being navigateble-to via [shift + ] tab key.
+                    element.setAttribute("tabindex", -1);
+                })
+        }
+    }
+        
+    componentDidMount() {
+        const { twitterJSURL } = this.props;
+
+        const initWidget = () => {
+            if (!window.twttr) {
+                console.error('Failure to load window.twttr in TwitterTimelineEmbed, aborting load.')
+                return;
+            }
+            const options = this.buildOptions();
+            this.buildChromeOptions(options);
+            this.renderWidget(options);
+        };
+
+        const onLoadedTwitterJSLib = ()=> {
+            hasScriptBeenLoaded = true;
+            initWidget();
+        };
+
+        setTimeout(()=>{
+            if (hasScriptBeenLoaded) {
+                initWidget();
+                return;
+            }
+            const head = window.document.head;
+            if (!head) {
+                return;
+            }
+            const scriptElem = window.document.createElement("script");
+            scriptElem.src = twitterJSURL;
+            scriptElem.defer = scriptElem.async = true;
+            scriptElem.id = "twitter-js";
+            scriptElem.type = "text/javascript";
+            scriptElem.onload = onLoadedTwitterJSLib;
+            head.appendChild(scriptElem);
+            hasScriptBeenLoaded = true;
+        }, 50);
+    }
+    
+    componentWillUnmount() {
+        this.isMountCanceled = true
+    }
+    
+    render() {
+        const { isLoading } = this.state
+        const { placeholder, className } = this.props
+        return (
+            <React.Fragment>
+                { isLoading && placeholder }
+                <div ref={this.embedContainerRef} className={className} style={{ opacity: 0, transition: "opacity .3s ease-out" }} />
+            </React.Fragment>
+        );
+    }
+}
--- src/encoded/static/components/static-pages/HomePage.js
+++ src/encoded/static/components/static-pages/HomePage.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'underscore';
+//import { TwitterTimelineEmbed, TwitterShareButton, TwitterFollowButton, TwitterHashtagButton, TwitterMentionButton, TwitterTweetEmbed, TwitterMomentShare, TwitterDMButton, TwitterVideoEmbed, TwitterOnAirButton } from 'react-twitter-embed';
+import TwitterTimelineEmbed from '../lib/react-twitter-embed/TwitterTimelineEmbed';
 
 import { console } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
 import { BasicStaticSectionBody } from '@hms-dbmi-bgm/shared-portal-components/es/components/static-pages/BasicStaticSectionBody';
@@ -28,23 +30,28 @@ export default class HomePage extends React.PureComponent {
         "session": PropTypes.bool.isRequired
     };
 
-    introText(){
-        var introContent = _.findWhere(this.props.context.content, { 'name' : 'home.introduction' }); // Content
-
-        if (introContent){
-            return <BasicStaticSectionBody {..._.pick(introContent, 'content', 'filetype')} />;
-        }
-
-        return <p className="text-center">Introduction content not yet indexed.</p>;
-    }
-
     /**
      * The render function. Renders homepage contents.
      * @returns {Element} A React <div> element.
      */
     render() {
         const { windowWidth, context, session } = this.props;
-        const { announcements = null } = context || {};
+        const { announcements = null, content = [] } = context || {};
+        const introContent = _.findWhere(content, { 'name' : 'home.introduction' }); // Content
+        const introToShow = introContent ?
+            <BasicStaticSectionBody {..._.pick(introContent, 'content', 'filetype')} />
+            : <p className="text-center">Introduction content not yet indexed.</p>;
+
+        const twitterPlaceholder = (
+            <div className="twitter-loading-placeholder">
+                <i className="icon icon-twitter icon-2x fab"/>
+            </div>
+        );
+        const twitterTimelineEmbed = (
+            <TwitterTimelineEmbed sourceType="profile" screenName="4dn_dcic"
+                placeholder={twitterPlaceholder} autoHeight noFooter noHeader />
+        );
+
         return (
             <div className="homepage-wrapper">
 
@@ -54,10 +61,22 @@ export default class HomePage extends React.PureComponent {
                     <div className="row">
                         <div className="col-12 col-md-8">
                             <h2 className="homepage-section-title">Introduction</h2>
-                            { this.introText() }
+                            { introToShow }
                         </div>
-                        <div className="col-12 col-md-4 pull-right">
+                        <div className="col-12 col-md-4 social-connections-column">
+                            {/*
                             <LinksColumn {...{ session, windowWidth }} />
+                            */}
+                            <h2 className="homepage-section-title">
+                                <span>Tweets</span>
+                                <a href="https://help.twitter.com/en/twitter-for-websites-ads-info-and-privacy" target="_blank"
+                                    rel="noopener noreferrer" className="right privacy-ext">
+                                    <i className="icon icon-fw icon-info-circle fas" data-tip="Cookie & Privacy Information for Twitter"/>
+                                </a>
+                            </h2>
+                            <div className="twitter-timeline-container">
+                                { twitterTimelineEmbed }
+                            </div>
                         </div>
                     </div>
                     <div className="mt-4">
