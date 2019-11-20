0e17e75f3bcee887b5a588ab3009f19922a60b45
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-11-20 11:35:03 -0500

    Remove heightMode unless lack dimensions + ...
    Allow per-slide height or width to override initial/default.

--- src/encoded/static/components/static-pages/placeholders/BasicCarousel.js
+++ src/encoded/static/components/static-pages/placeholders/BasicCarousel.js
@@ -2,18 +2,21 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import memoize from 'memoize-one';
 import _ from 'underscore';
 import Carousel from 'nuka-carousel';
 
 /** @see https://www.npmjs.com/package/nuka-carousel for documentation of all props/options allowed */
 const defaultCarouselOptions = {
+    // Populated from props.slideHeight & props.slideWidth:
     //'initialSlideHeight' : 540,
     //'initialSlideWidth' : 720,
     'autoplay': false,
     'autoplayInterval': 5000,
     'cellSpacing': 20,
     'cellAlign': "center",
-    'heightMode' : "max",
+    // Set if no image slide dimensions are supplied.
+    //'heightMode' : "max",
     'slidesToShow': 1,
     'slidesToScroll': 1,
     'slideIndex': 0,
@@ -46,8 +49,12 @@ export class BasicCarousel extends React.PureComponent {
 
     static propTypes = {
         'slides': PropTypes.arrayOf(PropTypes.shape({
+            // Required:
             'img' : PropTypes.string.isRequired,
-            'alt' : PropTypes.string
+            // Optional, overrides slideHeight and slideWidth (if set) per slide
+            'alt' : PropTypes.string,
+            'width' : PropTypes.number,
+            'height' : PropTypes.number
         })).isRequired,
         'showSlideCount': PropTypes.bool,
         'autoplay': PropTypes.bool,
@@ -76,11 +83,27 @@ export class BasicCarousel extends React.PureComponent {
         }, 10);
     }
 
+    static allSlidesHaveDimensions(slides = []){
+        const slidesLen = slides.length;
+        for (var i = 0; i < slidesLen; i++){
+            // When const/let are used inside a for loop in ES6+,
+            // new function/closure is created.
+            const { width = null, height = null } = slides[i];
+            if (typeof width !== "number" || typeof height !== "number"){
+                return false;
+            }
+        }
+        return true;
+    }
+
     constructor(props){
         super(props);
         this.renderCenterLeftControls = this.renderCenterLeftControls.bind(this);
         this.renderCenterRightControls = this.renderCenterRightControls.bind(this);
         this.renderBottomLeftControls = this.renderBottomLeftControls.bind(this);
+        this.memoized = {
+            allSlidesHaveDimensions : memoize(BasicCarousel.allSlidesHaveDimensions)
+        };
     }
 
     renderCenterLeftControls(sliderProps){
@@ -104,8 +127,8 @@ export class BasicCarousel extends React.PureComponent {
             slides = [],
             showSlideCount,
             carouselOptions,
-            slideHeight = null,
-            slideWidth = null,
+            slideHeight: commonSlideHeight = null,
+            slideWidth: commonSlideWidth = null,
             autoplay = null
         } = this.props;
 
@@ -122,38 +145,40 @@ export class BasicCarousel extends React.PureComponent {
             carouselProps.renderBottomLeftControls = this.renderBottomLeftControls;
         }
 
-        let commonImageStyle = null;
-
         if (typeof autoplay === "boolean") {
             carouselProps.autoplay = autoplay;
         }
 
-        if (typeof slideHeight === "number" && typeof slideWidth === "number") {
-            // I forgot if Nuka Carousel can automatically resize itself without presence of slideHeight/slideWidth, but think so.
+        if (typeof commonSlideHeight === "number" && typeof commonSlideWidth === "number") {
+            // I forgot if Nuka Carousel can automatically resize itself without presence of commonSlideHeight/slideWidth, but think so.
             // If we set the below values, then will see multiple slides at once (e.g. current one, + slices of previous/next)
             // SEE: https://gyazo.com/35f4efc58d428bc18a335bbe3f1228e7
-            // carouselProps.slideHeight = slideHeight + "px"; // Nuka Carousel requires "px" appended and treats floats as percentages, unlike React elements.
-            // carouselProps.slideWidth = slideWidth + "px";
+            // carouselProps.slideHeight = commonSlideHeight + "px"; // Nuka Carousel requires "px" appended and treats floats as percentages, unlike React elements.
+            // carouselProps.slideWidth = commonSlideWidth + "px";
 
             // Instead we set initialSlideWidth and initialSlideHeight which keeps the 1 current slide visible and others hidden.
             // Nuka Carousel I believe might then resize slides if needed (?) idk. Or atleast stretch slide containers to be
             // 100% width of parent container or something.
-            carouselProps.initialSlideHeight = 720;
-            carouselProps.initialSlideWidth = 540;
-            commonImageStyle = { // This maybe isn't needed (?)
-                height: slideHeight,
-                width: slideWidth
-            };
+            carouselProps.initialSlideHeight = commonSlideHeight;
+            carouselProps.initialSlideWidth = commonSlideWidth;
+        } else if (!this.memoized.allSlidesHaveDimensions(slides)){
+            // No dimensions set anywhere, set heightMode: max to try to calculate.
+            // (unless set already)
+            carouselProps.heightMode = carouselProps.heightMode || "max";
         }
 
         return (
             <div className="basic-carousel-wrapper" ref={BasicCarousel.refFunc} style={{ opacity: '0' }}>
                 <Carousel {...carouselProps}>
                     {
-                        slides.map(function({ img: src, alt }, index) {
+                        slides.map(function({ img: src, alt, width, height }, index) {
+                            const style = {
+                                "width": width || commonSlideWidth || undefined,
+                                "height": height || commonSlideHeight || undefined
+                            };
                             return (
                                 <div className="text-center" key={index}>
-                                    <img {...{ src, alt }} style={commonImageStyle} />
+                                    <img {...{ src, alt, style }} />
                                 </div>
                             );
                         })
