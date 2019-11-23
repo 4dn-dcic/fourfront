28bb7dfdd54e0a71f1b1687b0f9e9145bb2223b3
Alex Balashov <alexander_balashov@hms.harvard.edu>
2019-11-22 20:12:18 -0500

    Publication Column Definitions

--- src/encoded/static/components/browse/PublicationSearchView.js
+++ src/encoded/static/components/browse/PublicationSearchView.js
@@ -1,12 +1,13 @@
 'use strict';
 
-import React from 'react';
+import React, { useMemo } from 'react';
 import memoize from 'memoize-one';
 import _ from 'underscore';
 
 import { SearchView as CommonSearchView } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/SearchView';
-import { console, analytics, object, navigate as spcNavigate } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
+import { console, analytics, object, navigate as spcNavigate, valueTransforms } from '@hms-dbmi-bgm/shared-portal-components/es/components/util';
 import { TableRowToggleOpenButton } from '@hms-dbmi-bgm/shared-portal-components/es/components/browse/components/table-commons';
+import { LocalizedTime } from '@hms-dbmi-bgm/shared-portal-components/es/components/ui/LocalizedTime';
 
 import { columnExtensionMap } from './columnExtensionMap';
 import { Schemas } from './../util';
@@ -18,35 +19,94 @@ const publicationColExtensionMap = _.extend({}, columnExtensionMap, {
     // We override display_title for this view to be wider.
     // And awesomer.
     "display_title" : {
-        "widthMap" : { "sm" : 150, "md" : 300, "lg" : 520 },
-        "render" : function(result, columnDefinition, props, termTransformFxn, width){
+        "widthMap" : { "sm" : 320, "md" : 520, "lg" : 670 },
+        "render" : function(result, columnDefinition, props, termTransformFxn){
             return <PublicationSearchResultTitle {...props} {...{ result }} />;
+        }
+    },
+    "nonexistent_year_field" : {
+        "widthMap" : { "sm" : 60, "md" : 60, "lg" : 60 },
+        "render" : function(result, columnDefinition, props, termTransformFxn){
+            const { date_published = null } = result;
+            if (!date_published) return null;
+            return (
+                <span className="value">
+                    <LocalizedTime timestamp={date_published} formatType="date-year" />
+                </span>
+            );
+        }
+    },
+    "journal" : {
+        "widthMap" : { "sm" : 100, "md" : 120, "lg" : 150 },
+        "render" : function(result, columnDefinition, props, termTransformFxn){
+            const { journal = null } = result;
+            if (!journal) return null;
+            return (
+                <span className="value">
+                    <JournalTitle journal={journal} width={props.width} />
+                </span>
+            );
+        }
+    },
+    "number_of_experiment_sets" : {
+        "widthMap" : { "sm" : 50, "md" : 50, "lg" : 50 },
+        "render" : function(result, columnDefinition, props, termTransformFxn){
+            const { number_of_experiment_sets: numSets = null } = result;
+            if (numSets === null) return null;
+            return (
+                <span className="value">
+                    { valueTransforms.decorateNumberWithCommas(numSets) }
+                </span>
+            );
         }
     }
 });
 
+/**
+ * This is memoized into a couple of different functions - 1 per column (common colWidth between all cells in same col)
+ * Make sure to keep this in sync with CSS rules (line height, etc.)
+ */
+function maxCharsToShowPerLine(colWidth){
+    const charWidth = 6; // Rough upper estimate (excl "i", "l", ".", etc.) for this font/size/weight
+    const textAreaWidth = colWidth - 90; // Account for ToggleButton + cell padding, and line splitting on spaces (not in middle of words)
+    return Math.floor(textAreaWidth / charWidth);
+}
+
 
 class PublicationSearchResultTitle extends React.PureComponent {
 
-    /**
-     * Re-used for all titles - keep this in sync with CSS rules (line height, etc.)
-     * We could try to be smarter and like, roughly measure each character of alphabet or something, but... probably not worth it for now.
-     */
-    static maxCharsToShowPerLineBody = memoize(function(colWidth){
-        const charWidth = 6; // Rough upper estimate (excl "i", "l", ".", etc.)
-        const textAreaWidth = colWidth - 90; // Account for ToggleButton + cell padding, and line splitting on spaces (not in middle of words)
-        return Math.floor(textAreaWidth / charWidth);
-    });
+    static maxCharsToShowPerLine = memoize(maxCharsToShowPerLine);
 
-    static maxCharsToShowForTitle = memoize(function(colWidth){
-        const charWidth = 7; // Rough upper estimate (excl "i", "l", ".", etc.)
-        const textAreaWidth = colWidth - 90; // Account for ToggleButton + cell padding, and line splitting on spaces (not in middle of words)
-        return Math.floor(textAreaWidth / charWidth);
-    });
+    static buildAuthorsElement(maxCharLen, authors = []){
+        const initAuthorsLen = authors.length;
+        if (initAuthorsLen === 0) return null;
+        const initAuthors = authors.slice(0);
+        const authorsToKeep = [];
+        let charCount = 0;
+        while (charCount < maxCharLen && initAuthors.length){
+            const author = initAuthors.shift();
+            const authorLen = author.length;
+            charCount += authorLen;
+            if (charCount < maxCharLen) {
+                authorsToKeep.push(author);
+            } else {
+                break;
+            }
+        }
+        const authorsToKeepLen = authorsToKeep.length;
+        if (initAuthorsLen === authorsToKeepLen) {
+            return authorsToKeep.join(" &bull; ");
+        } else {
+            return authorsToKeep.join(", ") + `, et al.`;
+        }
+    }
 
     constructor(props){
         super(props);
         this.onClickTrack = this.onClickTrack.bind(this);
+        this.memoized = {
+            buildAuthorsString : memoize(PublicationSearchResultTitle.buildAuthorsString)
+        };
     }
 
     onClickTrack(evt){
@@ -65,25 +125,13 @@ class PublicationSearchResultTitle extends React.PureComponent {
     render(){
         const { result, detailOpen, toggleDetailOpen, rowNumber, href, navigate = spcNavigate, width: colWidth } = this.props;
         const { title: origTitle, "@id" : id, abstract, authors = null } = result; // We use "title" here, not "display_title" (which contains year+author, also)
-        const titleLen = abstract.length;
-        const titleMaxLen = PublicationSearchResultTitle.maxCharsToShowForTitle(colWidth) * 3;
-        let title = abstract;
-        if (titleMaxLen > titleLen) {
+        const charsPerLine = PublicationSearchResultTitle.maxCharsToShowPerLine(colWidth);
+        const titleLen = origTitle.length;
+        const titleMaxLen = charsPerLine * 3; // 3 lines max for it
+        let title = origTitle;
+        if (titleLen > titleMaxLen) {
             title = title.slice(0, titleMaxLen) + "...";
         }
-        //const abstract = abstractLen < charsToShow ? origAbstract : origAbstract.slice(0, charsToShowPerLine) + "...";
-        let authorsBlock;
-
-        if (Array.isArray(authors) && authors.length > 0){
-            authorsBlock = (
-                <div className="authors-list text-ellipsis-container mb-08">
-                    { authors.join(", ") }
-                </div>
-            );
-        }
-
-        console.log('TTT', result, colWidth, titleMaxLen, title.length);
-        //const
         return (
             <React.Fragment>
                 <TableRowToggleOpenButton onClick={toggleDetailOpen} open={detailOpen} />
@@ -91,13 +139,85 @@ class PublicationSearchResultTitle extends React.PureComponent {
                     <h5 className="mt-0 mb-0 text-500">
                         <a href={id} onClick={this.onClickTrack}>{ title }</a>
                     </h5>
-                    { authorsBlock }
-                    {/* <p className="abstract mb-0">{ abstract }</p> */}
+                    <AuthorsBlock authors={authors} maxCharacters={titleMaxLen} />
                 </div>
             </React.Fragment>
         );
     }
 }
+
+
+const AuthorsBlock = React.memo(function AuthorsBlock(props){
+    const { maxCharacters: maxCharLen = 20, authors = [] } = props;
+    const initAuthorsLen = authors.length;
+    if (initAuthorsLen === 0) {
+        return (
+            <div className="authors-list">
+                <em>No Authors</em>
+            </div>
+        );
+    }
+    const initAuthors = authors.slice(0);
+    const authorsToKeep = [];
+    let charCount = 0;
+    while (charCount < maxCharLen && initAuthors.length){
+        const author = initAuthors.shift();
+        const authorLen = author.length;
+        charCount += authorLen;
+        if (charCount < maxCharLen) {
+            authorsToKeep.push(author);
+        } else {
+            break;
+        }
+    }
+    const authorsAreTrimmed = authorsToKeep.length < initAuthorsLen;
+    return (
+        <div className="authors-list">
+            {
+                authorsToKeep.map(function(authorStr, i){
+                    return <span className="author-name" key={authorStr}>{ authorStr }</span>;
+                })
+            }
+            { authorsAreTrimmed ? <span className="author-name"><em>et al.</em></span> : null }
+        </div>
+    );
+});
+
+
+
+const JournalTitle = React.memo(function JournalTitle({ journal, width: colWidth }){
+
+    // Not memoized since this component itself is memoized with only real dynamic prop being colWidth.
+    const maxCharsPerLine = maxCharsToShowPerLine(colWidth);
+    const maxChars = maxCharsPerLine * 5;
+    const journalLen = journal.length;
+
+    if (journalLen > maxChars) {
+        return <span className="value">{ journal.slice(0, maxChars).trim() + "..." }</span>;
+    }
+
+    if (journalLen > (maxChars - (maxCharsPerLine - 20))) {
+        // check if ~ >= lines worth of extra space in case line break causes to not fit (e.g. title is only few chars)
+        return <span className="value">{ journal }</span>;
+    }
+
+    const [ , journalTitle, journalAttribution ] = useMemo(function(){
+        // Check if in form of "Title (Attribution content)" and split on first paren
+        const splitParenPattern = new RegExp("^([^(]+\\s)(\\(.+\\))$");
+        return journal.split(splitParenPattern);
+    }, [journal]);
+
+    if (typeof journalAttribution === "string"){
+        return (
+            <span className="value">
+                <span className="d-block text-500">{ journalTitle.trim() }</span>
+                <span className="d-block">{ journalAttribution }</span>
+            </span>
+        );
+    }
+
+    return <span className="value text-500">{ journal }</span>;
+});
 
 
 export default class PublicationSearchView extends React.PureComponent {
