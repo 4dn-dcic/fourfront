82af6ab636c40b1439c2cdee1fdcbc4d61ddfe40
utku-ozturk <utku.ozturk@karyasmd.com>
2020-04-04 19:04:06 +0300

    minor file size bug fix

--- src/encoded/batch_download.py
+++ src/encoded/batch_download.py
@@ -485,7 +485,10 @@ def metadata_tsv(context, request):
                 xfile_vals['File Download URL'] = request.host_url + xfile['href'] if xfile.get('href') else None
                 xfile_vals['File Format'] = xfile.get('file_format', {}).get('display_title')
                 xfile_vals['md5sum'] = xfile.get('md5sum')
-                xfile_vals['Size (MB)'] = xfile.get('file_size')
+                if(xfile.get('file_size') != None):
+                    xfile_vals['Size (MB)'] = format(float(xfile.get('file_size')) / (1024 * 1024), '.2f')
+                else:
+                    xfile_vals['Size (MB)'] = None
                 xfile_vals['Related File Relationship'] = 'secondary file for'
                 xfile_vals['Related File'] = all_row_vals.get('File Accession')
                 files_returned.append(xfile_vals)
@@ -516,7 +519,9 @@ def metadata_tsv(context, request):
             return file_row_dict
 
         file_cache[file_row_dict['File Download URL']] = idx
-        file_row_dict['Size (MB)'] =format((float(file_row_dict['Size (MB)'])/1024)/1024,'.3f')
+        if('Size (MB)' in file_row_dict and file_row_dict['Size (MB)'] != None):
+            file_row_dict['Size (MB)'] = format(
+                float(file_row_dict['Size (MB)']) / (1024 * 1024), '.2f')
         if file_row_dict['File Status'] in ['uploading', 'to be uploaded', 'upload failed']:
             file_row_dict['File Download URL'] = '### Not Yet Uploaded: ' + file_row_dict['File Download URL']
             summary['counts']['Total Files'] += 1
