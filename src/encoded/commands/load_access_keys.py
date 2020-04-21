f8a3f26c9af44f072711691c51022baac09c63f9
William Ronchetti <wrr33@cornell.edu>
2020-04-21 09:38:35 -0400

    hotfix: catch webtest apperror instead of more specific pyramid error

--- src/encoded/commands/load_access_keys.py
+++ src/encoded/commands/load_access_keys.py
@@ -5,8 +5,7 @@ import json
 import os
 import boto3
 from pyramid.paster import get_app
-from pyramid.exceptions import HTTPNotFound
-from webtest import TestApp
+from webtest import TestApp, AppError
 from dcicutils.beanstalk_utils import get_beanstalk_real_url
 
 log = structlog.getLogger(__name__)
@@ -120,7 +119,7 @@ def main():
         for key_id in key_ids:
             try:
                 testapp.patch_json(key_id, {'status': 'deleted'})
-            except HTTPNotFound:
+            except AppError:
                 log.error('load_access_keys: key_id: %s does not exist in database but exists in ES' % key_id)
 
 
