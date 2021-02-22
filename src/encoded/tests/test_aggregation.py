615c7a9693067d4489247dd5d54f230986ae2447
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-02-21 23:52:50 -0500

    Use es_app, which replaces workbook app, in test_aggregation.py

--- src/encoded/tests/test_aggregation.py
+++ src/encoded/tests/test_aggregation.py
@@ -2,7 +2,7 @@ import pytest
 
 from dcicutils.qa_utils import notice_pytest_fixtures
 from ..util import delay_rerun
-from .workbook_fixtures import app_settings, app, workbook
+# from .workbook_fixtures import app_settings, app, workbook
 
 
 # NOTE WELL: Even though app_settings and app are not autouse fixtures, they must be imported.
@@ -10,18 +10,19 @@ from .workbook_fixtures import app_settings, app, workbook
 #  it will find different versions of those fixtures, which is what will cause the tests to fail
 #  with: 404  "The resource could not be found."
 #  -kmp 28-Jun-2020
-notice_pytest_fixtures(app_settings, app, workbook)
+# notice_pytest_fixtures(app_settings, app, workbook)
 
 
 pytestmark = [pytest.mark.working, pytest.mark.indexing, pytest.mark.flaky(rerun_filter=delay_rerun)]
 
 
-def test_aggregation_facet(workbook, testapp):
-    notice_pytest_fixtures(workbook, testapp)
+def test_aggregation_facet(workbook, es_testapp):
+    notice_pytest_fixtures(workbook, es_testapp)
 
-    res = testapp.get('/search/?type=ExperimentSetReplicate').json
-    badge_facets = [facet for facet in res['facets'] if facet['title'] in
-                   ['Commendations', 'Warnings']]
+    res = es_testapp.get('/search/?type=ExperimentSetReplicate').json
+    badge_facets = [facet
+                    for facet in res['facets']
+                    if facet['title'] in ['Commendations', 'Warnings']]
     assert badge_facets
     titles = [facet['title'] for facet in badge_facets]
     assert 'Commendations' in titles and 'Warnings' in titles
@@ -29,10 +30,10 @@ def test_aggregation_facet(workbook, testapp):
     assert len([t for t in terms if t != 'No value']) == 3
 
 
-def test_aggregation_itemview(workbook, testapp):
-    notice_pytest_fixtures(workbook, testapp)
+def test_aggregation_itemview(workbook, es_testapp):
+    notice_pytest_fixtures(workbook, es_testapp)
 
-    res = testapp.get('/experiment-set-replicates/4DNESAAAAAA1/').json
+    res = es_testapp.get('/experiment-set-replicates/4DNESAAAAAA1/').json
     assert 'aggregated-items' in res.keys()
     parents = ''.join([badge['parent'] for badge in res['aggregated-items']['badges']])
     assert 'biosample' in parents and 'experiment-set-replicate' in parents
@@ -41,10 +42,10 @@ def test_aggregation_itemview(workbook, testapp):
     assert len(items) == len(list(set(items)))
 
 
-def test_aggregation_view(workbook, testapp):
-    notice_pytest_fixtures(workbook, testapp)
+def test_aggregation_view(workbook, es_testapp):
+    notice_pytest_fixtures(workbook, es_testapp)
 
-    res = testapp.get('/experiment-set-replicates/4DNESAAAAAA1/@@aggregated-items').json
+    res = es_testapp.get('/experiment-set-replicates/4DNESAAAAAA1/@@aggregated-items').json
     agg = res['aggregated_items']
     assert 'badges' in agg.keys()
     assert len(agg['badges']) >= 3
