576dbff37177feaea7cea8211e2de201ab0a989b
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-02-22 00:08:44 -0500

    Use es_testapp instead of workbook's testapp in test_purge_item_type.py

--- src/encoded/tests/test_purge_item_type.py
+++ src/encoded/tests/test_purge_item_type.py
@@ -1,18 +1,18 @@
 import pytest
 import time
 from dcicutils.qa_utils import notice_pytest_fixtures
-from .workbook_fixtures import app_settings, app, workbook
+# from .workbook_fixtures import app_settings, app, workbook
 from encoded.commands.purge_item_type import purge_item_type_from_storage
 
 
-notice_pytest_fixtures(app_settings, app, workbook)
+# notice_pytest_fixtures(app_settings, app, workbook)
 
 
 pytestmark = [pytest.mark.working]
 
 
 @pytest.fixture
-def dummy_static_section(testapp):
+def dummy_static_section(es_testapp):
     static_section = {  # from workbook_inserts
         "name": "search-info-header.Workflow_copy",
         "uuid": "442c8aa0-dc6c-43d7-814a-854af460b015",
@@ -20,12 +20,12 @@ def dummy_static_section(testapp):
         "title": "Workflow Information",
         "body": "Some text to be rendered as a header"
     }
-    testapp.post_json('/static_section', static_section, status=201)
-    testapp.post_json('/index', {'record': True})
+    es_testapp.post_json('/static_section', static_section, status=201)
+    es_testapp.post_json('/index', {'record': True})
 
 
 @pytest.fixture
-def many_dummy_static_sections(testapp):
+def many_dummy_static_sections(es_testapp):
     static_section_template = {
         "name": "search-info-header.Workflow",
         "section_type": "Search Info Header",
@@ -35,35 +35,36 @@ def many_dummy_static_sections(testapp):
     paths = []
     for i in range(6):  # arbitrarily defined
         static_section_template['name'] = 'search-info-header.Workflow:%s' % i
-        resp = testapp.post_json('/static_section', static_section_template, status=201).json
+        resp = es_testapp.post_json('/static_section', static_section_template, status=201).json
         paths.append(resp['@graph'][0]['@id'])
-    testapp.post_json('/index', {'record': True})
+    es_testapp.post_json('/index', {'record': True})
     return paths
 
 
 @pytest.mark.parametrize('item_type', ['static_section'])  # maybe should test some other types...
-def test_purge_item_type_from_db(testapp, dummy_static_section, item_type):
+def test_purge_item_type_from_db(es_testapp, dummy_static_section, item_type):
     """ Tests purging all items of a certain item type from the DB """
-    assert purge_item_type_from_storage(testapp, [item_type]) is True
-    testapp.post_json('/index', {'record': True})
-    testapp.get('/search/?type=StaticSection', status=404)
-    testapp.get('/static-sections/442c8aa0-dc6c-43d7-814a-854af460b015?datastore=database', status=404)
+    assert purge_item_type_from_storage(es_testapp, [item_type]) is True
+    es_testapp.post_json('/index', {'record': True})
+    es_testapp.get('/search/?type=StaticSection', status=404)
+    es_testapp.get('/static-sections/442c8aa0-dc6c-43d7-814a-854af460b015?datastore=database', status=404)
 
 
-def test_purge_item_type_from_db_many(testapp, many_dummy_static_sections):
+def test_purge_item_type_from_db_many(es_testapp, many_dummy_static_sections):
     """ Tests posting/deleting several static sections and checking all are gone """
     paths_to_check = many_dummy_static_sections
-    assert purge_item_type_from_storage(testapp, ['static_section']) is True
-    testapp.post_json('/index', {'record': True})
+    assert purge_item_type_from_storage(es_testapp, ['static_section']) is True
+    es_testapp.post_json('/index', {'record': True})
     path_string = '%s?datastore=database'
     for path in paths_to_check:
-        testapp.get(path_string % path, status=404)
-    testapp.get('/search/?type=StaticSection', status=404)
+        es_testapp.get(path_string % path, status=404)
+    es_testapp.get('/search/?type=StaticSection', status=404)
 
 
-def test_purge_item_type_with_links_fails(testapp, workbook):
+def test_purge_item_type_with_links_fails(es_testapp, workbook):
     """ Tries to remove 'lab', which should fail since it has links """
-    testapp.post_json('/index', {'record': True})  # must index everything so individual links show up
+    notice_pytest_fixtures(workbook)
+    es_testapp.post_json('/index', {'record': True})  # must index everything so individual links show up
     time.sleep(5)  # wait for indexing to catch up
-    assert not purge_item_type_from_storage(testapp, ['lab'])
-    testapp.post_json('/index', {'record': True})
+    assert not purge_item_type_from_storage(es_testapp, ['lab'])
+    es_testapp.post_json('/index', {'record': True})
