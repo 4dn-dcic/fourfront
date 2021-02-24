b457e2fa3791e2df57f341ffd66ad2d3ade8564b
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-02-24 15:41:26 -0500

    Wire in a visibly defined set of schema formats. Add some test cases for schema_formats.py that check what's declared against what's needed.

--- src/encoded/schema_formats.py
+++ src/encoded/schema_formats.py
@@ -1,10 +1,7 @@
-import glob
-import io
-import json
-import pkg_resources
 import re
 import rfc3987
 
+from dcicutils.misc_utils import ignored
 from jsonschema_serialize_fork import FormatChecker
 from pyramid.threadlocal import get_current_request
 from .server_defaults import (
@@ -13,27 +10,13 @@ from .server_defaults import (
     TEST_PREFIX,
     test_accession,
 )
-from uuid import UUID
 
 
-def compute_accession_codes():
-    letter_pairs = set()
-    for file in glob.glob(pkg_resources.resource_filename('encoded', 'schemas/*.json')):
-        with io.open(file) as fp:
-            schema = json.load(fp)
-        letter_pair = schema.get('properties', {}).get('accession', {}).get('accessionType')
-        if letter_pair:
-            if not isinstance(letter_pair, str) or len(letter_pair) != 2:
-                raise RuntimeError("accession_type in %s is not a 2-character string:", letter_pair)
-            letter_pairs.add(letter_pair)
-    return "|".join(sorted(letter_pairs))
-
-
-ACCESSION_CODES = compute_accession_codes()
+ACCESSION_CODES = "BS|ES|EX|FI|FS|IN|SR|WF"
 
 accession_re = re.compile(r'^%s(%s)[1-9A-Z]{7}$' % (ACCESSION_PREFIX, ACCESSION_CODES))
 test_accession_re = re.compile(r'^%s(%s)[0-9]{4}([0-9][0-9][0-9]|[A-Z][A-Z][A-Z])$' % (TEST_PREFIX, ACCESSION_CODES))
-uuid_re = re.compile(r'(?i)\{?(?:[0-9a-f]{4}-?){8}\}?')
+uuid_re = re.compile(r'(?i)\{?(?:[0-9a-f]{4}-?){8}\}?')  # NoQA - https://youtrack.jetbrains.com/issue/PY-47380
 
 
 @FormatChecker.cls_checks("uuid")
@@ -44,7 +27,7 @@ def is_uuid(instance):
 
 
 def is_accession(instance):
-    ''' just a pattern checker '''
+    """Just a pattern checker."""
     # Unfortunately we cannot access the accessionType here
     return (
         accession_re.match(instance) is not None or
@@ -66,14 +49,14 @@ def is_accession_for_server(instance):
 
 @FormatChecker.cls_checks("gene_name")
 def is_gene_name(instance):
-    ''' should check a webservice at HGNC/MGI for validation '''
+    """This SHOULD check a webservice at HGNC/MGI for validation, but for now this just returns True always.."""
+    ignored(instance)
     return True
 
 
 @FormatChecker.cls_checks("target_label")
 def is_target_label(instance):
     if is_gene_name(instance):
-        #note this always returns true
         return True
     mod_histone_patt = "^H([234]|2A|2B)[KRT][0-9]+(me|ac|ph)"
     fusion_patt = "^(eGFP|HA)-"
