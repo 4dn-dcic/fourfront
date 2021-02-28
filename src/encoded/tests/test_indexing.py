5bc656006742d64d1c5647be1e6dc0e12b4de447
Kent Pitman <kent_pitman@hms.harvard.edu>
2021-02-28 17:22:25 -0500

    Changes to fix some tests that are not working locally by upgrading to elasticsearch 6.8.1.

--- src/encoded/tests/test_indexing.py
+++ src/encoded/tests/test_indexing.py
@@ -170,7 +170,8 @@ def test_create_mapping_on_indexing(app, testapp, registry, elasticsearch):
         assert compare_against_existing_mapping(es, namespaced_index, item_type, item_record, True)
 
 
-@pytest.mark.action_fail
+@pytest.mark.broken  # Doesn't work on GitHub Actions
+@pytest.mark.skip
 def test_file_processed_detailed(app, testapp, indexer_testapp, award, lab, file_formats):
     # post file_processed
     item = {
