fd50973420015d9e234ceacd5a0269858aba30e2
utku-ozturk <utku.ozturk@karyasmd.com>
2021-03-10 23:12:50 +0300

    update test_validation_errors unit test - 2

--- src/encoded/tests/test_validation_errors.py
+++ src/encoded/tests/test_validation_errors.py
@@ -7,12 +7,17 @@ from .workbook_fixtures import app_settings, workbook
 
 notice_pytest_fixtures(app_settings, workbook)
 
-pytestmark = [
-    pytest.mark.working, pytest.mark.indexing,
-    # pytest.mark.flaky(rerun_filter=delay_rerun),
-]
+# https://stackoverflow.com/a/42512169
+if pytest.__version__ < "3.0.0":
+  pytest.skip()
+else:
+  pytestmark = pytest.mark.skip
 
-@pytest.mark.xfail(reason="validation_errors facet was removed in search.py")
+# pytestmark = [
+#     pytest.mark.working, pytest.mark.indexing,
+#     # pytest.mark.flaky(rerun_filter=delay_rerun),
+# ]
+
 def test_validation_err_facet(workbook, testapp):
     res = testapp.get('/search/?type=ExperimentSetReplicate').json
     val_err_facets = [facet for facet in res['facets'] if facet['title'] == 'Validation Errors']
@@ -20,13 +25,11 @@ def test_validation_err_facet(workbook, testapp):
     assert val_err_facets[0]['aggregation_type'] == 'terms'
 
 
-@pytest.mark.xfail(reason="validation_errors facet was removed in search.py")
 def test_validation_err_itemview(workbook, testapp):
     res = testapp.get('/experiment-set-replicates/4DNESAAAAAA1/').json
     assert 'validation-errors' in res.keys()
 
 
-@pytest.mark.xfail(reason="validation_errors facet was removed in search.py")
 def test_validation_err_view(workbook, testapp):
     res = testapp.get('/experiment-set-replicates/4DNESAAAAAA1/@@validation-errors').json
     assert res['@id'] == '/experiment-set-replicates/4DNESAAAAAA1/'
