44fd7dd7021c1e097a515054efae8982e912a21f
utku-ozturk <utku.ozturk@karyasmd.com>
2021-04-22 12:40:28 +0300

    biosample treatments paranthesis updates

--- src/encoded/static/components/item-pages/DefaultItemView.js
+++ src/encoded/static/components/item-pages/DefaultItemView.js
@@ -531,11 +531,12 @@ export class OverViewBodyItem extends React.PureComponent {
             if (!treatment || !treatment.display_title || !object.atIdFromObject(treatment)){
                 return null;
             }
+            const treatmentType = treatment.treatment_type ? (<div>{`(${treatment.treatment_type})`}</div>) : null;
             return (
                 <div key={object.atIdFromObject(treatment)} >
                     { wrapperElementType === 'div' && typeof index === 'number' ? (index + 1) + '. ' : null }
                     { object.itemUtil.generateLink(treatment, true) }
-                    <div>({ treatment.treatment_type })</div>
+                    { treatmentType }
                 </div>
             );
         },
