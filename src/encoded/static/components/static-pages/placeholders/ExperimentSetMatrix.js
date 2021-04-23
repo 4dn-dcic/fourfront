69bb1ed9b20d588d090cff1ca9a6cc2eed5284ae
ugur-camoglu <ugur.camoglu@karyasmd.com>
2021-04-23 18:18:39 +0300

    minor update

--- src/encoded/static/components/static-pages/placeholders/ExperimentSetMatrix.js
+++ src/encoded/static/components/static-pages/placeholders/ExperimentSetMatrix.js
@@ -434,13 +434,15 @@ class VisualBody extends React.PureComponent {
             for (i = 0; i < statePrioritizationForGroups.length; i++){
                 stateToTest = statePrioritizationForGroups[i];
                 if ( _.any(data, { 'state' : stateToTest }) ){
-                    const anyNonAdditionalFound = _.any(data, function (item) { return item.is_additional_data !== true; });
-                    if (anyNonAdditionalFound) {
-                        submissionState = stateToTest;
-                    }
-                    else {
-                        submissionState = 'additional-data';
-                    }
+                    //comment out this code to set additional data’s style explicitly
+                    /*     const anyNonAdditionalFound = _.any(data, function (item) { return item.is_additional_data !== true; });
+                        if (anyNonAdditionalFound) {
+                            submissionState = stateToTest;
+                        }
+                        else {
+                            submissionState = 'additional-data';
+                        } */
+                    submissionState = stateToTest;
                     break;
                 }
             }
