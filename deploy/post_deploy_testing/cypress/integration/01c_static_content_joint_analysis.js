d2f233d893a200e39320fb2d9d4f925e5dc1c907
ugur-camoglu <ugur.camoglu@karyasmd.com>
2022-01-13 15:59:06 +0300

    update 1c test

--- deploy/post_deploy_testing/cypress/integration/01c_static_content_joint_analysis.js
+++ deploy/post_deploy_testing/cypress/integration/01c_static_content_joint_analysis.js
@@ -1,10 +1,10 @@
 describe('Joint Analysis Page', function () {
 
     before(function(){
-        cy.visit('/joint-analysis-plans').wait(100).end();
+        cy.visit('/joint-analysis').wait(100).end();
     });
 
-    it('Redirected to /joint-analysis from /joint-analysis-plans', function(){
+    it.skip('Redirected to /joint-analysis from /joint-analysis-plans', function(){
         cy.location('pathname').should('equal', '/joint-analysis').wait(5000).end();
     });
 
