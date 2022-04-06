b672dd8ede534aa7b3256ee973f3b1fed2e941af
ugur-camoglu <ugur.camoglu@karyasmd.com>
2022-04-06 10:08:14 +0300

    session control

--- src/encoded/static/components/item-pages/components/WorkflowDetailPane/index.js
+++ src/encoded/static/components/item-pages/components/WorkflowDetailPane/index.js
@@ -63,7 +63,7 @@ export class WorkflowDetailPane extends React.PureComponent {
                 commonDetailProps.minWidth = 800;
             }
 
-            if (node.meta && node.meta.run_data && node.meta.run_data.file){
+            if (node.meta && node.meta.run_data && node.meta.run_data.file&& session){
                 // File
                 return <FileDetailBody {...commonDetailProps} file={node.meta.run_data.file} />;
             }
