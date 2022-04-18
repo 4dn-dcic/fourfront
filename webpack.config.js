4cddb41a0a3e392377113089b38d072d858cdda6
ugur-camoglu <ugur.camoglu@karyasmd.com>
2022-04-18 16:02:17 +0300

    add sass

--- webpack.config.js
+++ webpack.config.js
@@ -64,7 +64,19 @@ const rules = [
                 loader: 'babel-loader'
             }
         ]
-    }
+    },
+    //add css load
+    {
+        test: /\.s[ac]ss$/i,
+        use: [
+            // Creates `style` nodes from JS strings
+            "style-loader",
+            // Translates CSS into CommonJS
+            "css-loader",
+            // Compiles Sass to CSS
+            "sass-loader",
+        ],
+    },
 ];
 
 const resolve = {
