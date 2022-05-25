003677aeee93c8518860146e25ab531f26fa0535
utku-ozturk <utku.ozturk@karyasmd.com>
2022-05-25 16:03:52 +0300

    bump version

--- src/encoded/static/components/app.js
+++ src/encoded/static/components/app.js
@@ -272,7 +272,7 @@ export default class App extends React.PureComponent {
                 'message' : (
                     <div>
                         <p className="mb-0">
-                            <a href="https://www.google.com/chrome/" rel="noopener noreferrer" target="_blank" className="text-500">Google Chrome</a>
+                            <a href="https://www.google.com/chrome/" rel="noopener noreferrer" target="_blank" className="text-500">Google Chrome</a>&nbsp;
                             or <a href="https://www.mozilla.org/en-US/firefox/" rel="noopener noreferrer" target="_blank" className="text-500">Mozilla Firefox</a> are
                             the recommended browser(s) for using the 4DN Data Portal.
                         </p>
