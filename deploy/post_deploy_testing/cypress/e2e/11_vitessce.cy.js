a3b3468c67dd632e84a07dbd545fd1507f941696
utku-ozturk <utku.ozturk@karyasmd.com>
2023-01-26 09:53:39 +0300

    coverage improved

--- deploy/post_deploy_testing/cypress/e2e/11_vitessce.cy.js
+++ deploy/post_deploy_testing/cypress/e2e/11_vitessce.cy.js
@@ -10,29 +10,22 @@ describe('Vitessce display page', function () {
             cy.visit('/search/');
         });
 
-        it('Ensure logged in, visit vitesse search page ', function () {
+        it('Ensure logged in, then search FileMicroscopy items that are Vitessce compatible', function () {
             cy.login4DN({ 'email': '4dndcic@gmail.com', 'useEnvToken': true }).wait(1000)
                 .visit('/search/?type=FileMicroscopy&tags=vitessce')
-                .searchPageTotalResultCount().should('be.greaterThan', 1).end();
-
-            let intervalCount = null;
-            cy.searchPageTotalResultCount().then((totalCountExpected) => {
-                intervalCount = Math.min(0, parseInt(totalCountExpected / 3));
-            });
-
-            cy.scrollToBottom().then(() => {
-                cy.get('.search-results-container .search-result-row[data-row-number="' + intervalCount + '"] .search-result-column-block[data-field="display_title"] a').click({ force: true }).wait(200).end();
-            }).end();
-
+                .searchPageTotalResultCount().should('be.greaterThan', 0).end()
+                .get('.search-results-container .search-result-row[data-row-number="0"] .search-result-column-block[data-field="display_title"] a').click({ force: true }).wait(200).end();
         });
 
-        it('Preview vitessce', function () {
+        it('Preview a Vitessce-compatible item, check viewer is loaded and displays the image correctly', function () {
+            cy.login4DN({ 'email': '4dndcic@gmail.com', 'useEnvToken': true }).wait(1000);
             cy.window().then(function (w) {
-                let currPagePath = "/";
-                cy.location('pathname').should('not.equal', currPagePath)
+                cy.location('pathname').should('not.equal', "/")
                     .then(function (pathName) {
-                        currPagePath = pathName;
-                        console.log(currPagePath);
+                        pathName = Cypress._.filter(pathName.split('/'));
+                        const fileAccession = pathName[pathName.length - 1];
+                        cy.wrap(fileAccession).as('fileAccession');
+                        console.log('File accession:', fileAccession);
                     }).wait(3000).end()
                     .get('h1.page-title').should('not.be.empty').end()
                     .get('div.rc-tabs span[data-tab-key="file-vitessce"]').should('contain', 'Vitessce').end();
@@ -41,13 +34,10 @@ describe('Vitessce display page', function () {
                 cy.get('div.title .title-left').each(function ($title, index) {
                     if (index === 0) {
                         expect($title[0].innerText).to.eql('Spatial');
-                    }
-                    else if (index == 1) {
+                    } else if (index == 1) {
                         expect($title[0].innerText).to.eql('Spatial Layers');
                     }
-                }).end()
-                    .wait(2000).login4DN({ 'email': '4dndcic@gmail.com', 'useEnvToken': true }).wait(1000);
-
+                }).end().get('@fileAccession').then((fileAccession) => cy.get('[id="layer-' + fileAccession + '.ome.tiff-controls"]').should('exist'));
             }).end().wait(3000);
         });
     });
