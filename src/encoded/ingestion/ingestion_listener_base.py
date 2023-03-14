09df468971f6d4e710950eac466fa775cbc243bc
David Michaels <david_michaels@hms.harvard.edu>
2023-03-14 08:30:34 -0400

    Minor updates based on PR-1783 feedback.

--- src/encoded/ingestion/ingestion_listener_base.py
+++ src/encoded/ingestion/ingestion_listener_base.py
@@ -1,19 +1,12 @@
 from dcicutils.misc_utils import environ_bool
 from ..util import resolve_file_path
 
-VARIANT_SCHEMA = resolve_file_path('./schemas/variant.json')
-VARIANT_SAMPLE_SCHEMA = resolve_file_path('./schemas/variant_sample.json')
 STATUS_QUEUED = 'Queued'
 STATUS_INGESTED = 'Ingested'
 STATUS_DISABLED = 'Ingestion disabled'
 STATUS_ERROR = 'Error'
 STATUS_IN_PROGRESS = 'In progress'
 SHARED = 'shared'
-STRUCTURAL_VARIANT_SCHEMA = resolve_file_path("./schemas/structural_variant.json")
-STRUCTURAL_VARIANT_SAMPLE_SCHEMA = resolve_file_path(
-    "./schemas/structural_variant_sample.json"
-)
-
 DEBUG_SUBMISSIONS = environ_bool("DEBUG_SUBMISSIONS", default=False)
 
 
