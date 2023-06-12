3cc462d90bf8c2f0e520aa472b8fb9c962f5b2e4
David Michaels <david_michaels@hms.harvard.edu>
2023-06-12 16:27:39 -0400

    C4-1019: continue ingestion-to-snovault migration work

--- src/encoded/server_defaults.py
+++ src/encoded/server_defaults.py
@@ -1,11 +1,3 @@
-import random
-import uuid
-
-#from jsonschema_serialize_fork import NO_DEFAULT
-#from pyramid.path import DottedNameResolver
-#from pyramid.threadlocal import get_current_request
-#from dcicutils.misc_utils import utc_now_str
-#from snovault.schema_utils import server_default
 from snovault.server_defaults import (
     add_last_modified,
     enc_accession,
