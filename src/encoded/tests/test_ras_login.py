286a8062c3a469092b619c5fac46550b27fb9175
William Ronchetti <wrr33@cornell.edu>
2023-10-19 12:13:04 -0400

    lock new snovault, update test to use GET

--- src/encoded/tests/test_ras_login.py
+++ src/encoded/tests/test_ras_login.py
@@ -3,6 +3,6 @@ import pytest
 
 def test_login_redirects_to_ras(ras_testapp):
     """ Tests that a POST to /login will redirect the user to the RAS login page """
-    res = ras_testapp.post_json('/login', {})
+    res = ras_testapp.get('/login')
     assert '307' in res.status
     assert 'nih.gov' in res.location
