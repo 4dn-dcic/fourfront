177ea9ac4fd648574a88eeeaf2332d25ef65a5e3
utku-ozturk <utku.ozturk@karyasmd.com>
2024-01-28 23:24:55 +0300

    auth0 custom info

--- src/encoded/static/components/navigation/components/AccountNav.js
+++ src/encoded/static/components/navigation/components/AccountNav.js
@@ -52,9 +52,13 @@ export const AccountNav = React.memo(function AccountNav(props){
     const { session, updateAppSessionState, schemas, ...passProps } = props;
     const { windowWidth, href } = passProps;
     if (!session) { // Render login button
+        // works only with Auth0 lock
+        // TODO remove custom info after RAS transition completed
+        const auth0CustomInfo = '<p><span class="text-danger font-weight-bold">WARNING</span> - support for login to the portal via <b>GitHub</b> is ending in <b>early 2024</b>.</p><p>If you currently use your Github credentials to log in to the data portal you will need to transition to using a Google account or NIH credentials - eRA, <a href="https://login.gov">login.gov</a> or NIH passport.  More info and a guide to setting up a linked Google account can be found <a href="https://data.4dnucleome.org/help/user-guide/account-creation" target="_blank">here</a>.<p>';
+
         return (
             <Nav className="navbar-acct">
-                <LoginController {...{ updateAppSessionState, auth0Options }}>
+                <LoginController {...{ updateAppSessionState, auth0Options, href, auth0CustomInfo }}>
                     <LoginNavItem {...{ schemas, session, href, windowWidth }} key="login-register" className="user-account-item" />
                 </LoginController>
             </Nav>
