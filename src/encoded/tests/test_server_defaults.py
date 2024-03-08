1171f41f22602e065dce957f807f22096e3924a1
William Ronchetti <wrr33@cornell.edu>
2024-03-08 12:50:48 -0500

    fix test relying on 4dn accession

--- src/encoded/tests/test_server_defaults.py
+++ src/encoded/tests/test_server_defaults.py
@@ -20,7 +20,7 @@ def test_server_defaults(admin, anontestapp):
     item = res.json['@graph'][0]
     assert item['now'].startswith('2')
     assert item['user'] == admin['@id']
-    assert item['accession'].startswith('4DNAB')
+    assert item['accession'].startswith('TSTBS')  # recent change, use TEST accession instead
 
     anontestapp.patch_json(
         res.location, {}, status=200,
