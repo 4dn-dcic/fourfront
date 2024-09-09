03bd69aed3bd4deaed89d244f6bdccb3578869fa
Onurcankaratay <karatayonurcan@outlook.com>
2024-09-09 15:48:24 +0300

    Cypress 01b test fix

--- deploy/post_deploy_testing/cypress/e2e/01b_static_content.cy.js
+++ deploy/post_deploy_testing/cypress/e2e/01b_static_content.cy.js
@@ -17,6 +17,17 @@ describe('Static Page & Content Tests', function () {
     it('Click & visit each page from menu, ensure ToC exists somewhere, ensure ToC works.', function(){
 
         cy.on('uncaught:exception', function(err, runnable){
+            // TODO: Investigate hydration errors occurring during SSR in Cypress tests.
+            // It appears that Cypress injects a prop into the div on the client side, which doesn't exist on the server side, leading to hydration issues.
+            // This suppression is temporary and should be removed once the issue is resolved.
+            // https://github.com/cypress-io/cypress/issues/27204
+            if (
+                /hydrat/i.test(err.message) ||
+                /Minified React error #418/.test(err.message) ||
+                /Minified React error #423/.test(err.message)
+            ) {
+                return false;
+            }
 
             expect(err.message).to.include("return response;");
 
