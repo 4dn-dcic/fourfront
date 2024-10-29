f267db3bb62f993c332da447bb9dd04b28c80e4a
David Michaels <david_michaels@hms.harvard.edu>
2024-10-29 12:20:21 -0400

    debugging ga for test_aggregation.py::test_aggregation_facet

--- src/encoded/tests/test_init.py
+++
@@ -1,6 +0,0 @@
-# import pytest
-#
-# pytestmark = pytest.mark.working
-#
-# Tests that were here aren't needed because the functionality comes from dcicutils.env_utils now,
-# and these tests were moved to that library. -kmp 27-Mar-2020
